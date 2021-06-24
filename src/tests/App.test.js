4bf63d3eb9a5a5d10f106e0a9136f73581b65ce5
OldRoge <rogeriomrjr@icloud.com>
2021-06-24 18:24:01 -0300

    Done requirement 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render /* fireEvent */ } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,33 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Verifica se contém um conjunto fixo de links de navegação no topo.', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const textHome = getByText(/Home/i);
+    fireEvent.click(textHome);
+    const link = history.location.pathname;
+    expect(link).toBe('/');
+  });
+
+  it('O segundo link deve possuir o texto About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const textAbout = getByText(/About/i);
+    fireEvent.click(textAbout);
+    const link = history.location.pathname;
+    expect(link).toBe('/about');
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokemons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const favPokemons = getByText(/Favorite Pokémons/i);
+    fireEvent.click(favPokemons);
+    const link = history.location.pathname;
+    expect(link).toBe('/favorites');
+  });
 });
