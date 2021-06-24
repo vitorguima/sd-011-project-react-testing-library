ed3b5179c47528771dfaa290a7fb6b558f224e7b
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-06-24 18:38:14 -0300

    requisitos 2 - 2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,17 +1,3 @@
-import React from 'react';
-import renderWithRouter from '../renderWithRouter';
-import FavoritePokemons from '../components/FavoritePokemons';
 
-test('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
-  const { getByText } = renderWithRouter(<FavoritePokemons />);
-  const favoritePokemonTest = getByText(/No favorite pokemon found/);
-  expect(favoritePokemonTest).toBeInTheDocument();
-});
-
-// test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
-//   const { getByText } = renderWithRouter(<FavoritePokemons />);
-//   const favoritePokemonTest = getByText(/No favorite pokemon found/);
-//   expect(favoritePokemonTest).toBeInTheDocument();const
-// });
 
 
