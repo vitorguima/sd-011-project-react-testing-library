3257a52c63b8a35a7ae9478ee611fb49919101dc
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-24 14:48:50 -0300

    Adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,43 @@
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('renders a reading with the text `Pokédex`', () => {
+  it('Verifica se a página inicialmente carregada é "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica os links de navegação da página inicial', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Home')).toBeTruthy();
+    expect(getByText('About')).toBeTruthy();
+    expect(getByText('Favorite Pokémons')).toBeTruthy();
+  });
+
+  it('Verifica se a apliação é redicionada para / ao clicar em Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const homeLink = getByText('Home');
+    fireEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se a apliação é redicionada para /about ao clicar em About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const aboutLink = getByText('About');
+    fireEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Aplicação é redirecionado para /favorites ao clicar em Favorite Pokémons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoritePokemonsLink = getByText('Favorite Pokémons');
+    fireEvent.click(favoritePokemonsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 });
