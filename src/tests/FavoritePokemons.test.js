8392f48d6d7e5c3be6e971a79ef0f7f21f120050
Diego Ribeiro <dihrm2001@gmail.com>
2021-06-24 16:08:13 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+import pokemons from '../data';
+
+const [pikachu, charmander] = pokemons;
+
+test('Renderiza mensagem `No favorite pokemon found` se não existirem pokémons favoritos',
+  () => {
+    const { getByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ [] } />,
+    );
+    const title = getByText('No favorite pokemon found');
+    expect(title).toBeInTheDocument();
+  });
+
+test('Pokemons favoritados são exibidos na tela corretamente', () => {
+  renderWithRouter(
+    <FavoritePokemons pokemons={ [pikachu, charmander] } />,
+  );
+
+  const cards = document.querySelectorAll('.favorite-pokemon');
+
+  expect(cards.length).toBe(2);
+});
+
+test('Não exibe nenhum card quando não há pokemons favoritados', () => {
+  renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+  const cards = document.querySelectorAll('.favorite-pokemons');
+
+  expect(cards.length).toBe(0);
+});
