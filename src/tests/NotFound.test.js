c99e2ad614e52760f04caf105f84782f26111a53
Matheus_Alexandre <alexandremhm@gmail.com>
2021-06-24 17:07:28 -0300

    Add requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('testa componente notFound', () => {
+  test('Teste se página contém um heading h2', () => {
+    const { getByRole } = render(<NotFound />);
+    const head = getByRole('heading');
+    expect(head).toBeInTheDocument();
+    expect(head.textContent).toBe('Page requested not found 😭');
+  });
+  test('Testa a imagem gif ', () => {
+    const { container } = render(<NotFound />);
+    const ImgPoke = container.querySelector('img').src;
+    expect(ImgPoke).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
