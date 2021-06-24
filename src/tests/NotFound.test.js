dc1b40b3d93cf7b750f39c6ffdca203e4bf783ed
Amandha W. Barok <amandha.barok@gmail.com>
2021-06-24 15:40:37 -0700

    Fourth requiriment done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requirement number 4', () => {
+  it('should render a heading h2 with the content `Page request not found`', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const heading = getByText('Page requested not found');
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should have a image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText(/Pikachu crying/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
