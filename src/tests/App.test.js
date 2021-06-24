3a91f27add44d966a5f0b16a7fe0fee5b503eb03
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-06-24 14:56:19 -0300

    Realizacao do primeiro teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('1 - Testa o componente <App.js />', () => {
+  it('renderiza um reading com o texto `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testa se contém os links de navegação (Home, About, Favorite Pokémons).', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const linkHome = getByRole('link', { name: 'Home' });
+    const linkAbout = getByRole('link', { name: 'About' });
+    const linkFavoritePokemon = getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavoritePokemon).toBeInTheDocument();
+  });
+
+  it('Testa o redirecionamento para as paginas ao clicar nos links de navegacao',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+
+      const homeButton = getByText('Home');
+      const aboutButton = getByText('About');
+      const favoriteButton = getByText('Favorite Pokémons');
+      expect(homeButton).toBeInTheDocument();
+      expect(aboutButton).toBeInTheDocument();
+      expect(favoriteButton).toBeInTheDocument();
+
+      fireEvent.click(homeButton);
+      expect(history.location.pathname).toBe('/');
+      fireEvent.click(aboutButton);
+      expect(history.location.pathname).toBe('/about');
+      fireEvent.click(favoriteButton);
+      expect(history.location.pathname).toBe('/favorites');
+      history.push('/path/random');
+      expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+    });
 });
