462277c9fed6938f2441b64e1c21cf4105536cae
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-24 15:11:52 -0300

    [filizzola-t-l] =  questão 2 About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -6,4 +6,7 @@ test('Teste se a página contém as informações sobre a Pokédex.', () => {
   const { container } = render(<About />);
   const getAbout = container.querySelector('h2');
   expect(getAbout.textContent).toBe('About Pokédex');
+
+  const getP = container.querySelectorAll('p');
+  expect(getP.length).toBe(2);
 });
