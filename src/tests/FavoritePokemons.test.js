33b15c9baf45fb2576d085ccdf11a58ac745a483
heyset <c.inaciomatheus@gmail.com>
2021-06-24 20:56:30 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,17 +2,52 @@ import React from 'react';
 import { fireEvent } from '@testing-library/dom';
 
 import renderWithHistory from './helpers/renderWithHistory';
-import FavoritePokemons from '../components/FavoritePokemons';
+import sleep from './helpers/sleep';
 
-describe('Quando não há pokémon favoritado', () => {
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Quando não há pokémon favoritado:', () => {
   let getByText;
-  let getByTestId;
+  let queryByTestId;
 
   beforeEach(() => {
-    ({ getByText, getByTestId } = renderWithHistory(<FavoritePokemons />));
+    ({ getByText, queryByTestId } = renderWithHistory(<FavoritePokemons />));
   });
 
   it('mostra texto indicando que não há pokémon favoritado.', () => {
-    expect(getByText('No favorite pokemon found')).not.toBeInTheDocument();
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  it('NÃO mostra cards de pokémons', () => {
+    expect(queryByTestId('pokemon-name')).not.toBeInTheDocument();
+  });
+});
+
+describe('Quando há pokémons favoritados:', () => {
+  it('mostra todos na tela de favoritos', async () => {
+    const NAVIGATION_DELAY = 50;
+
+    const {
+      getByLabelText,
+      history,
+      getByText,
+      getByAltText } = renderWithHistory(<App />);
+
+    history.push('/pokemons/25');
+    await sleep(NAVIGATION_DELAY);
+
+    fireEvent.click(getByLabelText('Pokémon favoritado?'));
+
+    history.push('/pokemons/4');
+    await sleep(NAVIGATION_DELAY);
+
+    fireEvent.click(getByLabelText('Pokémon favoritado?'));
+
+    fireEvent.click(getByText('Favorite Pokémons'));
+    await sleep(NAVIGATION_DELAY);
+
+    expect(getByAltText('Charmander is marked as favorite')).toBeInTheDocument();
+    expect(getByAltText('Pikachu is marked as favorite')).toBeInTheDocument();
   });
 });
