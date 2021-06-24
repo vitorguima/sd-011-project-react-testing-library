aa2e998f2821b9cb25e63de6da992fb751293840
LeoFuna <leo.funa@gmail.com>
2021-06-24 15:57:11 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes no componente Not Found', () => {
+  test('Contem h2 com mensagem especifica', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const notFoundMessage = getByText(/Page requested not found/);
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+  test('Mostra imagem especifica', () => {
+    renderWithRouter(<NotFound />);
+    const myImgHtml = document.querySelector('img');
+    expect(myImgHtml.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
