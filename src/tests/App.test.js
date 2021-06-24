d15061346cd9d4864a40b44809cdef1a0434f71a
marlon307 <marlon-ramosb@hotmail.com>
2021-06-24 17:02:54 -0300

    Rq 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('shows the Pokédex when the route is `/`.', () => {
+  test('Teste o componente About.js .', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('Teste se o topo da aplicação contém um conjunto fixo de links', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
 });
