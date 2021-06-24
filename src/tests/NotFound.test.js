6cc26ec05f95b4ae053bc6f903d3daaef9a895b1
KaioHPs <kaiohpsalternativo@gmail.com>
2021-06-24 18:32:33 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Verifies the About component', () => {
+  it('renders a h2 with the text `Page requested not found 😭`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+    expect(getByText('😭')).toBeInTheDocument();
+  });
+
+  it('renders an gif of the of a sad Pikachu', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const notFoundImgs = getAllByRole('img');
+    expect(notFoundImgs.length).toBe(2);
+    expect(notFoundImgs[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
