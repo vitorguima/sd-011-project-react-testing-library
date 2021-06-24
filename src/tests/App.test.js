3c635fe9727e2339a28089e70dfe6dfd9bcad9cc
Leandro Lopes <leandro_carlos@uol.com.br>
2021-06-24 17:34:11 -0300

    item 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste o componente App', () => {
+  test('pág principal é renderizada ao carregar a aplicação no caminho de URL/', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Encountered pokémons');
+    expect(home).toBeInTheDocument();
+  });
+
+  test('teste se existe o link home e sua funcionalidade', () => {
+    const { getByRole, history: { location: { pathname } } } = renderWithRouter(<App />);
+
+    const linkHome = getByRole('link', {
+      name: /home/i,
+    });
+    userEvent.click(linkHome);
+    expect(pathname).toBe('/');
+  });
+
+  test('teste se existe o link About e sua funcionalidade', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    const linkAbout = getByRole('link', {
+      name: /about/i,
+    });
+    userEvent.click(linkAbout);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+
+  test('teste se existe o link Pokémons Favoritados e sua funcionalidade', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    const linkFavorite = getByRole('link', {
+      name: /favorite/i,
+    });
+    userEvent.click(linkFavorite);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Teste a página Not Found ao entrar em uma URL desconhecida', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('/route-not-exists/');
+
+    const linkNotExist = getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(linkNotExist).toBeInTheDocument();
+  });
 });
