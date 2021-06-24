7a8318cd1ab88435264317d795d376bbb9bf1ede
Tulio Sploradori <tuliospl@gmail.com>
2021-06-24 17:05:44 -0300

    correçao do lint requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,17 +1,17 @@
-import { fireEvent, getByText } from '@testing-library/dom';
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import RenderWithRouter from '../components/RenderWithRouter';
 
 describe('testa os elementos da aplicação Favorites Pokémons', () => {
   it('mensagem no favorite pokemon found', () => {
-    RenderWithRouter(<App />);
+    const { getByText } = RenderWithRouter(<App />);
     fireEvent.click(getByText(/Favorite Pokémons/i));
     const noMatch = getByText(/No favorite pokemon found/i);
     expect(noMatch).toBeInTheDocument();
   });
   it('e exibido todos os cards favoritados', () => {
-    const { container } = RenderWithRouter(<App />);
+    const { container, getByText } = RenderWithRouter(<App />);
     fireEvent.click(getByText(/More details/i));
     fireEvent.click(getByText(/Pokémon favoritado?/i));
     fireEvent.click(getByText(/Home/i));
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import RenderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
+
+describe('testa a aplicação NotFound', () => {
+  it('a pagina contem um h2 com Page requested not found', () => {
+    const { history } = RenderWithRouter(<App />);
+    history.push('//');
+  });
+});
