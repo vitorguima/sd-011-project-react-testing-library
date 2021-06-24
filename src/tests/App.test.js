4d3fc40bfb96f708c6961a4d80827d0edc1bcd4c
fernando lasmar <f3rnandolasmar@gmail.com>
2021-06-24 16:21:50 -0300

    Iniciando projeto, #VQV

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,17 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('Se o topo da aplicação contém links de navegação', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkHome = getByText(/Home/i);
+  expect(linkHome).toBeInTheDocument();
+  const linkAbout = getByText(/About/i);
+  expect(linkAbout).toBeInTheDocument();
+  const linkFavoritePokemon = getByText(/Favorite Pokémons/i);
+  expect(linkFavoritePokemon).toBeInTheDocument();
+});
