f3f69af510219647aa3fb99447cf64ba1bea5005
Matheus_Alexandre <alexandremhm@gmail.com>
2021-06-24 16:51:56 -0300

    Add requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
-// import renderWithRouter from '../renderWithRouter';
+import Data from '../data';
+import renderWithRouter from '../renderWithRouter';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   test('testa mensagem No favorite pokemon found', () => {
@@ -9,5 +10,17 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
     const head = getByText(/No favorite pokemon found/i);
     expect(head).toBeInTheDocument();
     expect(head.textContent).toMatch(/No favorite pokemon found/i);
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const PokemonArr = [Data[0], Data[1]];
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ PokemonArr } />);
+    expect(getByText('Pikachu')).toBeInTheDocument();
+    expect(getByText('Charmander')).toBeInTheDocument();
+  });
+  test('Nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    const PokemonArr = [];
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ PokemonArr } />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
 });
