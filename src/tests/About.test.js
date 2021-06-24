4f95b4343be7feece7493d2b96a1d45baaa3a80f
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-24 16:55:56 -0300

    Referencias citadas

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -11,12 +11,13 @@ test('Teste se a página contém as informações sobre a Pokédex', () => {
 test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
   const { container } = renderWithRouter(<About />);
   const aboutText = container.querySelector('h2');
-  expect(aboutText).toHaveTextContent('About Pokédex')
+  //https://stackoverflow.com/questions/58976251/checking-text-appears-inside-an-element-using-react-testing-library
+  expect(aboutText).toHaveTextContent('About Pokédex');
 });
 
 test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
   const { container } = renderWithRouter(<About />);
   const pokedexImg = container.querySelector('img');
-  expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png')
+  //https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+  expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
-
