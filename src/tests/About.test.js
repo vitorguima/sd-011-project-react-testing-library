577e112d3c1f33880c80f6b5cf5027fc8c2c0fb1
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-06-24 18:26:27 -0300

    requisitos 2 - 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -39,4 +39,5 @@ test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
   const { getByRole } = renderWithRouter(<About />);
   const pokedexImage = getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
   expect(pokedexImage).toBeInTheDocument();
+  expect(pokedexImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const favoritePokemonTest = getByText(/No favorite pokemon found/);
+  expect(favoritePokemonTest).toBeInTheDocument();
+});
+
+// test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+//   const { getByText } = renderWithRouter(<FavoritePokemons />);
+//   const favoritePokemonTest = getByText(/No favorite pokemon found/);
+//   expect(favoritePokemonTest).toBeInTheDocument();const
+// });
+
+
