9cd59768d73e2eb8a6c4724cbe4a64d1a493e400
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-24 16:10:07 -0300

    teste notfound requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 4', () => {
+  it('not found', () => {
+    const { container } = renderWithRouter(<App />, { route: '/not/found' });
+    const h2 = container.querySelector('h2');
+    expect(h2.textContent).toBe('Page requested not found 😭');
+  });
+  it('img src teste', () => {
+    const { container } = renderWithRouter(<App />, { route: '/not/found' });
+    const img = container.querySelector('img');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
