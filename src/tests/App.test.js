62b1ea97650305bb2234399fd11f7a03c5750731
marsleite <marsleite@gmail.com>
2021-06-24 17:02:37 -0300

    requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRoute';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,26 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Primeiro requisito do projeto', () => {
+  describe('Conjunto fixo de links de navegação.', () => {
+    it('Link da página Home', () => {
+      const { getByText } = renderWithRouter(<App />);
+      const home = getByText(/Home/i);
+      fireEvent.click(home);
+    });
+
+    it('Link da página About.', () => {
+      const { getByText } = renderWithRouter(<App />);
+      const about = getByText(/About/i);
+      fireEvent.click(about);
+    });
+
+    it('Link da página favoritos', () => {
+      const { getByText } = renderWithRouter(<App />);
+      const favoritos = getByText(/Favorite Pokémons/i);
+      fireEvent.click(favoritos);
+    });
+  });
 });
