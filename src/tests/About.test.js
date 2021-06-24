5334328290eed7932e3e0897e622e7cd2453471f
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-24 15:51:13 -0300

    Inicia projeto

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+test('Mostra a pokedex quando a rota é `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
