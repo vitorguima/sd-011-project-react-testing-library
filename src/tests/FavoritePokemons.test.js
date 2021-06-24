43607d20e51ce62539b26ff234587894d3ab4866
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-06-24 18:11:53 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -10,3 +10,28 @@ it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
   const noFavoritePokemons = getByText(/No favorite pokemon found/i);
   expect(noFavoritePokemons).toBeInTheDocument();
 });
+
+it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const goToFavorite = getByText(/Favorite Pokémons/i);
+  fireEvent.click(goToFavorite);
+
+  const noFavorite = getByText(/No favorite pokemon found/i);
+  expect(noFavorite).toBeInTheDocument();
+});
+
+it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+  const { getByText, getAllByTestId } = renderWithRouter(<App />);
+  const detailsButton = getByText(/More details/i);
+
+  fireEvent.click(detailsButton);
+  const favoriteButton = getByText(/Pokémon favoritado?/i);
+  fireEvent.click(favoriteButton);
+
+  const goToFavorite = getByText(/Favorite Pokémons/i);
+  fireEvent.click(goToFavorite);
+
+  const pokemonNameId = 'pokemon-name';
+  const pokemonName = getAllByTestId(pokemonNameId);
+  expect(pokemonName).toHaveLength(1);
+});
