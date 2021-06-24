5acef60b4be88f852167a91411e456b3d1e5437d
Tulio Sploradori <tuliospl@gmail.com>
2021-06-24 17:31:36 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,7 +4,9 @@ import App from '../App';
 
 describe('testa a aplicação NotFound', () => {
   it('a pagina contem um h2 com Page requested not found', () => {
-    const { history } = RenderWithRouter(<App />);
-    history.push('//');
+    const { history, container } = RenderWithRouter(<App />);
+    history.push('/digimon/');
+    expect(container.querySelector('h2')).toHaveTextContent('Page requested not found');
+    expect(container.querySelector('img')).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
