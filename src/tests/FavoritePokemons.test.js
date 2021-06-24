da189f12d187b267a71d00f329d68d5f78bf9c2b
Diogo Augusto <diogotrescastro@gmail.com>
2021-06-24 17:09:04 -0300

    Resoluçao do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+// it('', () => {});
+it('Testa se exibe `No favorite pokemon found`, se não tiver favoritados', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const favorite = getByText(/Favorite Pokémons/);
+  fireEvent.click(favorite);
+  const notFound = getByText(/No favorite pokemon found/);
+  expect(notFound).toBeInTheDocument();
+});
+
+it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+  const { getByText, getByLabelText } = renderWithRouter(<App />);
+  const moreDetails = getByText(/More details/);
+  fireEvent.click(moreDetails);
+  const favorite = getByLabelText(/Pokémon favoritado?/);
+  fireEvent.click(favorite);
+  const linkFav = getByText('Favorite Pokémons');
+  fireEvent.click(linkFav);
+  const pokemon = getByText(/Pikachu/);
+  expect(pokemon).toBeInTheDocument();
+});
