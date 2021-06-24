e0086e74f3472b81dff5e239219564ee2c4cf5fe
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-06-24 18:35:48 -0300

    requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Verifica requisito 3', () => {
+  it('Teste se a pessoa não tiver pokémons favoritos..', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const texto = screen.getByText(/No favorite pokemon found/i);
+    expect(texto).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados..', () => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import { NotFound } from '../components';
+
+describe('Verifica requisito 4', () => {
+  it('Teste este se página o texto Page requested not found 😭.', () => {
+    renderWithRouter(<NotFound />);
+    const not = screen.getByText(/Page requested not found/i);
+    expect(not).toBeInTheDocument();
+  });
+
+  it('teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(/Pikachu crying because /i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
