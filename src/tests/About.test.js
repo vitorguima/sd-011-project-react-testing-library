8c661de989f819bcd767f0dfb8240085f4f6583a
fredmartins <fredweb22@gmail.com>
2021-06-24 15:21:45 -0300

    commit inicial

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+  
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
