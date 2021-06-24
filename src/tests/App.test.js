9c75b8f99aa08b5f265737cd4fc52f5693e7c73d
IsleneGomes <islenegomes@gmail.com>
2021-06-24 20:51:50 -0300

    Requisito 1, com renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testa o componente App.js', () => {
   test('renders a reading with the text `Pokédex`', () => {
-    const { getByText } = render(
+    const { getByText } = renderWithRouter(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
@@ -15,26 +16,38 @@ describe('Testa o componente App.js', () => {
   });
 
   test('O primeiro link deve possuir o texto Home', () => {
-    render(<App />);
-    const textHome = screen.getByRole('link', {
-      name: /Home/i,
-    });
-    expect(textHome).toBeInTheDocument();
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/home');
+    const home = getByText(/Home/);
+    expect(home).toBeInTheDocument();
+    expect(home).toBeInTheDocument();
   });
 
   test('O primeiro link deve possuir o texto About', () => {
-    render(<App />);
-    const textAbout = screen.getByRole('link', {
-      name: /About/i,
-    });
-    expect(textAbout).toBeInTheDocument();
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+    const about = getByText(/About/);
+    expect(about).toBeInTheDocument();
   });
 
   test('O primeiro link deve possuir o texto Favorite Pokémons', () => {
-    render(<App />);
-    const textFavoritePokemons = screen.getByRole('link', {
-      name: /Favorite Pokémons/i,
-    });
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+    const aboutAll = getByText(/About/);
+    expect(aboutAll).toBeInTheDocument();
     expect(textFavoritePokemons).toBeInTheDocument();
+  });
+
+  test('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/Not Found/');
+    const noMatch = getByText(/Página não encontrada/i);
+    expect(noMatch).toBeInTheDocument();
   });
 });
