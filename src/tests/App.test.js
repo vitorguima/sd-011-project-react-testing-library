099a44c55f56b9d92f5c1074884f713fff6cd31e
Gisele Costa <jikinha@gmail.com>
2021-06-24 17:55:27 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testa o componente <App.js />', () => {
+  it('se a página principal é renderizada ao carregar a aplicação no caminho /.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/');
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
+
+describe('Testa se o topo contém um conjunto fixo de links de navegação.', () => {
+  it('O primeiro link deve possuir o texto Home.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('O segundo link deve possuir o texto About.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
+
+describe('Teste se a aplicação é redirecionada para a página Not Found', () => {
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = getByText(/Not Found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
 });
