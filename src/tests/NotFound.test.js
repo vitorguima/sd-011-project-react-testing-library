1aa153fc49f10a863df93f05bc29ea42a9079f62
Marco Galindo <marco1408@gmail.com>
2021-06-24 16:43:48 -0300

    requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+test('A página contém um heading h2 com o texto Page requested not found', () => {
+  const { getByText } = render(<NotFound />);
+  const header = getByText('Page requested not found');
+  expect(header).toBeInTheDocument();
+});
+
+test('A página contém a seguinte imagem de uma Pokédex', () => {
+  const { getByAltText } = render(<NotFound />);
+  const img = getByAltText('Pikachu crying because the page requested was not found');
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
