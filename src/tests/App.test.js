2e83e7375da2affc2da5b4185821ffb027a11372
heyset <c.inaciomatheus@gmail.com>
2021-06-24 18:16:11 -0300

    Corrige lint nos erros estranhos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -7,34 +7,35 @@ import App from '../App';
 const NAVIGATE_DELAY = 100;
 
 describe('When the app loads:', () => {
+  const LINKS_TEST_ID = 'nav-bar-link';
+
   it('it is in the home page', () => {
     const { history } = renderWithHistory(<App />);
     expect(history.location.pathname).toBe('/');
   });
 
   describe('on the nav bar:', () => {
-
     it('there are three links', () => {
-      const { getAllByTestId, getByText } = renderWithHistory(<App />);
-      const links = getAllByTestId('nav-bar-link');
+      const { getAllByTestId } = renderWithHistory(<App />);
+      const links = getAllByTestId(LINKS_TEST_ID);
       const TOTAL_LINKS = 3;
       expect(links.length).toBe(TOTAL_LINKS);
     });
 
     describe('the links are, in order:', () => {
       it('Home', () => {
-        const { getAllByTestId, getByText } = renderWithHistory(<App />);
-        const links = getAllByTestId('nav-bar-link');
+        const { getAllByTestId } = renderWithHistory(<App />);
+        const links = getAllByTestId(LINKS_TEST_ID);
         expect(links[0].innerHTML).toBe('Home');
       });
       it('About', () => {
-        const { getAllByTestId, getByText } = renderWithHistory(<App />);
-        const links = getAllByTestId('nav-bar-link');
+        const { getAllByTestId } = renderWithHistory(<App />);
+        const links = getAllByTestId(LINKS_TEST_ID);
         expect(links[1].innerHTML).toBe('About');
       });
       it('Favorite Pokémons', () => {
-        const { getAllByTestId, getByText } = renderWithHistory(<App />);
-        const links = getAllByTestId('nav-bar-link');
+        const { getAllByTestId } = renderWithHistory(<App />);
+        const links = getAllByTestId(LINKS_TEST_ID);
         expect(links[2].innerHTML).toBe('Favorite Pokémons');
       });
     });
@@ -42,7 +43,7 @@ describe('When the app loads:', () => {
     describe('the links redirect correctly', () => {
       it('to the /home page', async () => {
         const { getAllByTestId, getByText } = renderWithHistory(<App />);
-        const links = getAllByTestId('nav-bar-link');
+        const links = getAllByTestId(LINKS_TEST_ID);
         fireEvent.click(links[0]);
 
         await sleep(NAVIGATE_DELAY);
@@ -52,7 +53,7 @@ describe('When the app loads:', () => {
 
       it('to the /about page', async () => {
         const { getAllByTestId, getByText } = renderWithHistory(<App />);
-        const links = getAllByTestId('nav-bar-link');
+        const links = getAllByTestId(LINKS_TEST_ID);
         fireEvent.click(links[1]);
 
         await sleep(NAVIGATE_DELAY);
@@ -62,7 +63,7 @@ describe('When the app loads:', () => {
 
       it('to the /favorites page', async () => {
         const { getAllByTestId, getByText } = renderWithHistory(<App />);
-        const links = getAllByTestId('nav-bar-link');
+        const links = getAllByTestId(LINKS_TEST_ID);
         fireEvent.click(links[2]);
 
         await sleep(NAVIGATE_DELAY);
