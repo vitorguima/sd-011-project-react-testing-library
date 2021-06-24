118c996d52d180689f54d400c6bd2e5d9e2e98ad
thiagofox <thiagofox00@gmail.com>
2021-06-24 15:54:08 -0300

    requisito 1 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -12,6 +13,7 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={ ['/'] }>
@@ -21,4 +23,63 @@ test('shows the Pokédex when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
-// teste
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const linkHome = getByText(/Home/);
+  const linkAbout = getByText(/About/);
+  const linkFavoritePkm = getByText(/Favorite Pokémons/);
+  expect(linkAbout).toBeInTheDocument();
+  expect(linkHome).toBeInTheDocument();
+  expect(linkFavoritePkm).toBeInTheDocument();
+});
+
+// test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+//   const { getByText } = renderWithRouter(<App />);
+//   const linkAbout = getByText(/About/);
+//   expect(linkAbout).toBeInTheDocument();
+// });
+
+// test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+//   const { getByText } = renderWithRouter(<App />);
+//   const linkFavoritePkm = getByText(/Favorite Pokémons/);
+//   expect(linkFavoritePkm).toBeInTheDocument();
+// });
+
+test(`Testa se ao clicar no link Home
+  da barra de navegação a pagina é redirecionada`, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Home/i));
+  const homePath = history.location.pathname;
+  expect(homePath).toBe('/');
+  const pokedex = getByText(/Pokédex/);
+  expect(pokedex).toBeInTheDocument();
+});
+
+test(`Testa se ao clicar no link About 
+  da barra de navegação a pagina é redirecionada`, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/About/i));
+  const homePath = history.location.pathname;
+  expect(homePath).toBe('/about');
+  const AboutPokedex = getByText(/About Pokédex/);
+  expect(AboutPokedex).toBeInTheDocument();
+});
+
+test(`Testa se ao clicar no link About 
+  da barra de navegação a pagina é redirecionada`, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  const homePath = history.location.pathname;
+  expect(homePath).toBe('/favorites');
+  const AboutPokedex = getByText(/Favorite pokémons/);
+  expect(AboutPokedex).toBeInTheDocument();
+});
+
+it(`Teste se a aplicação é redirecionada para 
+  a página Not Found ao entrar em uma URL desconhecida.`, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/pagina/que-nao-existe/');
+  const pkmNoMatch = getByText(/Page requested not found/i);
+  expect(pkmNoMatch).toBeInTheDocument();
+});
