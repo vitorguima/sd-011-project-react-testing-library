ed205ab2dd1312fa4f6d270f56813bc1ad64deae
Matheus Gaspar <msouzagaspar@gmail.com>
2021-06-24 17:52:56 -0300

    mostra mensagem se não existirem pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('FavoritePokemons.js component', () => {
+  it('should render message if there are no favorite pokemons', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const message = getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
