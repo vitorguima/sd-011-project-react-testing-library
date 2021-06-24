d4915dcea99a605c6a995a9c275ff939429d06c0
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-24 15:33:22 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente FavoritePokemons', () => {
+  it('Verifica se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const favoritePokemonsButton = getByText('Favorite Pokémons');
+    fireEvent.click(favoritePokemonsButton);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  it('Verifica se é exibido na tela os pokemons favoritados', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText('More details'));
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    fireEvent.click(getByText('Home'));
+    fireEvent.click(getByText('Bug'));
+    fireEvent.click(getByText('More details'));
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    fireEvent.click(getByText('Favorite Pokémons'));
+    expect(getByText('Pikachu')).toBeInTheDocument();
+    expect(getByText('Caterpie')).toBeInTheDocument();
+  });
+});
