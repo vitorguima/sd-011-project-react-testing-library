7947e4afc85d68ba022eb22603da90a8b6d08526
Tulio Sploradori <tuliospl@gmail.com>
2021-06-24 16:35:50 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,24 @@
-// import { getByText } from '@testing-library/dom';
-// import App from '../App';
-// import React from 'react'
-// import RenderWithRouter from '../components/RenderWithRouter'
+import { fireEvent, getByText } from '@testing-library/dom';
+import React from 'react';
+import App from '../App';
+import RenderWithRouter from '../components/RenderWithRouter';
 
-// describe('testa os elementos da aplicação Favorites Pokémons', () => {
-//   it('mensagem no favorite pokemon found', () => {
-//     const { container, history, getByText } = RenderWithRouter(<App />)
-//     history.push('/favorites/');
-//     const noMatch = getByText(/No favorite pokemon found/i)
-//     expect(container.querySelector('favorite-pokemons').lenght === 0).toBeInTheDocument(noMatch);
-//   })
-// });
+describe('testa os elementos da aplicação Favorites Pokémons', () => {
+  it('mensagem no favorite pokemon found', () => {
+    RenderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const noMatch = getByText(/No favorite pokemon found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+  it('e exibido todos os cards favoritados', () => {
+    const { container } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    fireEvent.click(getByText(/Pokémon favoritado?/i));
+    fireEvent.click(getByText(/Home/i));
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    fireEvent.click(getByText(/More details/i));
+    fireEvent.click(getByText(/Pokémon favoritado?/i));
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    expect(container.querySelectorAll('.favorite-pokemon')).toHaveLength(2);
+  });
+});
