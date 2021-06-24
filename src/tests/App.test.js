43d93ea78268ee0a5d1c1a0809979fe80503e1fb
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-06-24 14:39:01 -0300

    Adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,30 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Test the operation of navigation links', () => {
+  it('Check if path="/" directs do Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/));
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+    expect(getByText(/Encountered pokémons/)).toBeInTheDocument();
+  });
+
+  it('Check if path="/about/ directs do About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/));
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+    expect(getByText(/About Pokédex/)).toBeInTheDocument();
+  });
+
+  it('Check if path="/favorites" directs to Favorite Pokémons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/));
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+    expect(getByText(/Favorite pokémons/)).toBeInTheDocument();
+  });
 });
