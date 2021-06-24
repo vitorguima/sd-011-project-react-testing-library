c14685804c6fef07cfae494b54b12a2cca32aded
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-24 15:58:39 -0300

    [filizzola-t-l] =  questão 4 not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('', () => {
+  const { container } = render(<NotFound />);
+  const getAbout = container.querySelector('h2');
+  expect(getAbout.textContent).toBe('Page requested not found 😭');
+
+  const getImg = container.querySelector('.not-found-image');
+  expect(getImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
