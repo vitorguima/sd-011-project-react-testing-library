061b06565b322740a4cd881d64ee6f6d5ce80bff
jamneto <netomoura22@gmail.com>
2021-06-24 17:27:24 -0300

    cumprimento do requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('render the text \'No favorite pokemon found\' with no favs', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  expect(localStorage.length).toBe(0);
+  const text = getByText(/No favorite pokemon found/);
+  expect(text).toBeInTheDocument();
+});
+// test('show all favorited pokemon', () => {
+
+//   const { getByText } = renderWithRouter(<FavoritePokemons />);
+//   const pikaText = getByText(/Pikachu/);
+//   expect(pikaText).toBeInTheDocument();
+
+// });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('renders a reading with the text `Page requested not found 😭` in a <h2>', () => {
+  const { getByText, getByRole } = renderWithRouter(<NotFound />);
+  const h2 = getByRole('heading', { level: 2 });
+  expect(h2).toBeInTheDocument();
+  const text = getByText(/Page requested not found/);
+  expect(text).toBeInTheDocument();
+});
+
+test('renders a specified image', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const image = getByAltText(/Pikachu crying because the page requested was not found/);
+  const { src } = image;
+  expect(src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
