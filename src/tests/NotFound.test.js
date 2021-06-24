8daddd64a0fcb1ecf6b2b7a610f306404faaa806
heyynat <natalifplima@gmail.com>
2021-06-24 18:30:33 -0400

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+test('Teste se página contém um heading com o texto Page requested not found 😭', () => {
+  const { getByRole, getByText } = renderWithRouter(<NotFound />);
+
+  expect(getByRole('heading')).toBeInTheDocument();
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+  expect(getByText('😭')).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const image = container.querySelector('.not-found-image');
+
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
