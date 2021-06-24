f3241160c5a1ad6bdc461d37aba45174efacdf3a
Renan <renan.vamo@gmail.com>
2021-06-24 19:21:59 -0300

    Adiciona os testes do req02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('teste', () => {});
+import React from 'react';
+import { renderWithRouter } from '../components';
+import About from '../components/About';
+
+describe('testing the \'About\' component', () => {
+  it('tests if render informations about Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const textAbout = getByText('About Pokédex');
+    expect(textAbout).toBeInTheDocument();
+  });
+
+  it('tests if contain a heading <h2> \'About Pokédex \'', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const heading = getByText('About Pokédex');
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('tests if the page has 2 paragraphs', () => {
+    renderWithRouter(<About />);
+    const elementP = document.querySelectorAll('p');
+    expect(elementP).toHaveLength(2);
+  });
+
+  it('test if the elemnt <img> has a specific source', () => {
+    renderWithRouter(<About />);
+    const imgElement = document.getElementsByTagName('img');
+    expect(imgElement[0].src).toBe(
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+// import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+// import { renderWithRouter } from '../components';
+// import App from '../App';
+
+describe('', () => {
+  it('teste', () => {
+
+  });
+});
