86d39f994099b6820d7ab62db32133c2fbe7a9bb
Diogo Augusto <diogotrescastro@gmail.com>
2021-06-24 17:15:43 -0300

    Resoluçao do requisito 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+// test('', () => {});
+
+it('Testa se página contém um heading h2 com o texto `Page requested not found`', () => {
+  const { container } = render(<NotFound />);
+  const titleH2 = container.querySelector('h2');
+  expect(titleH2).toHaveTextContent(/Page requested not found 😭/);
+});
+
+it('Testa se a página contém a seguinte imagem de uma Pokédex', () => {
+  const { container } = render(<NotFound />);
+  const img = container.querySelector('img');
+  expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
