e74addb152c99da33a6f45128c97e52d98d9b9f5
Hugo Mouto <hmoutol@gmail.com>
2021-06-24 17:00:59 -0300

    feat: App test working

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+// import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Test if all the Home page is being exhibited correctly', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test(('check if nav bar has Home, About and Favorite Pokémons'), () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const Home = getByText(/Home/i);
+    const About = getByText(/About/i);
+    const Favorite = getByText(/Favorite Pokémons/i);
+    expect(Home && About && Favorite).toBeInTheDocument();
+  });
+
+  // test('', () => {})
 });
