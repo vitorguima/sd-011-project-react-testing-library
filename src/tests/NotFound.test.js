54dae2aa5ca38e65690644e222cfa214f4ffa5de
Alberto Candido <albertocandido98@gmail.com>
2021-06-24 17:09:53 -0300

    Finaliza requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('NotFound tests', () => {
+  test('Teste se pág contém um heading com texto Page requested not found 😭', () => {
+    const { container } = render(<NotFound />);
+    const title = container.querySelector('h2');
+    expect(title.textContent).toBe('Page requested not found 😭');
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { container } = render(<NotFound />);
+    const img = container.querySelector('img');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
