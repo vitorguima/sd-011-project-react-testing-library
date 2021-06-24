cfc4e4101450ee3326fd768b7106f55e16a86119
Marco Galindo <marco1408@gmail.com>
2021-06-24 15:53:13 -0300

    requisito 2 finalizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+test('A página contém um heading h2 com o texto About Pokédex', () => {
+  const { getByText } = render(<About />);
+  const header = getByText('About Pokédex');
+  expect(header).toBeInTheDocument();
+});
+
+test('A página contém a seguinte imagem de uma Pokédex', () => {
+  const { getByAltText } = render(<About />);
+  const pokedexImage = getByAltText('Pokédex');
+  expect(pokedexImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
+
+test('A página contém dois parágrafos com texto sobre a Pokédex', () => {
+  const { getAllByTestId } = render(<About />);
+  const pokedexParagraph = getAllByTestId('about-paragraph');
+  expect(pokedexParagraph.length).toBe(2);
+});
