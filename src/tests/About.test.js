6742ca9fbefcc8db6cc09a1ddb7a96fe3286b4d5
Tulio Sploradori <tuliospl@gmail.com>
2021-06-24 15:44:37 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
+
+describe('testa a aplicação na pagina About', () => {
+  it('contem informaçoes sobre a Pokedex', () => {
+    const { container, getByText, history } = renderWithRouter(<App />);
+    history.push('/about/');
+    const text = getByText(/About Pokédex/i);
+    expect(text).toBeInTheDocument();
+    expect(container.querySelector('h2')).toBeInTheDocument();
+    expect(container.querySelectorAll('p')).toHaveLength(2);
+    expect(container.querySelector('img')).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { getByText } from '@testing-library/dom';
+import React from 'react'
+import RenderWithRouter from '../components/RenderWithRouter'
+
+describe('testa os elementos da aplicação Favorites Pokémons', () => {
+  it('mensagem no favorite pokemon found', () => {
+    const { container, history } = RenderWithRouter(<App />)
+    history.push('/favorites/');
+    const noMatch = getByText(/No favorite pokemon found/i)
+    if (container.className('favorite-pokemons').lenght === 0) {
+      expect(noMatch).toBeInTheDocument();
+    }
+
+  })
+});
