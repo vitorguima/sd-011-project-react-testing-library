8eb9146ddb00e871b7083b4f6d4476f1f63c662c
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-06-24 14:43:06 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Check the behavior of the NotFound page', () => {
+  it('', () => {
+    const { getByText, getByAltText } = render(<NotFound />);
+
+    const imgAltText = 'Pikachu crying because the page requested was not found';
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const title = getByText(/Page requested not found/);
+    const notFoundImg = getByAltText(imgAltText);
+    expect(title).toBeInTheDocument();
+    expect(notFoundImg).toHaveAttribute('src', imgSrc);
+  });
+});
