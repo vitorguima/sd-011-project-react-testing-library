dce79a566e9bf6b90bc88854537b05d79233db91
Ghislaine <glenox79@gmail.com>
2021-06-24 17:05:48 -0300

    Requisitos App-test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testes do componente App', () => {
+  it('verifica se renderiza o texto `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica se a página Pokédex renderiza ao carregar url "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se possui o link de navegação Home', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/);
+    fireEvent.click(home);
+  });
+
+  it('Verifica se possui o link de navegação About', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const about = getByText(/About/);
+    fireEvent.click(about);
+  });
+
+  it('Verifica se possui o link de navegação Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const favorite = getByText(/Favorite Pokémons/);
+    fireEvent.click(favorite);
+  });
 });
