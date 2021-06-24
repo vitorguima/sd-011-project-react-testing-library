7d36618c585037dfa7ebf7e735c13d413a62d06a
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-24 14:42:57 -0300

    [filizzola-t-l] = começando primeira questão

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,20 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const title = getByText(/Encountered pokémons/i);
+  expect(title).toBeInTheDocument();
+  const home = getByText(/Home/i);
+  fireEvent.click(home);
+
+  const link = history.location.pathname;
+  expect(link).toBe('/');
+
+  const about = getByText(/About/i);
+  fireEvent.click(about);
+  const linkAbout = history.location.pathname;
+
+  expect(linkAbout).toBe('/about');
+});
