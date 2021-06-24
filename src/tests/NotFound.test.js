f695456973ba8833eeb37d79d8a535cb1273c386
Hamander <1hamander@gmail.com>
2021-06-24 18:49:44 -0300

    Requisito_4 Completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente Not-Found', () => {
+  it('Test if page contain h2 with text { Encountered pokémons }', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const headingTwo = getByRole('heading', { level: 2 });
+    expect(headingTwo.textContent).toBe('Page requested not found 😭');
+  });
+
+  it('Test if the component show image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const notFoundImg = getByAltText(/Pikachu crying because/);
+    expect(notFoundImg).toBeInTheDocument();
+  });
+});
