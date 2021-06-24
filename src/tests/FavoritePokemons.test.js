547ff9bb662192fcf42ca56bfa0b888ad93bae45
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-24 17:25:51 -0300

    Requisito 4 realizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se a página contém as informações sobre a Pokédex', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const pokedexText = getByText(/No favorite pokemon found/i);
+  expect(pokedexText).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se página contém um h2 com o texto Page requested not found 😭', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const notFoundText = container.querySelector('h2');
+  // https://stackoverflow.com/questions/58976251/checking-text-appears-inside-an-element-using-react-testing-library
+  expect(notFoundText).toHaveTextContent('Page requested not found 😭');
+});
+
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const notFoundImg = container.querySelector('img');
+  // https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+  expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
