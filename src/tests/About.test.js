6c9c18f24f8eb01e0cdf871a4333161dd28cacdd
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-24 15:14:39 -0300

    [filizzola-t-l] =  questão 2 About Finalizada

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,4 +9,8 @@ test('Teste se a página contém as informações sobre a Pokédex.', () => {
 
   const getP = container.querySelectorAll('p');
   expect(getP.length).toBe(2);
+
+  const getImg = container.querySelector('.pokedex-image');
+  expect(getImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+
 });
