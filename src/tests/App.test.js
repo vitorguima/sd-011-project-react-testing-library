59a908a5aded6b10d65113fb693ac35cf3479191
Tales Coelho <tales-coelho@live.com>
2021-06-24 15:00:55 -0300

    exercicio 1 terminado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,9 @@
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-Describe('Teste o componente <App.js />', () => {
+describe('Teste o componente <App.js />', () => {
   it('Verifica se a primeira página carregada é a URL /', () => {
     const { history } = renderWithRouter(<App />);
     const { pathname } = history.location;
@@ -14,5 +15,37 @@ Describe('Teste o componente <App.js />', () => {
     expect(getByText('Home')).toBeTruthy();
     expect(getByText('About')).toBeTruthy();
     expect(getByText('Favorite Pokémons')).toBeTruthy();
+  });
+
+  it('Verifica se ao Clicar em Home redireciona para / ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const homeLink = getByText('Home');
+    fireEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se ao Clicar em About redireciona para /About ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const aboutLink = getByText('About');
+    fireEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Verifica se ao Clicar em Favorite redireciona para /Favorite', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoriteLink = getByText('Favorite Pokémons');
+    fireEvent.click(favoriteLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Verifica se inserir uma URL errada redireciona para notFound', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/erro');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/erro');
+    expect(getByText(/Page requested not found/i));
   });
 });
