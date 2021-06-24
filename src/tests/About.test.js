e69850f82834874d1dafc14e4a0a12f2b4a5f376
renan <renan_menezes07@hotmail.com>
2021-06-24 18:25:09 -0300

    Resolução do requisito 2.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente <About />', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(getByText('About Pokédex')).toBeInTheDocument();
+  });
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(getByText(/This application simulates/)).toBeInTheDocument();
+    expect(getByText(/One can filter Pokémons/)).toBeInTheDocument();
+  });
+  it('Testa se a página contém uma imagem de uma Pokédex.', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    expect(getByAltText('Pokédex')).toBeInTheDocument();
+  });
+});
