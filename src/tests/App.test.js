83a36037dfff64c97982894ff4eaf8e2cb546e0b
Tulio Sploradori <tuliospl@gmail.com>
2021-06-24 14:26:22 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import RenderWithRouter from '../components/RenderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('testa a aplicação no elemento app.js', () => {
+  it('Pokédex é renderizada ao carregar a aplicação no caminho de URL "/"', () => {
+    const { getByTestId, history } = RenderWithRouter(<App />);
+    history.push('/');
+    const btn = getByTestId('next-pokemon');
+    expect(btn).toBeInTheDocument();
+  });
+  it('contem links de navegação, home, about, favorites na pagina inicial', () => {
+    const { getByRole } = RenderWithRouter(<App />);
+    const nav = getByRole('navigation');
+    expect(nav).toBeInTheDocument();
+    expect(nav).toContainHTML('Home', 'About', 'Favorite Pokémons');
+  });
+  it('renderiza a pagina home ao clicar em home`', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const Url = history.location.pathname;
+    expect(Url).toBe('/');
+  });
+  it('renderiza a pagina about ao clicar em about', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const Url = history.location.pathname;
+    expect(Url).toBe('/about');
+  });
+  it('renderiza a pagina Favorite Pokémons ao clicar Favorite Pokémons', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const Url = history.location.pathname;
+    expect(Url).toBe('/favorites');
+  });
+  it('renderiza a pagina notfoud ao entrar em um link desconhecido', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    history.push('/Xablau/');
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
 });
