be6044e278c5fcd9c24fe9835279476b5426eaf6
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-06-24 16:33:36 -0300

    Requisito 4 - Concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { getByText } = render(<NotFound />);
+    const home = getByText(/Page requested not found/);
+    expect(home).toBeInTheDocument();
+  });
+  it('se página mostra uma imagem', () => {
+    const { getByAltText } = render(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
