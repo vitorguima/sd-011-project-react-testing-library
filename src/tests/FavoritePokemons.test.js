5bbcd49f51f05209303578be7dc86ade03c641bb
Rodolfo <rodolforrcont@gmail.com>
2021-06-24 15:53:00 -0300

    FavoritePokemons testes

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+it('Teste o componente <FavoritePokemons.js /> renderiza sem favoritados', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const linkFavorites = getByText(/Favorite Pokémons/i);
+  fireEvent.click(linkFavorites);
+  const notFoundPokemon = getByText(/No favorite pokemon found/i);
+  expect(notFoundPokemon).toBeInTheDocument();
+});
+
+it('Teste o componente <FavoritePokemons.js /> renderiza com favoritados', () => {
+  const { getByText, container } = renderWithRouter(<App />);
+  const clickBug = getByText('Bug');
+  fireEvent.click(clickBug);
+  const linkMoreDetails = getByText(/More Details/i);
+  fireEvent.click(linkMoreDetails);
+  const labelFavoritePokemon = container.querySelector('#favorite');
+  fireEvent.click(labelFavoritePokemon);
+  const linkFavorites = getByText(/Favorite Pokémons/i);
+  fireEvent.click(linkFavorites);
+  expect(getByText('Caterpie')).toBeInTheDocument();
+});
