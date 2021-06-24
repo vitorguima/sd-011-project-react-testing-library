c6ec0bca145ad9d79f8caa222ca77e84f6270642
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-24 18:35:02 -0300

    [filizzola-t-l] =  questão 7 iniciada

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Verifica se tem o Summary renderizado', () => {
+  const { getByText, container } = renderWithRouter(<App />);
+  const details = getByText('More details');
+  fireEvent.click(details);
+  const getAbout = container.querySelectorAll('h2')[1];
+  expect(getAbout.textContent).toBe('Summary');
+});
+
+test('Verifica se tem o Game Locations of<Name> renderizado', () => {
+  const { getByText, container } = renderWithRouter(<App />);
+  const details = getByText('More details');
+  fireEvent.click(details);
+  const getAbout = container.querySelectorAll('h2')[2];
+  expect(getAbout.textContent).toBe('Game Locations of Pikachu');
+});
