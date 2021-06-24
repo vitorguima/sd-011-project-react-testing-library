8a87c1eed0f46e8ef7b2a6672dc610853b462f39
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-06-24 19:03:38 -0300

    requisito 2 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,4 +1,28 @@
 import React from 'react';
-import About from 
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
 
-test('', () => {});
+describe('Test the About Component', () => {
+  it('Test if the page contains informations abour Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const firstParagraph = getByText(/This application simulates a Pokédex/);
+    expect(firstParagraph).toBeInTheDocument();
+  });
+
+  it('Test if the page contains a heading H2 with the text "About Pokédex"', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const headingH2 = getByRole('heading', { level: 2 });
+    expect(headingH2).toHaveTextContent(/About Pokédex/);
+  });
+
+  it('Test if the page contains 2 paragraphs about Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(getByText(/This application simulates a Pokédex/)).toBeInTheDocument();
+    expect(getByText(/One can filter Pokémons by type/)).toBeInTheDocument();
+  });
+
+  it('Test if the page contains an image of a Pokédex', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    expect(getByAltText(/Pokédex/)).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
