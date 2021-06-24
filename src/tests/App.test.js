04f92dbdc83f5052d958cfdfc3122b25b3d162c6
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-24 15:11:15 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('1- Test <App.js /> component', () => {
+  it('Should render a Pokedex text', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const nav = getByText('Pokédex');
+
+    expect(nav).toBeInTheDocument();
+  });
+
+  it('Should render Home, About and Favorite Pokémons links', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favoritePokemons = getByText('Favorite Pokémons');
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  it('Should redirect on click', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favoritePokemons = getByText('Favorite Pokémons');
+
+    fireEvent.click(home);
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    fireEvent.click(about);
+    expect(getByText('About Pokédex')).toBeInTheDocument();
+    fireEvent.click(favoritePokemons);
+    expect(getByText('Favorite pokémons')).toBeInTheDocument();
+    history.push('/xablau');
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
 });
