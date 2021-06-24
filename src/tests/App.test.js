762774924d2652b85ecc5a71b219a4f2750e8f2a
Rodolfo <rodolforrcont@gmail.com>
2021-06-24 15:14:15 -0300

    App Test - renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -9,6 +10,27 @@ test('renders a reading with the text `Pokédex`', () => {
       <App />
     </MemoryRouter>,
   );
-  const heading = getByText(/Pokédex/i);
+  const heading = getByText(/Pokédex/);
   expect(heading).toBeInTheDocument();
+});
+
+it('Verificar se entra na página sobre e exibe o titulo', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const titlePage = getByText(/Encountered pokémons/i);
+  expect(titlePage).toBeInTheDocument();
+
+  const linkHome = getByText(/Home/i);
+  fireEvent.click(linkHome);
+  const url = history.location.pathname;
+  expect(url).toBe('/');
+
+  const linkAbout = getByText(/About/i);
+  fireEvent.click(linkAbout);
+  const urlAbout = history.location.pathname;
+  expect(urlAbout).toBe('/about');
+
+  const linkFavorites = getByText(/Favorite Pokémons/i);
+  fireEvent.click(linkFavorites);
+  const urlFavorites = history.location.pathname;
+  expect(urlFavorites).toBe('/favorites');
 });
