9f4afcedf5ca9ea9b5a7463c2156a32ea6ba8bad
Ghislaine <glenox79@gmail.com>
2021-06-24 19:51:38 -0300

    Requisito FavoritePokemons-test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente FavoritePokemons', () => {
+  it('Verifica se é exibido a mensagem "No favorite pokemon found"', () => {
+    const { getByText, history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/pagina/que-não-existe/');
+    const notFoundFavorite = getByText(/No favorite pokemon found/i);
+    expect(notFoundFavorite).toBeInTheDocument();
+  });
+
+  it('verifica se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    fireEvent.click(getByText(/Favorite pokémons/i));
+  });
+
+  it('Verifica se é exibido a mensagem "No favorite pokemon found"', () => {
+    const { getByText, history } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    history.push('/pagina/que-não-existe/');
+    const notFoundFavorite = getByText(/No favorite pokemon found/i);
+    expect(notFoundFavorite).toBeInTheDocument();
+  });
+
+});
