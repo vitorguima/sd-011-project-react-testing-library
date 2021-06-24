bd7f6b52756de4658286acb56c15b57970dba921
Rodolfo <rodolforrcont@gmail.com>
2021-06-24 16:00:57 -0300

    NotFound testes

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+it('Teste o componente <NotFound.js /> seu título e sua tag', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const titlePage = container.querySelector('h2');
+  expect(titlePage).toBeTruthy();
+  expect(titlePage.textContent).toBe('Page requested not found 😭');
+});
+
+it('Teste o componente <NotFound.js /> seu título e sua tag', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const imageSource = container.querySelector('.not-found-image');
+  expect(imageSource.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
