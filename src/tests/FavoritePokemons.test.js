f501e63cf0a5af213993dddd6e2753550c9218e5
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-06-24 19:30:21 -0300

    correcoes req 2 e 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
 
+test('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
+  const fvPokemons = [];
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ fvPokemons } />);
+  const favoritePokemonTest = getByText(/No favorite pokemon found/);
+  expect(favoritePokemonTest).toBeInTheDocument();
+});
 
-
+test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+  const { getAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+  const favoritePokemons = getAllByTestId('pokemon-name');
+  expect(favoritePokemons[0]).toHaveTextContent(/Pikachu/i);
+  expect(favoritePokemons[1]).toHaveTextContent(/Charmander/i);
+  expect(favoritePokemons[2]).toHaveTextContent(/Caterpie/i);
+  expect(favoritePokemons[3]).toHaveTextContent(/Ekans/i);
+});
