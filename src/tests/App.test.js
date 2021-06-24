08fbf34c9f40a89e5bfb6ff7a126f83df77f2115
Halysson2 <halyssonsena83@gmail.com>
2021-06-24 15:07:19 -0300

    iniciando o projeto. VAMOS COMTUDO

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
-
+// Vamos começar
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
