360b824d5ac5dfa4528e9688727b2f1c8e312863
Daniel <danielfdoficial@gmail.com>
2021-06-24 17:18:32 -0300

    requisito 3, Favorite Pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Testes relativos ao componente FavoritePokemons', () => {
+  it(
+    'Verifica se aparece uma mensagem caso a pessoa não tenha favoritado nenhum pokemon',
+    () => {
+      const { getByText } = render(
+        <MemoryRouter initialEntries={ ['/favorites'] }>
+          <FavoritePokemons />
+        </MemoryRouter>,
+      );
+      const msg = 'No favorite pokemon found';
+      expect(getByText(msg)).toBeInTheDocument();
+    },
+  );
+
+  it(
+    'Verifica se aparece um card de pokemon para cada pokemon favoritado',
+    () => {
+      const { getByText } = render(
+        <MemoryRouter initialEntries={ ['/favorites'] }>
+          <FavoritePokemons pokemons={ pokemons } />
+        </MemoryRouter>,
+      );
+      pokemons.forEach((pokemon) => {
+        const name = getByText(pokemon.name);
+        return expect(name).toBeInTheDocument();
+      });
+    },
+  );
+});
