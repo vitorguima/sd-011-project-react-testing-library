2ec2f08b24ca9cb5cad4393629bcd5bc36841c56
marlon307 <marlon-ramosb@hotmail.com>
2021-06-24 17:07:35 -0300

    Rq 1 update

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
@@ -14,5 +15,32 @@ describe('shows the Pokédex when the route is `/`.', () => {
     expect(getByText('Home')).toBeInTheDocument();
     expect(getByText('About')).toBeInTheDocument();
     expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  test('Teste se a aplicação é redirecionada para a página inicial, na URL /', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página de About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Teste se a aplicação é redirecionada para a pg de Pokémons Favoritados', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página Not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/notFound');
+    expect(getByText('Page requested not found')).toBeInTheDocument();
   });
 });
