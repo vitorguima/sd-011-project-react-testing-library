fcd730dc3fa449db9dee9dc48287d4b8523a8453
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-06-24 16:30:47 -0300

    Requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const favoritePokemonsLink = getByText(/Favorite Pokémons/i);
+  fireEvent.click(favoritePokemonsLink);
+  const noFavoritePokemons = getByText(/No favorite pokemon found/i);
+  expect(noFavoritePokemons).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+it('Teste se página contém um heading h2 com o texto Page requested not found 😭', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const h2 = container.querySelector('h2');
+  expect(h2).toHaveTextContent(/Page requested not found 😭/);
+});
+
+it('Teste se página mostra a imagem ', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const image = container.querySelector('img');
+  expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
