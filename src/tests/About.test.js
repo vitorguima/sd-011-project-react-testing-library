f91d9775a7c367af5096ac65463d9b085ac78586
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-24 15:08:52 -0300

    [filizzola-t-l] = começando questão 2 About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('Teste se a página contém as informações sobre a Pokédex.', () => {
+  const { container } = render(<About />);
+  const getAbout = container.querySelector('h2');
+  expect(getAbout.textContent).toBe('About Pokédex');
+});
