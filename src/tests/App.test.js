198af50012e0973bb4adeb0f241a5b8631d72f62
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-06-24 17:02:13 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../RenderWithRouter';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Verifica requisito 1', () => {
+  it('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('Verifica se o componente app tem link Home', () => {
+    renderWithRouter(<App />);
+    const titulo = screen.getByText(/Home/i);
+    expect(titulo).toBeInTheDocument();
+  });
+
+  it('Verifica se o componente app tem link About', () => {
+    renderWithRouter(<App />);
+    const sobre = screen.getByText(/About/i);
+    expect(sobre).toBeInTheDocument();
+  });
+
+  it('Verifica se o componente app tem link home', () => {
+    renderWithRouter(<App />);
+    const favorito = screen.getByText(/Favorite Pokémons/i);
+    expect(favorito).toBeInTheDocument();
+  });
+
+  it('Deve renderizar a ErrorPage caso seja uma rota não existente', () => {
+    const { history, container } = renderWithRouter(<App />);
+    const rotaErrada = '/NotFound';
+    history.push(rotaErrada);
+
+    expect(container.innerHTML).toMatch(/Not Found/i);
+  });
 });
