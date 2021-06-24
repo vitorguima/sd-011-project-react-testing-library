96e1498ed971aa7958b6248acb7364a693a82987
Diego Ribeiro <dihrm2001@gmail.com>
2021-06-24 16:12:37 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+it('Renderiza o título `Page requested not found`', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const title = getByText(/Page requested not found/i);
+  expect(title).toBeInTheDocument();
+});
+
+it('Renderiza a imagem apropriada', () => {
+  renderWithRouter(<NotFound />);
+  const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const image = document.querySelector('img');
+  expect(image.src).toBe(url);
+});
