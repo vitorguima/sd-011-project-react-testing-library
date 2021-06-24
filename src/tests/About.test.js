ded77851ecfd5ed6555a81838c7dfd4bd83e9272
Gisele Costa <jikinha@gmail.com>
2021-06-24 18:01:49 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe(' Teste o componente <About.js />', () => {
+  it('se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByText } = render(<About />);
+    const header = getByText('About Pokédex');
+    expect(header).toBeInTheDocument();
+  });
+
+  it('se a página contém a seguinte imagem de uma Pokédex', () => {
+    const { getByAltText } = render(<About />);
+    const pokedexImage = getByAltText('Pokédex');
+    expect(pokedexImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
