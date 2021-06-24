0e438cefcded615876310911a4bc16441695ec01
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-24 16:18:56 -0300

    [filizzola-t-l] =  questão 5 initial commit

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import App from '../App';
+import Data from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+const namePokemons = Data.map(({ name }) => name);
+
+test('Testando componente Pokedex.js', () => {
+  const { container, getByText } = renderWithRouter(<App />);
+  const title = container.querySelector('h2');
+  expect(title.textContent).toBe('Encountered pokémons');
+
+  const Button = getByText(/Próximo pokémon/);
+  expect(Button).toBeInTheDocument();
+
+  namePokemons.forEach((pokemon) => {
+    expect(getByText(pokemon)).toBeInTheDocument();
+    fireEvent.click(Button);
+  });
+
+  expect(getByText('Pikachu')).toBeInTheDocument();
+});
