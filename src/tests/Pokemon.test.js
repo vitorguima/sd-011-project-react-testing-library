c64117de5a8ebf5438139ece33cd07755b88fcaf
VictorS <czpask@gmail.com>
2021-06-24 18:54:10 -0300

    correcao lint Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
-import pokemons from '../data';
 import Pokemon from '../components/Pokemon';
 
 describe('testes do componente Pokemon.js', () => {
