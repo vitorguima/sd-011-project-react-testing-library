22d825aeda77a5595c793c13d6363718f1b6f6ba
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-06-24 18:50:50 -0300

    cria os testes para o componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('test the Component <NotFound />', () => {
+  it('has a heading h2 with the text "Page requested not found 😭"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const aboutTitle = getByRole('heading', { level: 2 });
+    expect(aboutTitle).toBeInTheDocument();
+    expect(aboutTitle).toHaveTextContent('Page requested not found 😭');
+  });
+
+  it('renders the image of a pikachu crying with the alt text /Pikachu crying/', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const notFoundImage = getByAltText(/Pikachu crying/);
+    expect(notFoundImage).toBeInTheDocument();
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(notFoundImage.src).toBe(src);
+  });
+});
