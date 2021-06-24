ed8ca4d54817e399c65539e1b083d13b592d8e85
OldRoge <rogeriomrjr@icloud.com>
2021-06-24 19:18:23 -0300

    Done requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando funcionalidades do componente Favoritos', () => {
+  it('Testa se componente <favoritePokemons /> renderiza caso não encontrados', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const favoritesLink = getByText(/Favorite Pokémons/i);
+    fireEvent.click(favoritesLink);
+    const notFound = getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Testa se o componente <favoritePokemons /> renderiza favoritos', () => {
+    const { getByText, container } = renderWithRouter(<App />);
+    const clickFire = getByText('Fire');
+    const moreDetails = getByText(/More Details/i);
+    const favoritesAll = getByText(/Favorite Pokémons/i);
+
+    fireEvent.click(clickFire);
+    fireEvent.click(moreDetails);
+
+    const favoritePokemon = container.querySelector('#favorite');
+    fireEvent.click(favoritePokemon);
+    fireEvent.click(favoritesAll);
+    expect(getByText('Charmander')).toBeInTheDocument();
+  });
+});
