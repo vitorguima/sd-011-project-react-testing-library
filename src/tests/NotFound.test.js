21f873316a681e52f2b0b5e37be71b61683f5753
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-24 15:38:27 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testando o componente <NotFound.js />', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const h2Title = container.querySelector('h2');
+  const img = container.querySelector('img');
+  expect(h2Title.textContent).toBe('Page requested not found 😭');
+  expect(img.src).toBe(imgURL);
+});
