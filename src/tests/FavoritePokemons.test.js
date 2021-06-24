a7d774954b8a8ef59c67e2f1e5f0ed9b584cb175
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-06-24 20:12:38 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,12 +5,49 @@ import { FavoritePokemons } from '../components';
 
 describe('Verifica requisito 3', () => {
   it('Teste se a pessoa não tiver pokémons favoritos..', () => {
-    renderWithRouter(<FavoritePokemons />);
+    const NO_FAVORITE = '';
+    renderWithRouter(
+      <FavoritePokemons
+        pokemons={ NO_FAVORITE }
+      />,
+    );
     const texto = screen.getByText(/No favorite pokemon found/i);
     expect(texto).toBeInTheDocument();
   });
 
   it('Teste se é exibido todos os cards de pokémons favoritados..', () => {
-    renderWithRouter(<FavoritePokemons />);
+    const FAVORITO = [{
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+      summary:
+      'This intelligent Pokémon roasts hard berries',
+    },
+    ];
+
+    const { getByTestId } = renderWithRouter(
+      <FavoritePokemons
+        pokemons={ FAVORITO }
+      />,
+    );
+    const render = getByTestId('renderiza-favorito');
+    expect(render).toBeInTheDocument();
+    expect(render).toHaveTextContent(/Pikachu/i);
   });
 });
