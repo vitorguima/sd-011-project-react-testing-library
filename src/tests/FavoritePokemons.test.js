ba739e48819805751ef3bc090a1ea61fe682c94c
Jessica Arruda <sicarruda@gmail.com>
2021-06-24 19:30:11 -0300

    Iniciando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import About from '../components/About';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('', () => {
+
+  });
+});
