fed8baf6b9de05b466baca8d93c7689c9535a9a0
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-06-24 17:46:08 -0300

    Requisito 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+it('Teste se página contém um heading h2', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+
+  const headingTwo = getByRole('heading', { level: 2 });
+  expect(headingTwo.textContent).toBe('Page requested not found 😭');
+});
+
+it('Teste se página mostra a imagem ', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const image = getAllByRole('img');
+  expect(image[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
