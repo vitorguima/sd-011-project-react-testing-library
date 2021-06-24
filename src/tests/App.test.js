135488779f71446c1162738831e93051935d6cee
Matheus Gaspar <msouzagaspar@gmail.com>
2021-06-24 16:50:00 -0300

    verifica se os links no topo da página são renderizados

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,29 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App.js component', () => {
+  it('should render a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should render a set of navigation links in the top of the page', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const homeLink = getByText(/Home/i);
+    const aboutLink = getByText(/About/i);
+    const favoritePokemonsLink = getByText(/Favorite Pokémons/i);
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
 });
