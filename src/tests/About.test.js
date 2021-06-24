5206978cd4aadef79b78fa0c85b9f299287cb4a3
Tales Coelho <tales-coelho@live.com>
2021-06-24 15:16:08 -0300

    exercicio 2 terminado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <About.js />', () => {
+  it('Teste se a página contém as informações sobre a Pokédex. e 2 paragrafos', () => {
+    const { container } = renderWithRouter(<About />);
+    const pPokedex = container.querySelectorAll('p');
+    expect(pPokedex.length).toBe(2);
+    expect(pPokedex[0].textContent).toContain('This application simulates a Pokédex');
+    expect(pPokedex[1].textContent).toContain('One can filter Pokémons by type');
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { container } = renderWithRouter(<About />);
+    const titlePokedex = container.querySelector('h2');
+    expect(titlePokedex.textContent).toContain('About Pokédex');
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex.', () => {
+    const { container } = renderWithRouter(<About />);
+    const imageURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imagePokedex = container.querySelector('img');
+    expect(imagePokedex.src).toContain(imageURL);
+  });
+});
