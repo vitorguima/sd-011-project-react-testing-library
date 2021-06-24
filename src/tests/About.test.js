770be95ce562f0b290431a9fda68da2db016c4fa
Leandro Lopes <leandro_carlos@uol.com.br>
2021-06-24 18:13:16 -0300

    item 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Teste o componente About', () => {
+  test('Teste se a página contém um heading h2', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const pokedex = getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+
+    expect(pokedex).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    const p1 = getByText(/This application simulates a Pokédex/);
+    const p2 = getByText(/One can filter Pokémons by type/);
+
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém imagem de uma Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const img = getByRole('img');
+
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
