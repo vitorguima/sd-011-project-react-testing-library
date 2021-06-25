3ddfa4a20c960e86fc7a81daa11982cb9f975dd7
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-06-25 10:52:18 -0300

    Adiciona o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  test('A page contém o título "Page requested not found 😭"', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const NotFoundPage = getByText(/Page requested not found/i);
+    expect(NotFoundPage).toBeInTheDocument();
+  });
+
+  it('A página mostra a imagem do Pika chorando?', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
