64e4e465ac854f0bd998d56daeec1d2ed82043ec
marcoswarmling <marcoswarmling15@gmail.com>
2021-06-25 18:23:18 -0300

    requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente Pokemon ', () => {
+  it('Testa se é renderizado um card com as informações de determinado pokémon', () => {
+    const { getByText, getByTestId, getByAltText, history } = renderWithRouter(<App />);
+    const buttonNormal = getByText(/Normal/);
+    fireEvent.click(buttonNormal);
+    const pokeName = getByTestId('pokemon-name');
+    expect(pokeName.innerHTML).toBe('Snorlax');
+    const pokeType = getByTestId('pokemon-type');
+    expect(pokeType.innerHTML).toBe('Normal');
+    const pokeWeight = getByTestId('pokemon-weight');
+    expect(pokeWeight.innerHTML).toBe('Average weight: 460.0 kg');
+    const pokeImage = getByAltText('Snorlax sprite');
+    expect(pokeImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png');
+    const buttonD = getByText('More details');
+    fireEvent.click(buttonD);
+    const url = history.location.pathname;
+    expect(url).toBe('/pokemons/143');
+  });
+
+  it('Testa se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { getByText, getByRole, getByAltText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const checkbox = getByRole('checkbox');
+    fireEvent.click(checkbox);
+    const buttonHome = getByText('Home');
+    fireEvent.click(buttonHome);
+    const star = getByAltText('Pikachu is marked as favorite');
+    expect(star).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
