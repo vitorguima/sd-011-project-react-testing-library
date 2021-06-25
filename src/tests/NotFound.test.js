49ff43425cf82201252ff9148b92d94c42f537e1
Ghislaine <glenox79@gmail.com>
2021-06-25 10:24:52 -0300

    Requisito 04 Not Found.test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa componente Not Found', () => {
+  it('Verifica se contem heading h2 com o texto Page requested not found 😭', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const headingh2 = getByText(/Page requested not found/i);
+    expect(headingh2).toBeInTheDocument();
+  });
+
+  it('Verifica se página mostra determinada imagem', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const imgGifPikachu = getByAltText(/Pikachu/i);
+    expect(imgGifPikachu).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
