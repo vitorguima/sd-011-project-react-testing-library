9aa0c6d229d60438072fa5c08ce97f3539f846c3
Renan <renan.vamo@gmail.com>
2021-06-25 17:33:11 -0300

    Adiciona o req 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,10 +1,19 @@
-// import React from 'react';
-// import { fireEvent } from '@testing-library/react';
-// import { renderWithRouter } from '../components';
-// import App from '../App';
+import React from 'react';
+import { renderWithRouter } from '../components';
+import App from '../App';
 
-describe('', () => {
-  it('teste', () => {
+describe('testing \'NotFound\' component', () => {
+  it('tests if the page has the heading \'Page requested not found \'', () => {
+    const { getByAltText, getByText, history } = renderWithRouter(<App />);
 
+    history.push('/unkown/url');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/unkown/url');
+
+    const notFountMsg = getByText('Page requested not found');
+    expect(notFountMsg).toBeInTheDocument();
+
+    const imgSrc = getByAltText(/pikachu crying/i).src;
+    expect(imgSrc).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
