532ee8419cd6d987e3ad70b84bae0822ccaa6454
Renato Bispo <renatoslvbispo@gmail.com>
2021-06-25 15:16:25 -0300

    Finish requirement 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import pokemons from '../data';
 import { renderWithRouter } from '../helpers';
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js:', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  it('The page must contain the 404 message.', () => {
+    const notFoundMessage = /Page requested not found Crying emoji/i;
+    const notFoundHeading = screen.getByRole('heading', {
+      name: notFoundMessage,
+    });
+
+    expect(notFoundHeading).toBeInTheDocument();
+  });
+
+  it('The page must contain the 404 image.', () => {
+    const notFoundImage = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    const notFoundImageSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(notFoundImage).toBeInTheDocument();
+    expect(notFoundImage.src).toBe(notFoundImageSrc);
+  });
+});
