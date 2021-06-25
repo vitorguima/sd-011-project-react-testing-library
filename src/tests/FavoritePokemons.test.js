b02f934097b57142e6a90a7987be742cd7f56260
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-06-25 14:17:35 -0300

    requisito 3 incompleto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
-import { fireEvent } from '@testing-library/dom';
 
 describe('Test the FavoritePokemons Component', () => {
   it('Test if the message "No favorite pokemon found" is shown in the screen', () => {
@@ -10,25 +10,26 @@ describe('Test the FavoritePokemons Component', () => {
     expect(getByText(/No favorite pokemon found/)).toBeInTheDocument();
   });
 
-  // it('Test if all favorited pokemons cards are shown', () => {
-  //   const { getByRole, history } = renderWithRouter(<App />);
+  it('Test if all favorited pokemons cards are shown', () => {
+    const { getByRole, getByAltText, history } = renderWithRouter(<App />);
 
-  //   const detailsLink = getByRole('link', { name: /More details/ });
-  //   fireEvent.click(detailsLink);
-  //   const { pathname } = history.location;
-  //   console.log(history.location);
-  //   expect(pathname).toBe('/pokemons/25');
+    const detailsLink = getByRole('link', { name: /More details/ });
+    fireEvent.click(detailsLink);
+    const detailsURL = history.location.pathname;
+    expect(detailsURL).toBe('/pokemons/25');
 
-  //   const favoriteInput = getByRole('checkbox');
-  //   expect(favoriteInput).not.toBeChecked();
-  //   fireEvent.click(favoriteInput);
-  //   expect(favoriteInput).toBeChecked();
+    const favoriteInput = getByRole('checkbox');
+    expect(favoriteInput).not.toBeChecked();
+    fireEvent.click(favoriteInput);
+    expect(favoriteInput).toBeChecked();
 
-  //   const favoriteLink = getByRole('link', { name: /Favorite Pokémons/ });
-  //   fireEvent.click(favoriteLink);
-  //   history.replace('/favorites', location);
-  //   expect(pathname).toBe('/favorites');
-  // });
+    const favoriteLink = getByRole('link', { name: /Favorite Pokémons/ });
+    fireEvent.click(favoriteLink);
+    const favoritesURL = history.location.pathname;
+    expect(favoritesURL).toBe('/favorites');
+    const starImage = getByAltText(/is marked as favorite/i);
+    expect(starImage).toHaveAttribute('src', '/star-icon.svg');
+  });
 
   it('Test if the message "No favorite pokemon found" is shown in the screen', () => {
     const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
