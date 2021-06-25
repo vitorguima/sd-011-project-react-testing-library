10a9f82c5ee7bd5653e61bf8a260f2a406ddc2a3
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-25 13:51:44 -0300

    [filizzola-testing-library] - finalizando 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
+import data from '../data';
+
+const locationPikachu = data.map(({ foundAt }) => foundAt)[0]
+  .map(({ location }) => location);
 
 const detail = 'More details';
 
@@ -34,4 +38,22 @@ test('Verifica se details não é renderizado no pagina details', () => {
   const details = getByText('More details');
   fireEvent.click(details);
   expect(details).not.toBeInTheDocument();
+});
+
+test('testando a locarização pokemon', () => {
+  const { getByText, container } = renderWithRouter(<App />);
+  const details = getByText(detail);
+  fireEvent.click(details);
+  const imgPokemon = container.querySelectorAll('img');
+  // console.log(imgPokemon[2]);
+  const altP = 'Pikachu location';
+  expect(imgPokemon[1].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  expect(imgPokemon[1].alt).toBe(altP);
+  expect(imgPokemon[2].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  expect(imgPokemon[2].alt).toBe(altP);
+  console.log(locationPikachu[1]);
+  const kanto = 'Kanto Viridian Forest';
+  const power = 'Kanto Power Plant';
+  expect(getByText(kanto)).toBeInTheDocument();
+  expect(getByText(power)).toBeInTheDocument();
 });
