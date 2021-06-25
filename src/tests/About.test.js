5985ae215580ca80b8f69cb141306dba2d9ba5fa
Thiago Gomes <thiagog.macedom@gmail.com>
2021-06-25 16:02:09 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+// import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testas os elementos do componente about, 2 requisito.', () => {
+  it('Testa se a página contém informacões sobre a pokedex', () => {
+    const { getByText } = render(<About />);
+    const text = /This application simulates/i;
+
+    expect(getByText(text)).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = render(<About />);
+    const heading = getByRole('heading');
+
+    expect(heading.innerHTML).toBe('About Pokédex');
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { getAllByText } = render(<About />);
+    const paragraphs = getAllByText(/Pokémons/);
+
+    expect(paragraphs.length).toBe(2);
+  });
+
+  it('Verifiá se a página possui a imagem especificada', () => {
+    const { getByRole } = render(<About />);
+    const image = getByRole('img');
+
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
