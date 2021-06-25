84d310b5abb1d7040cb085fb68f06218bc9b7d8a
IsleneGomes <islenegomes@gmail.com>
2021-06-25 16:21:47 -0300

    Testando na malandragem...

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const favorites = getByText('No favorite pokemon found');
+    expect(favorites).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favorites = getByText('Favorite Pokémons');
+    fireEvent.click(favorites);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  // test('Deve exibir na tela a mensagem No favorite pokemon found', () => {
+  //   const { getByText, history } = renderWithRouter(<FavoritePokemons />);
+  //   history.push('/pagina/que-nao-existe/');
+  //   const noMatch = getByText(/not found/i);
+  //   expect(noMatch).toBeInTheDocument();
+  // });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import NotFound from '../components/NotFound';
 
-describe('Teste o componente <About.js />', () => {
-  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um h2 com o texto Page requested not found', () => {
     const { container } = render(<NotFound />);
     const tagh2 = container.querySelector('h2');
     expect(tagh2.textContent).toBe('Page requested not found 😭');
   });
 
-  test('Teste se a página contém uma imagem da Pokédex já definida', () => {
+  test('Teste se página mostra a imagem pedida', () => {
     const { container } = render(<NotFound />);
     const img = container.querySelector('img');
     expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
