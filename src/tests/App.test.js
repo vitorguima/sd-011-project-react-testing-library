841aef2ed5d6394e6bcc53bdc0fd15bed9268e5e
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-25 02:10:09 -0300

    done first requirement

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,22 +2,43 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Test for the links of page home', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-test('shows the pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  test('shows the pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('Check if the first link contain the text Home', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const LinkHome = getByText(/Home/i);
+    expect(LinkHome).toBeInTheDocument();
+  });
+
+  test('Check if the second link contain the text About', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const LinkAbout = getByText(/About/i);
+    expect(LinkAbout).toBeInTheDocument();
+  });
+
+  test('Check if the third link contain the text Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const LinkFavorite = getByText(/Favorite Pokémons/i);
+    expect(LinkFavorite).toBeInTheDocument();
+  });
 });
