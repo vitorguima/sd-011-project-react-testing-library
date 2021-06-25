e51d2e633f149d7e4eaed11f93b575568a801840
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-25 07:37:30 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('2- Test <About.js /> component', () => {
+  it('Should About page contain infomation about Pokédex', () => {
+    const { getByText } = render(<About />);
+    const firstInfo = getByText(/This application simulates a Pokédex/i);
+    const secondInfo = getByText(/One can filter Pokémons by type/i);
+
+    expect(firstInfo).toBeInTheDocument();
+    expect(secondInfo).toBeInTheDocument();
+  });
+
+  it('Should have an <h2> with text About Pokédex', () => {
+    const { getByText } = render(<About />);
+    const title = getByText('About Pokédex');
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Should have an image', () => {
+    const { getByRole } = render(<About />);
+    const img = getByRole('img');
+
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
