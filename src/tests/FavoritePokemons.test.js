8bace352aadf036d0ae42e416ce36e737dfec22e
anaclarabck <ackyotoku@gmail.com>
2021-06-25 16:32:31 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,14 +1,28 @@
-// import React from 'react';
-// import renderWithRouter from './renderWithRouter';
-// import FavoritePokemons from '../components/FavoritePokemons';
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Teste componente <About.js />', () => {
-  it('Exibe na tela -No favorite pokemon found-, se não há pokémons favoritos', () => {
+  it('Exibe -No favorite pokemon found-, se não houver pokémons favoritos', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const titleNoFavorite = getByText(/No favorite pokemon found/);
+    expect(titleNoFavorite).toBeInTheDocument();
   });
 
   it('Exibe todos os cards de pokémons favoritados', () => {
-  });
+    const { container, getByText } = renderWithRouter(<App />);
+    const pikachuDetails = container.querySelector('[href="/pokemons/25"]');
+    fireEvent.click(pikachuDetails);
 
-  it('Nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+    const pikachuFavorite = container.querySelector('[for="favorite"]');
+    fireEvent.click(pikachuFavorite);
+
+    const favoritePokemons = container.querySelector('[href="/favorites"]');
+    fireEvent.click(favoritePokemons);
+
+    const pikachu = getByText(/Pikachu/);
+    expect(pikachu).toBeInTheDocument();
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,4 @@
-test('', () => {});
+// import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+// import App from '../App';
