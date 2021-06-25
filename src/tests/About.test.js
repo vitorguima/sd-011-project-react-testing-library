813359d1b8981bdf91ee9ae3badf370fb71b82ac
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-06-25 18:43:38 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <About.js />', () => {
+  it('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const headingText = getByRole('heading', { level: 2 });
+    expect(headingText.innerHTML).toBe('About Pokédex');
+  });
+
+  it('Verifica se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const firstText = ('This application simulates a Pokédex, '
+      + 'a digital encyclopedia containing all Pokémons');
+    const secondText = ('One can filter Pokémons by type, '
+      + 'and see more details for each one of them');
+    const firstParagraph = getByText(firstText);
+    const secondParagraph = getByText(secondText);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('Verifica se a página contém a seguinte imagem de uma Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const aboutImage = getByRole('img');
+    expect(aboutImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/'
+      + '8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,57 +4,59 @@ import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testa o componente <App.js />', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-it('Verifica se a página principal é renderizada ao carregar a app na URL /', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-  const url = history.location.pathname;
-  expect(url).toBe('/');
-  const home = getByText('Encountered pokémons');
-  expect(home).toBeInTheDocument();
-});
+  it('Verifica se a página principal é renderizada ao carregar a app na URL /', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const url = history.location.pathname;
+    expect(url).toBe('/');
+    const home = getByText('Encountered pokémons');
+    expect(home).toBeInTheDocument();
+  });
 
-it('Verifica se na app contém links fixos para: Home, About e Favorite', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const home = getByText('Home');
-  expect(home).toBeInTheDocument();
-  const about = getByText('About');
-  expect(about).toBeInTheDocument();
-  const favorite = getByText('Favorite Pokémons');
-  expect(favorite).toBeInTheDocument();
-});
+  it('Verifica se na app contém links fixos para: Home, About e Favorite', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    expect(home).toBeInTheDocument();
+    const about = getByText('About');
+    expect(about).toBeInTheDocument();
+    const favorite = getByText('Favorite Pokémons');
+    expect(favorite).toBeInTheDocument();
+  });
 
-it('Verifica se é redirecionada para URL / ao clicar no link Home', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-  const linkHome = getByText(/Home/i);
-  fireEvent.click(linkHome);
-  const urlHome = history.location.pathname;
-  expect(urlHome).toBe('/');
-});
+  it('Verifica se é redirecionada para URL / ao clicar no link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkHome = getByText(/Home/i);
+    fireEvent.click(linkHome);
+    const urlHome = history.location.pathname;
+    expect(urlHome).toBe('/');
+  });
 
-it('Verifica se é redirecionada para URL /about ao clicar no link About', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-  const linkAbout = getByText(/About/i);
-  fireEvent.click(linkAbout);
-  const url = history.location.pathname;
-  expect(url).toBe('/about');
-});
+  it('Verifica se é redirecionada para URL /about ao clicar no link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkAbout = getByText(/About/i);
+    fireEvent.click(linkAbout);
+    const url = history.location.pathname;
+    expect(url).toBe('/about');
+  });
 
-it('Verifica se é redirecionada para URL /favorites ao clicar no link Favorite', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-  const linkFavorites = getByText(/Favorite Pokémons/i);
-  fireEvent.click(linkFavorites);
-  const urlFavorites = history.location.pathname;
-  expect(urlFavorites).toBe('/favorites');
-});
+  it('Verifica se é redirecionada para URL /favorites ao clicar no link Favorite', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkFavorites = getByText(/Favorite Pokémons/i);
+    fireEvent.click(linkFavorites);
+    const urlFavorites = history.location.pathname;
+    expect(urlFavorites).toBe('/favorites');
+  });
 
-it('Verifica se redireciona para URL Not Found ao entrar em uma URL desconhecida', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-  history.push('/pagina/que-nao-existe');
-  const noMatch = getByText(/Page requested not found/i);
-  expect(noMatch).toBeInTheDocument();
+  it('Verifica se redireciona para URL Not Found ao entrar em uma URL desconhecida', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe');
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
 });
