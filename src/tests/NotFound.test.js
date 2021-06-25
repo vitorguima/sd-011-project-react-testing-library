c5a0bb100262bb01205cda385142ae2e72fa6bcf
gabrielfndz <gabriel.omercador@hotmail.com>
2021-06-25 18:06:50 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Infos da página notFound', () => {
+  const { container } = render(<NotFound />);
+  const h2Text = container.querySelector('h2');
+  expect(h2Text.textContent).toBe('Page requested not found 😭');
+
+  const imgPikachu = container.querySelector('img');
+  expect(imgPikachu.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
