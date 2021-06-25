c6045dbd2cab6092e30ceef6a9b607407f665fb1
Renan <renan.vamo@gmail.com>
2021-06-25 18:28:07 -0300

    Muda o arquivo renderWithRouter de pasta para evitar conflito com o stryker

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,10 +1,54 @@
-// import React from 'react';
-// import { fireEvent } from '@testing-library/react';
-// import { renderWithRouter } from '../components';
-// import App from '../App';
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import { renderWithRouter } from '../components';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data'
 
-describe('', () => {
-  it('teste', () => {
+describe('testing Pokedex component', () => {
+  it('verify if contain a heading h2 \'Encountered pokémons\'', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ {} }
+        isFavorite={ [] }
+      />,
+    );
+    const heading = getByText('Encountered pokémons');
+    expect(heading).toBeInTheDocument();
+  });
 
+  it('tests if the next pokemon renders when user clicks in \'Próximo pokémon\'', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ {} }
+        isFavorite={ [] }
+      />,
+    );
+    pokemons.forEach((pokemon) => {
+      const actualPokemonName = pokemon.name;
+      expect(actualPokemonName).toBeInTheDocument();
+
+      const nextBtn = getByText('Próximo pokémon');
+      fireEvent.click(nextBtn);
+    });
+    const nextBtn = getByText('Próximo pokémon');
+    fireEvent.click(nextBtn);
+
+    const actualPokemonName = pokemons[0].name;
+    expect(actualPokemonName).toBeInTheDocument();
+
+    const nextPokemon = getByText('Charmander');
+    expect(nextPokemon).toBeInTheDocument();
+  });
+
+  it('test if ', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ {} }
+        isFavorite={ [] }
+      />,
+    );
   });
 });
