19a676e173c9b69f6057c46322b6b1e4e425aaf3
IsleneGomes <islenegomes@gmail.com>
2021-06-24 21:31:40 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,46 +1,54 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testa o componente App.js', () => {
   test('renders a reading with the text `Pokédex`', () => {
-    const { getByText } = renderWithRouter(<App />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
 
   test('O primeiro link deve possuir o texto Home', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/Home/i));
+    const { getByRole, history } = renderWithRouter(<App />);
+    const textHome = getByRole('link', {
+      name: /Home/i,
+    });
+    fireEvent.click(textHome);
     const { pathname } = history.location;
     expect(pathname).toBe('/');
-    const home = getByText(/Home/i);
-    expect(home).toBeInTheDocument();
   });
 
   test('O primeiro link deve possuir o texto About', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
+    const { getByRole, history } = renderWithRouter(<App />);
+    const about = getByRole('link', {
+      name: /About/i,
+    });
+    fireEvent.click(about);
     const { pathname } = history.location;
     expect(pathname).toBe('/about');
-    const about = getByText(/About/i);
-    expect(about).toBeInTheDocument();
   });
 
   test('O primeiro link deve possuir o texto Favorite Pokémons', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
+    const { getByRole, history } = renderWithRouter(<App />);
+    const favorites = getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    fireEvent.click(favorites);
     const { pathname } = history.location;
     expect(pathname).toBe('/favorites');
-    const favorites = getByText(/Favorite Pokémons/i);
-    expect(favorites).toBeInTheDocument();
   });
 
   test('deve testar um caminho não existente e a renderização do Not Found', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    history.push('/Not Found/');
-    const noMatch = getByText(/Page requested not found/i);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = getByText(/not found/i);
     expect(noMatch).toBeInTheDocument();
   });
 });
