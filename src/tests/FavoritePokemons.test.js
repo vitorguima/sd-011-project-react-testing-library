51dc9da130a030d8f29455697ab764ae4c5dbce9
Víctor Faria <vitilevi@hotmail.com>
2021-06-25 11:21:02 -0300

    Feito requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Testa o componente Favorite Pokemons', () => {
   it('Testa se é exibido a mensagem No favorite pokemon found na tela', () => {
@@ -9,7 +11,21 @@ describe('Testa o componente Favorite Pokemons', () => {
     expect(text).toBeInTheDocument();
   });
 
-  it('Testa se nenhum pokemom está favoritado', () => {
-    
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    const { getByText, getByRole, container } = renderWithRouter(<App />);
+    const btnDetails = getByText(/more details/i);
+    fireEvent.click(btnDetails);
+    const checkBox = getByRole('checkbox');
+    fireEvent.click(checkBox);
+    const favButton = getByText(/favorite pokémons/i);
+    fireEvent.click(favButton);
+    const pokemon = container.getElementsByClassName('pokemon');
+    expect(pokemon.length).toBe(1);
+  });
+
+  it('Testa se nenhum card é exibido se nenhum pokémon for favoritado', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const text = getByText(/no favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
   });
 });
