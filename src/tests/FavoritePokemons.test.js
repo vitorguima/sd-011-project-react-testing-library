ee42506730708c3f32b8e2b6ee5557618945bf80
Luan Xavier <luantenorio3@gmail.com>
2021-06-25 11:21:58 -0300

    Requisito 3 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,30 @@
-// import React from 'react';
-// import FavoritePokemons from '../components/FavoritePokemons';
-// import renderWithRouter from '../renderWithRouter';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-// describe('Testes do componente <FavoritePokemons/>', () => {
-//   it('Testa se aparece mensagem de aviso quando não há pokemons favoritos', () => {
+describe('Testes do componente <FavoritePokemons/>', () => {
+  it('Testa se aparece mensagem de aviso quando não há pokemons favoritos', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const favPokemonsLink = getByText('Favorite Pokémons');
 
-//   });
-// });
+    userEvent.click(favPokemonsLink);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    userEvent.click(getByText('Normal'));
+    userEvent.click(getByText('More details'));
+    userEvent.click(getByText('Pokémon favoritado?'));
+    userEvent.click(getByText('Home'));
+    userEvent.click(getByText('Poison'));
+    userEvent.click(getByText('More details'));
+    userEvent.click(getByText('Pokémon favoritado?'));
+    userEvent.click(getByText('Favorite Pokémons'));
+
+    expect(getByText('Snorlax')).toBeInTheDocument();
+    expect(getByText('Ekans')).toBeInTheDocument();
+  });
+});
