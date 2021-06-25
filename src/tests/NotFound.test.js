b5c22ba9991e08db464f97b5b14a9ad6fc6e88e4
Thiago Gomes <thiagog.macedom@gmail.com>
2021-06-25 17:13:12 -0300

    requisito 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound, requisito 4', () => {
+  it('Testa se a página contém um heading com o texto específico', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+
+    const heading = getByText(/Page requested not found/);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem específica', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+
+    const imagems = getAllByRole('img');
+    expect(imagems[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
