9b0b05885a9dd7c1b73dae9352dd347640aa0cec
marlon307 <marlon-ramosb@hotmail.com>
2021-06-25 15:59:24 -0300

    Rq 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Teste o componente FavoritePokemons.js', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText, getAllByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/));
+    fireEvent.click(getByText(/Pokémon favoritado/));
+    fireEvent.click(getByText(/Home/));
+    fireEvent.click(getByText(/Bug/));
+    fireEvent.click(getByText(/More details/));
+    fireEvent.click(getByText(/Pokémon favoritado/));
+    fireEvent.click(getByText(/Favorite Pokémons/));
+    const getFavorite = getAllByTestId('pokemon-name');
+    expect(getFavorite.length).toBe(2);
+  });
+});
