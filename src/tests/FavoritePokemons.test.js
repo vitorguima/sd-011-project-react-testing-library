60f66bbbfb4b685fbfddf91cd0523634ff7663eb
renan <renan_menezes07@hotmail.com>
2021-06-25 16:20:46 -0300

    Resolução requisito 3.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,6 @@
 import { fireEvent } from '@testing-library/react';
 import React from 'react';
+import App from '../App';
 import FavoritePokemons from '../components/FavoritePokemons';
 // import PokemonDetails from '../components/PokemonDetails';
 import renderWithRouter from './renderWithRouter';
@@ -14,19 +15,19 @@ describe('Testa o componente <FavoritePokemons.js />', () => {
   });
 
   it('Testa se é exibido todos os cards de pokémons favoritados.', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />);
-    fireEvent.call(localStorage.removeItem('favoritePokemonIds'));
-    // const detailButton = getByText('More details');
-    // fireEvent.click(detailButton);
+    const { getByText, getByLabelText, queryByText } = renderWithRouter(<App />);
 
-    const favoriteCheckBox = getByText('Pokémon favoritado?');
+    const moreDetailsButton = getByText('More details');
+    fireEvent.click(moreDetailsButton);
+
+    const favoriteCheckBox = getByLabelText(/Pokémon favoritado/);
     fireEvent.click(favoriteCheckBox);
-    expect(favoriteCheckBox.value).toEqual(true);
+    console.log(favoriteCheckBox.value);
+    expect(favoriteCheckBox).toBeChecked(true);
 
     const favoriteNavButton = getByText('Favorite Pokémons');
     fireEvent.click(favoriteNavButton);
-
-    expect(getByText(/No favorite pokemon found/)).not.toBeInTheDocument();
+    expect(queryByText(/No favorite pokemon found/)).toBe(null);
   });
 
   it('Testa se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/react';
+import React from 'react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente <Pokedex.js />', () => {
+  it('Testa se página contém um heading h2 com o texto "Encountered pokémons".', () => {
+    const { getByText } = renderWithRouter(<Pokedex />);
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido o próximo Pokémon quando o Próximo pokémon é clicado.', () => {
+    const { getByTextId } = renderWithRouter(<Pokedex />);
+    const nextPokemonButton = getByTextId('next-pokemon');
+    const nextPokemon = jest.spyOn(Pokedex, 'nextPokemon');
+    nextPokemon(numberOfPokemons);
+    expect(Pokedex.state().pokemonIndex).toBe(0);
+    fireEvent.click(nextPokemonButton);
+    expect(Pokedex.state().pokemonIndex).toBe(1);
+  });
+
+  it('Testa se é mostrado apenas um Pokémon por vez.', () => {
+  });
+
+  it('Testa se a Pokédex tem os botões de filtro.', () => {
+  });
+
+  it('Testa se a Pokédex contém um botão para resetar o filtro.', () => {
+  });
+
+  it('Testa se é criado botão de filtro para cada tipo de Pokémon.', () => {
+  });
+
+  it('O botão de Próximo deve ser desabilitado quando a lista tiver um pokémon.', () => {
+  });
+});
