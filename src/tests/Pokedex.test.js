6fd6caf18bb48c7b87e841f50aad4a58357eea57
anaclarabck <ackyotoku@gmail.com>
2021-06-25 17:47:08 -0300

    Corrigido requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import Pokedex from '../components/Pokedex';
 
 const pokemonName = 'pokemon-name';
 
@@ -65,5 +66,65 @@ describe('Teste componente <Pokedex.js />', () => {
     fireEvent.click(btnAll);
     const pikachu = getByTestId(pokemonName);
     expect(pikachu.innerHTML).toBe('Pikachu');
+  });
+
+  it('Cria, dinamicamente, um botão de filtro para cada tipo de Pokémon', () => {
+    const pokemons = [{
+      id: 143,
+      name: 'Snorlax',
+      type: 'Normal',
+      averageWeight: {
+        value: '460.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Vermillion City',
+          map: 'https://cdn2.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
+        },
+      ],
+      summary: 'What sounds...',
+    },
+    {
+      id: 148,
+      name: 'Dragonair',
+      type: 'Dragon',
+      averageWeight: {
+        value: '16.5',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Johto Route 45',
+          map: 'https://cdn2.bulbagarden.net/upload/2/21/Johto_Route_45_Map.png',
+        },
+        {
+          location: 'Johto Dragon\'s Den',
+          map: 'https://cdn2.bulbagarden.net/upload/1/1e/Johto_Dragons_Den_Map.png',
+        },
+      ],
+      summary: 'They say that if...',
+    }];
+
+    const isPokemonFavoriteById = { 143: false, 148: false };
+    const pokedex = (
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />
+    );
+    const { getByText, getByRole, getAllByTestId } = renderWithRouter(pokedex);
+    const snorlax = getByText(/Snorlax/);
+    expect(snorlax).toBeInTheDocument();
+
+    const typeNormal = getByRole('button', { name: 'Normal' });
+    expect(typeNormal).toBeInTheDocument();
+
+    const typeDragon = getByRole('button', { name: 'Dragon' });
+    expect(typeDragon).toBeInTheDocument();
+
+    const allButtons = getAllByTestId('pokemon-type-button');
+    expect(allButtons.length).toBe(2);
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,4 @@
-test('', () => {});
+// import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+// import Pokemon from '../components/Pokemon';
