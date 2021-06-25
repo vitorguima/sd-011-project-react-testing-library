9f3eeab0ed8f7239bfbccda8a6ca46e9b75347d1
Reginara Gomes <refagomes@gmail.com>
2021-06-25 19:30:31 -0300

    Adiciona requisito 2! Yrrul

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Teste o componente <About.js />', () => {
+  test('Testa se contém heading h2 com texto About Pokédex', () => {
+    const { container } = render(<About />);
+    const headingh2 = container.querySelector('h2');
+    expect(headingh2.textContent).toBe('About Pokédex');
+  });
+
+  test('Testa se contém 2 parágrafos com texto sobre a Pokédex', () => {
+    const { container } = render(<About />);
+    const paragraphs = container.querySelectorAll('p');
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('Testa se contém imagem da Pokédex', () => {
+    const { container } = render(<About />);
+    const image = container.querySelector('img');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
