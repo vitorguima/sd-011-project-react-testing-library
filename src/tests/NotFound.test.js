3f065fc07111b61378934b2847d4f86277989c35
IsleneGomes <islenegomes@gmail.com>
2021-06-24 23:01:49 -0300

    Requisito NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <About.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { container } = render(<NotFound />);
+    const tagh2 = container.querySelector('h2');
+    expect(tagh2.textContent).toBe('Page requested not found 😭');
+  });
+
+  test('Teste se a página contém uma imagem da Pokédex já definida', () => {
+    const { container } = render(<NotFound />);
+    const img = container.querySelector('img');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
