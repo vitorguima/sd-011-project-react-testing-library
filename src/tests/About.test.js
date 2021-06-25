19e0c3c6a1718ddabbaabfd434e3e9698d97bf6e
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-06-25 15:48:10 -0300

    Requirement 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testing Requirement 02 - Component About.js', () => {
+  it('Test if about page contains h2 with text <About Pokédex>', () => {
+    const { getByText, container } = render(<About />);
+    expect(getByText('About Pokédex'));
+    expect(container.getElementsByTagName('p')).toHaveLength(2);
+  });
+
+  it('Verify image link', () => {
+    const { getByAltText } = render(<About />);
+    const image = getByAltText('Pokédex');
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toBe(url);
+  });
+});
