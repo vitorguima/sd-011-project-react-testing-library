389eb3240b9e1bad5f2c393acc302e18e5e1d159
jamneto <netomoura22@gmail.com>
2021-06-25 16:54:10 -0300

    cumprimento do requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,82 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('renders a card with pokemon information', () => {
+  const { getByTestId, getAllByText, getByText, getByAltText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const pikachuName = getByTestId('pokemon-name');
+  expect(pikachuName).toBeInTheDocument();
+  const pikachuType = getByTestId('pokemon-type');
+  expect(pikachuType).toBeInTheDocument();
+  const pikachuWeight = getByTestId('pokemon-weight');
+  expect(pikachuWeight).toBeInTheDocument();
+  const pikachuName2 = getByText(/Pikachu/);
+  expect(pikachuName2).toBeInTheDocument();
+  const type1 = getAllByText(/Electric/i);
+  expect(type1.length).toBe(2);
+  const avgWeight = getByText(/Average weight: 6.0 kg/);
+  expect(avgWeight).toBeInTheDocument();
+  const img = getByAltText(/Pikachu sprite/);
+  const { src, alt } = img;
+  expect(src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  expect(alt).toMatch('Pikachu sprite');
+});
+
+test('tests the More Details link', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const linkDetails = getByText(/More details/);
+  expect(linkDetails).toHaveAttribute('href', '/pokemons/25');
+});
+
+test('goes to correct path - pokemon details', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkDetails = getByText(/More details/);
+  fireEvent.click(linkDetails);
+  const url = history.location.pathname;
+  expect(url).toBe('/pokemons/25');
+});
+
+test('have a star on favourite pokemons', () => {
+  const { getByText, getByAltText } = renderWithRouter(<App />);
+  const linkDetails = getByText(/More details/);
+  fireEvent.click(linkDetails);
+  const checkBox = getByText(/Pokémon favoritado/);
+  fireEvent.click(checkBox);
+  const linkHome = getByText(/Home/);
+  fireEvent.click(linkHome);
+  const star = getByAltText(/Pikachu is marked as favorite/);
+  expect(star).toBeInTheDocument();
+  const { src, alt } = star;
+  expect(src).toContain('/star-icon.svg');
+  expect(alt).toMatch('Pikachu is marked as favorite');
+});
+
+// test('renders the correct path and pokemon details', () => {
+//   const { getByText,
+//     getByTestId, getAllByText, getByAltText, history } = renderWithRouter(<App />);
+//   const linkDetails = getByText(/More details/);
+//   fireEvent.click(linkDetails);
+//   const url = history.location.pathname;
+//   expect(url).toBe('/pokemons/25');
+// const pikachuName = getByTestId('pokemon-name');
+// expect(pikachuName).toBeInTheDocument();
+// const pikachuType = getByTestId('pokemon-type');
+// expect(pikachuType).toBeInTheDocument();
+// const pikachuWeight = getByTestId('pokemon-weight');
+// expect(pikachuWeight).toBeInTheDocument();
+// const length = 2;
+// const type = getAllByText(/Electric/);
+// expect(type.length).toBe(length);
+// const avgWeight = getByText(/Average weight: 6.0 kg/);
+// expect(avgWeight).toBeInTheDocument();
+// const img = getByAltText(/Pikachu sprite/);
+// const { src, alt } = img;
+// expect(src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+// expect(alt).toMatch('Pikachu sprite');
+// });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,61 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('renders a card with pokemon information', () => {
+  const { getByTestId, getAllByText, getByText, getByAltText, getByRole } = render(
+    <MemoryRouter initialEntries={ ['/pokemons/25'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const pikachuName = getByTestId('pokemon-name');
+  expect(pikachuName).toBeInTheDocument();
+  // const pikachuType = getByTestId('pokemon-type');
+  // expect(pikachuType).toBeInTheDocument();
+  // const pikachuWeight = getByTestId('pokemon-weight');
+  // expect(pikachuWeight).toBeInTheDocument();
+  // const pikachuNameTimes = getAllByText(/Pikachu/i);
+  // const length = 3;
+  // expect(pikachuNameTimes.length).toBe(length);
+  // const type = getByText(/Electric/);
+  // expect(type).toBeInTheDocument();
+  // const avgWeight = getByText(/Average weight: 6.0 kg/);
+  // expect(avgWeight).toBeInTheDocument();
+  // const img = getByAltText(/Pikachu sprite/);
+  // const { src, alt } = img;
+  // expect(src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  // expect(alt).toMatch('Pikachu sprite');
+  // const h2 = getByRole('heading', { level: 2 });
+  // expect(h2).toBeInTheDocument();
+  // const summary = getByText(/This intelligent Pokémon roasts hard berries with/,
+  //   / electricity to make them tender enough to eat./);
+  // expect(summary).toBeInTheDocument();
+});
+
+// test('renders the correct path and pokemon details', () => {
+//   const { getByText,
+//     getByTestId, getAllByText, getByAltText, history } = renderWithRouter(<App />);
+//   const linkDetails = getByText(/More details/);
+//   fireEvent.click(linkDetails);
+//   const url = history.location.pathname;
+//   expect(url).toBe('/pokemons/25');
+//   const pikachuName = getByTestId('pokemon-name');
+//   expect(pikachuName).toBeInTheDocument();
+//   const pikachuType = getByTestId('pokemon-type');
+//   expect(pikachuType).toBeInTheDocument();
+//   const pikachuWeight = getByTestId('pokemon-weight');
+//   expect(pikachuWeight).toBeInTheDocument();
+//   const pikachuNameTimes = getAllByText(/Pikachu/i);
+//   const length = 3;
+//   expect(pikachuNameTimes.length).toBe(length);
+//   const type = getByText(/Electric/);
+//   expect(type).toBeInTheDocument();
+//   const avgWeight = getByText(/Average weight: 6.0 kg/);
+//   expect(avgWeight).toBeInTheDocument();
+//   const img = getByAltText(/Pikachu sprite/);
+//   const { src, alt } = img;
+//   expect(src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+//   expect(alt).toMatch('Pikachu sprite');
+// });
