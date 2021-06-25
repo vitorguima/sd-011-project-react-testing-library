be21f9340f6d975ecff2f7ee44e11771adeac3d5
Savio Moraes <smmsavio@hotmail.com>
2021-06-25 00:16:42 -0300

    commit teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,22 +3,33 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Requisito 01', () => {
+  it('Renderiza Pokedéx ao carregar a aplicação no caminho de URL `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('Testa se o topo da aplicação contém um conjunto de links de navegação.', () => {
+
+  });
+
+  it('Redireciona para a página inicial, ao clicar no link Home', () => {
+
+  });
+
+  it('Redireciona para a página de About, na URL /about, ao clicar no link About', () => {
+
+  });
+
+  it('Redireciona para a página de Favoritos ao clicar no link Favorite Pokémons', () => {
+
+  });
+  it('Redireciona para a página Not Found ao entrar em uma URL desconhecida', () => {
+
+  });
 });
-
-// test('shows the Pokédex when the route is `/`', () => {
-//   const { getByText } = render(
-//     <MemoryRouter initialEntries={ ['/'] }>
-//       <App />
-//     </MemoryRouter>,
-//   );
-
-//   expect(getByText('Encountered pokémons')).toBeInTheDocument();
-// });
