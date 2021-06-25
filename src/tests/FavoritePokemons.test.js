2509358257e29cd279f82272a952f1d4b824bfc4
Arthur Hermann <arthurhermannc@gmail.com>
2021-06-24 21:33:13 -0300

    ajusta FavoritePokemons e adiciona NotFound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,47 +1,24 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
 
-describe('Testando componente PokemonDetails - R7', () => {
-  it('Verifica se as informações detalhadas são mostradas', () => {
-    const { getByRole, getByText, queryByText } = renderWithRouter(<App />);
-    const linkDet = getByRole('link', { name: /More details/i });
-    fireEvent.click(linkDet);
-    const nameDet = getByText(/Details/i);
-    expect(nameDet.innerHTML).toBe('Pikachu Details');
-    expect(queryByText(/More details/i)).not.toBeInTheDocument();
-    const summary = getByRole('heading', { name: /Summary/i });
-    expect(summary).toBeInTheDocument();
-    const pikachuText = getByText(/This intelligent/i);
-    expect(pikachuText).toBeInTheDocument();
+describe('Testando componente FavoritePokenons - R3', () => {
+  it('Verifica o comportamento do FavoritePokemons', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const textNoPokemons = getByText(/No favorite pokemon found/);
+    expect(textNoPokemons).toBeInTheDocument();
   });
-  it('Verifica se contém os mapas', () => {
-    const { getByRole, getAllByRole } = renderWithRouter(<App />);
-    const linkDet = getByRole('link', { name: /More details/i });
-    fireEvent.click(linkDet);
-    const img = getAllByRole('img');
-    expect(img[1].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
-    expect(img[1].alt).toBe('Pikachu location');
-    expect(img[2].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
-    expect(img[2].alt).toBe('Pikachu location');
-  });
-  it('Verifica se contém checkbox', () => {
-    const { getByRole,
-      getByLabelText, getAllByRole, getByText } = renderWithRouter(<App />);
-    const linkDet = getByRole('link', { name: /More details/i });
-    fireEvent.click(linkDet);
-    const checkbox = getByLabelText(/Pokémon favoritado/i);
-    expect(checkbox).toBeInTheDocument();
-    fireEvent.click(checkbox);
-    const img = getAllByRole('img');
-    expect(img[1].src).toContain('star-icon.svg');
-    expect(img[1].alt).toBe('Pikachu is marked as favorite');
-    const locTitle = getByRole('heading', { name: /Game Locations of Pikachu/i });
-    expect(locTitle).toBeInTheDocument();
-    const linkFav = getByRole('link', { name: /Favorite Pokémons/i });
-    fireEvent.click(linkFav);
-    const pikachu = getByText(/Pikachu/i);
-    expect(pikachu).toBeInTheDocument();
+  it('Verifica o comportame após favoritar', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const moreDetails = getByText(/More details/i);
+    fireEvent.click(moreDetails);
+    const input = getByRole('checkbox');
+    fireEvent.click(input);
+    const linkFavPoke = getByText('Favorite Pokémons');
+    fireEvent.click(linkFavPoke);
+    const weight = getByText(/kg/i);
+    expect(weight).toBeInTheDocument();
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando componente NotFound - R4', () => {
+  it('Verifica se o componente possui o texto correto', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const textNotFound = getByText(/Page requested not found/);
+    expect(textNotFound).toBeInTheDocument();
+  });
+  it('Teste se a página contém a img correta.', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const img = getAllByRole('img');
+    expect(img[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
