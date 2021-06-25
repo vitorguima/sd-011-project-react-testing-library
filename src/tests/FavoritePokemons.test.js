c9f4c2594ed4ae216c21934ff6e6f39bfc315ee3
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-06-25 12:52:59 -0300

    Adicionando requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// // import { About } from '../components';
+// import { NotFound } from '../components';
+
+// test('Teste se página contém um h2 com o texto Page requested not found', () => {
+//   const { getByRole } = render(<NotFound />);
+//   const heading = getByRole('heading', { level: 2, name: /Page requested not found/i });
+//   expect(heading).toBeInTheDocument();
+// });
+
+// test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+//   const { getByRole } = render(<NotFound />);
+//   const heading = getByRole('heading', { level: 2, name: /Page requested not found/i });
+//   expect(heading).toBeInTheDocument();
+// });
+
+// test('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+//   const { getByRole } = render(<NotFound />);
+//   const heading = getByRole('heading', { level: 2, name: /Page requested not found/i });
+//   expect(heading).toBeInTheDocument();
+// });
+// pode testar algo sobre length ===0
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+// import { About } from '../components';
+import { NotFound } from '../components';
+
+test('Teste se página contém um h2 com o texto Page requested not found', () => {
+  const { getByRole } = render(<NotFound />);
+  const heading = getByRole('heading', { level: 2, name: /Page requested not found/i });
+  expect(heading).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem', () => {
+  const { getByAltText } = render(<NotFound />);
+  const getImg = getByAltText(/Pikachu crying because the page requested was not found/);
+  expect(getImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
