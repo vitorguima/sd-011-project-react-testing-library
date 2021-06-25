1231e01322e98441b3c408e7d85775dd0af49562
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-06-25 13:12:21 -0300

    cristianbrum-project-react-testing-library

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = () => (
       <img
         className="pokedex-image"
         src={ `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png` }
-        alt="Pokédex"
+        alt="Pokédex!"
       />
     </section>
   </section>
