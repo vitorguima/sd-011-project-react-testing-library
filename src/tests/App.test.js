0606964bbdc74c041bf1db0177b8367271f1924d
marcoswarmling <marcoswarmling15@gmail.com>
2021-06-25 15:22:59 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -12,4 +13,46 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
-// opa
+
+describe('Testando componente App - R1', () => {
+  it('Verifica o endereço da URL "/" ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/');
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+  it('Verifica a existência e ordem dos links', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const link = getAllByRole('link');
+    expect(link[0].innerHTML).toBe('Home');
+    expect(link[1].innerHTML).toBe('About');
+    expect(link[2].innerHTML).toBe('Favorite Pokémons');
+  });
+  it('Ao clicar no Link Home, é direcionado para página inicial', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const link = getByRole('link', { name: /Home/ });
+    fireEvent.click(link);
+    const url = history.location.pathname;
+    expect(url).toBe('/');
+  });
+  it('Ao clicar no Link About é direcionado para página About', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const link = getByRole('link', { name: /About/ });
+    fireEvent.click(link);
+    const url = history.location.pathname;
+    expect(url).toBe('/about');
+  });
+  it('Ao clicar no Favorite é direcionado para Favoritos', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const link = getByRole('link', { name: /Favorite/ });
+    fireEvent.click(link);
+    const url = history.location.pathname;
+    expect(url).toBe('/favorites');
+  });
+  it('Ao digitar uma url errada vai para a pagina notFound', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/notFound');
+    const heading = getByText(/Not found/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
