879d7c060fe4620a64b464b82ef6f2f625bb9264
Hugo Mouto <hmoutol@gmail.com>
2021-06-25 15:28:18 -0300

    feat: Critério 3 - check

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Test if all the Favorite Pokemons page is being exhibited correctly', () => {
+  test('checks empty favorite', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noPokemonFound = getByText(/No favorite pokemon found/i);
+    expect(noPokemonFound).toBeInTheDocument();
+  });
+
+  test('checks if selected pokemon shows on Favorite screen', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const moreDetails = getByText(/More Details/i);
+    fireEvent.click(moreDetails);
+    const favoritePokemon = getByText('Pokémon favoritado?');
+    fireEvent.click(favoritePokemon);
+    const linkFavorite = getByText(/Favorite Pokémons/);
+    fireEvent.click(linkFavorite);
+    expect(getByText('Pikachu')).toBeInTheDocument();
+  });
+});
