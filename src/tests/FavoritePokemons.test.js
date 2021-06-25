fc91076dfe43e0352ee34b34d6d19360b36c14d1
Nick-713 <pereiranicolas713@gmail.com>
2021-06-25 15:16:43 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('No favorite pokemon found appears on the screen', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  const unfavoritePokemom = getByText(/No favorite pokemon found/i);
+  expect(unfavoritePokemom).toBeInTheDocument();
+});
