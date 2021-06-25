b98f4269c3d9562471b7aa4474036fdb1bd110df
Gustavo Lemes <gustavolemes997@gmail.com>
2021-06-25 20:34:47 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testes do componente <NotFound.js />', () => {
+
+  it('Testa se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { getByText } = render(<NotFound />);
+
+    const textNotFound = getByText(/Page requested not found/i);
+
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  it('Testa se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByAltText } = render(<NotFound />);
+
+    const image = getByAltText(/Pikachu crying because the page requested was not found/i);
+
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+
+})
