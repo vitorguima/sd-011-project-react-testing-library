5337b181e5c3b9ae9e09b2008ae76934ec83a1e0
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-06-25 09:33:24 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const headingH2 = getByText(/Page requested not found/i, { selector: 'h2' });
+  expect(headingH2).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const testImg = getByAltText(/Pikachu crying because the page requested was not found/);
+  expect(testImg).toBeInTheDocument();
+  expect(testImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
