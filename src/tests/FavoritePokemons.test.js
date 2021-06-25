af44394e1a2e08be967fbf42d0842c1db5e10615
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-06-25 15:31:54 -0300

    FavoritePokemons.js 100%

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+
+import renderWithMemory from '../renderWithMemory';
+import App from '../App';
+
+const initialEntries = { initialEntries: ['/favorites'] };
+
+test('Test favorites', () => {
+  localStorage.setItem('favoritePokemonIds', '[25]');
+  const { getByText } = renderWithMemory(<App />, initialEntries);
+
+  expect(localStorage.length).toBeTruthy();
+  expect(getByText('Pikachu')).toBeInTheDocument();
+  expect(getByText('Electric')).toBeInTheDocument();
+  expect(getByText(/6.0/)).toBeInTheDocument();
+});
+
+test('Test not favorites', () => {
+  localStorage.clear();
+  const { getByText } = renderWithMemory(<App />, initialEntries);
+
+  expect(localStorage.length).toBeFalsy();
+  const p = getByText('No favorite pokemon found');
+  expect(p).toContainHTML('<p>No favorite pokemon found</p>');
+});
