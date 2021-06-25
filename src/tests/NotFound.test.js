3de88dd604ab51b6732d800b38528ba1b21fadad
Matheus Gaspar <msouzagaspar@gmail.com>
2021-06-25 15:09:57 -0300

    requisito 4 - testes do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js component', () => {
+  it('should render specific text when page is not found', () => {
+    render(<NotFound />);
+    const headingText = screen.queryByText(/Page requested not found/i);
+    expect(headingText).toBeVisible();
+  });
+
+  it('should render specific image', () => {
+    render(<NotFound />);
+    const image = screen.queryByAltText(/Pikachu crying/i);
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
