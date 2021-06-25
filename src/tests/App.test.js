d6c988a4ddfad3e91ae1b621a2e379f01a1b9caa
Gustavo Mendes <gusm217@gmail.com>
2021-06-25 15:38:49 -0300

    Finalizado requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,73 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testes requisito 1', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  it('shows the Pokédex when the route is `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    const homePath = history.location.pathname;
+    history.push(homePath);
+    expect(getByText(/encountered pokémons/i)).toBeInTheDocument();
+  });
+
+  it('shows if there are three nav links: Home, About, Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    expect(getByText(/Home/i)).toBeInTheDocument();
+    expect(getByText(/about/i)).toBeInTheDocument();
+    expect(getByText(/favorite pokémons/i)).toBeInTheDocument();
+  });
+
+  it('test if nav Home link brings back to homepage', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const homePath = history.location.pathname;
+    const navHome = getByText(/Home/i);
+    const homeText = getByText(/encountered pokémons/i);
+
+    fireEvent.click(navHome);
+
+    expect(homePath).toBe('/');
+    expect(homeText).toBeInTheDocument();
+  });
+
+  it('test if nav About goes to /about page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const navAbout = getByText(/about/i);
+    const aboutPath = '/about';
+
+    fireEvent.click(navAbout);
+    history.push(aboutPath);
+
+    expect(getByText(/This application simulates a Pokédex/i)).toBeInTheDocument();
+    expect(aboutPath).toBe('/about');
+  });
+
+  it('test if nav Favorite Pokemons goest to /favorites page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const navFavorites = getByText(/favorite/i);
+    const favoritesPath = '/favorites';
+
+    fireEvent.click(navFavorites);
+    history.push(favoritesPath);
+
+    expect(favoritesPath).toBe('/favorites');
+  });
+
+  // it('test if theres no URL matching, return Not Found page', () => {
+  //   { getByText, history} = renderWithRouter(<Appzz)
+  // })
 });
