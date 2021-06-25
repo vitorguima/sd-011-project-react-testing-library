14d45c2f0ecfb9f417d4b0900affbd032d747d26
heyset <c.inaciomatheus@gmail.com>
2021-06-24 21:10:40 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+
+import renderWithHistory from './helpers/renderWithHistory';
+
+import NotFound from '../components/NotFound';
+
+describe('A página de não encontrado', () => {
+  let getByRole;
+  let getByAltText;
+
+  beforeEach(() => {
+    ({ getByRole, getByAltText } = renderWithHistory(<NotFound />));
+  });
+
+  it('possui um heading dizendo "Page requested not found"', () => {
+    const heading = getByRole('heading');
+
+    expect(heading.tagName).toBe('H2');
+    expect(heading).toHaveTextContent(/page requested not found/i);
+  });
+
+  it('possui um gif do pikachu chorando', () => {
+    const imgSource = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(image).toHaveAttribute('src', imgSource);
+  });
+});
