be4e57c025ef97495079cd6b5ef1f07231bcfa12
Lais Simenikim <l.simenikim@gmail.com>
2021-06-25 19:44:00 -0300

    add requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+test('Verifica se a página tem título com o texto "Page requested not found"', () => {
+  renderWithRouter(<NotFound />);
+
+  const title = screen.getByText(/Page requested not found/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('Verifica se há um h2 na página', () => {
+  const { container } = renderWithRouter(<NotFound />);
+
+  const title = container.querySelector('h2');
+  expect(title).toBeInTheDocument();
+});
+
+test('Verifica se a página contém uma imagem específica', () => {
+  const { container } = renderWithRouter(<NotFound />);
+
+  const img = container.querySelector('img');
+  expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
