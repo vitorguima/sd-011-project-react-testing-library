de09a1b41b4308c40c311915f39507bb9cee67b8
MIkaela Braga <bragamikaela@gmail.com>
2021-06-25 02:02:40 -0300

    Adicionando corretamente o renderWithRouter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+// import React from 'react';
+// import { render, fireEvent } from '@testing-library/react';
+// import renderWithRouter from '../renderWithRouter';
+// import About from '../components/About';
+
+// describe('teste do componente About.js', () => {
+//   test('testa se a página contém um ')
+// });
