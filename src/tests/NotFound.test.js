66c4e421fceaab813516ada02a8591aff5a86d7b
Hugo Mouto <hmoutol@gmail.com>
2021-06-25 16:59:58 -0300

    feat: Critério 4 - check

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Test if Not Found page is being exhibited correctly', () => {
+  test('checks for text on Not Found Page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const wrongRoute = '/noroute';
+    history.push(wrongRoute);
+    const errorText = getByText('Page requested not found');
+    expect(errorText).toBeInTheDocument();
+  });
+
+  test('checks for image on Not Found Page', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+    const wrongRoute = '/noroute';
+    history.push(wrongRoute);
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imageSrc = getByAltText('Pikachu crying because the'
+    + ' page requested was not found');
+    expect(imageSrc.src).toContain(imgUrl);
+  });
+});
