60128dcfc3fcd2723f2b24f4ab3c56e6845087aa
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-06-25 15:22:30 -0300

    Requirement 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,46 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Testing Requirement 01 - Component App.js', () => {
+  it('Test if app starts in url=/ ', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('If application contains navigation links', () => {
+    const { container } = renderWithRouter(<App />);
+    const links = container.getElementsByTagName('a');
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('About');
+    expect(links[2]).toHaveTextContent('Favorite Pokémons');
+  });
+
+  it('Test if path=/ redirects to homepage', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const textHome = getByText(/Encountered pokémons/);
+    expect(textHome).toBeInTheDocument();
+  });
+
+  it('Test if path=/about redirects to About page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    expect(history.location.pathname).toBe('/about');
+    expect(getByText('About Pokédex')).toBeInTheDocument();
+  });
+
+  it('Test if path=/favorites redirects to FavoritePokemons page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    expect(history.location.pathname).toBe('/favorites');
+    expect(getByText('Favorite pokémons')).toBeInTheDocument();
+  });
+
+  it('Test Error 404 not found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/poke');
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
 });
