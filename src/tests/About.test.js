5aff4fa8c1066cb56ce97dc3bd4b83b098097379
Savio Moraes <smmsavio@hotmail.com>
2021-06-25 16:14:17 -0300

    sobe código para ver se consigo identificar o erro

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Requisito 02 - Testa o componente <About />', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByText } = render(<About />);
+    const aboutTitle = getByText(/About Pokédex/);
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getAllByText } = render(<About />);
+    const paragraphs = getAllByText(/Pokémons/i);
+    expect(paragraphs).toContainHTML('p');
+  });
+
+  it('Teste se a página contém a imagem de uma Pokédex, através de um link.', () => {
+
+  });
+});
