b6403a00b9751338d84d23e31053d51aff02b470
Renato Bispo <renatoslvbispo@gmail.com>
2021-06-25 17:46:27 -0300

    Add isPokemonFavoriteById to mockedFavoritePokemons.js

--- src/mockedFavoritePokemons.js
+++ src/mockedFavoritePokemons.js
@@ -5,6 +5,17 @@ export const favoritePokemons = [
   pokemons[1],
   pokemons[2],
 ];
+
 export const nonFavoritePokemons = pokemons
   .filter(({ name: pokemonName }) => favoritePokemons
     .every(({ name: favoritePokemonName }) => favoritePokemonName !== pokemonName));
+
+export const isPokemonFavoriteById = pokemons.reduce((acc, { id: pokemonId }) => {
+    const isPokemonFavorite = favoritePokemons.some(({ id: favoritePokemonId }) =>
+      pokemonId === favoritePokemonId);
+
+    acc[pokemonId] = isPokemonFavorite;
+
+    return acc;
+  }
+, {});
