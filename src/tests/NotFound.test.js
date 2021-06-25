5fa9b2e77a8fd3db09311a0703a39439b6cc0041
Felipe Buso <felipe_buso@hotmail.com>
2021-06-25 12:07:12 -0300

    Item 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('When render NotFound', () => {
+  it('Have a <h2> with "Page not found"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const notFound = getByRole('heading', { level: 2 });
+    expect(notFound.innerHTML).toBe('Page requested not found'
+      + '<span role="img" aria-label="Crying emoji"> 😭</span>');
+  });
+  it('Have a img', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const imageNotFound = getByAltText('Pikachu crying because '
+      + 'the page requested was not found');
+    expect(imageNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
