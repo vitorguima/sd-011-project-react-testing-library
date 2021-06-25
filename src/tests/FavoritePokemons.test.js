7f51976b7e235d18fd18ea37dc0ed0bb9b053477
Víctor Faria <vitilevi@hotmail.com>
2021-06-24 23:08:19 -0300

    Começado req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente Favorite Pokemons', () => {
+  it('Testa se é exibido a mensagem No favorite pokemon found na tela', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const text = getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Testa se nenhum pokemom está favoritado', () => {
+    
+  });
+});
