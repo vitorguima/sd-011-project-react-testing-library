5b47a85897f215e9871eefaca80bbea3abbf93da
Cris Lustosa <cmlustosa@gmail.com>
2021-06-25 16:14:18 -0300

    Adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -15,10 +15,31 @@ test('renders a reading with the text `Pokédex`', () => {
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('A página deve possuir os links para Home, about e Favorite Pokemons', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText(/home/i)).toBeInTheDocument();
+  expect(getByText(/About/i)).toBeInTheDocument();
+  expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+});
+
+test('Ao clicar no link Home da barra de navegação vai para a página inicial', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/About'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const home = getByText(/home/i);
+  expect(home).toBeInTheDocument();
 });
