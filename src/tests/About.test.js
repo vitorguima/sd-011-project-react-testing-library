0cfea2b6226d4550bfb70d7f73c8e46aefca7b29
MIkaela Braga <bragamikaela@gmail.com>
2021-06-25 03:33:33 -0300

    Requisito 2 com lint revisado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,8 +1,26 @@
-// import React from 'react';
-// import { render, fireEvent } from '@testing-library/react';
-// import renderWithRouter from '../renderWithRouter';
-// import About from '../components/About';
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
 
-// describe('teste do componente About.js', () => {
-//   test('testa se a página contém um ')
-// });
+describe('teste do componente About.js', () => {
+  test('testa se a página contém tag h2 com o texto About Pokédex', () => {
+    const { getByRole } = render(<About />);
+
+    const titleElement = getByRole('heading', { level: 2 });
+    expect(titleElement.innerHTML).toBe('About Pokédex');
+  });
+
+  test('testa se a página contém 2 parágrafos com o texto sobre Pokédex', () => {
+    const { getAllByText } = render(<About />);
+
+    const paragrapherElement = getAllByText(/Pokémons/i);
+    expect(paragrapherElement.length).toEqual(2);
+  });
+
+  test('testa se a página contém imagem de uma Pokédex', () => {
+    const { getByRole } = render(<About />);
+
+    const image = getByRole('img');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
