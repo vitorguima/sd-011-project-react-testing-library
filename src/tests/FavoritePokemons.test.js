ca76b61b9db31ecd8cd9bc598daa91865f86c35b
Jessica Arruda <sicarruda@gmail.com>
2021-06-25 18:24:00 -0300

    requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,28 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import About from '../components/About';
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+// ideia tirada do codigo https://github.com/tryber/sd-011-project-react-testing-library/pull/76/files da Gisele Costa
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
-  test('', () => {
-
+  test(
+    'Teste se é exibido na tela a mensagem No favorite '
+      + 'pokemon found, se a pessoa não tiver pokémons favoritos',
+    () => {
+      const { getByText } = renderWithRouter(<App />);
+      fireEvent.click(getByText('Favorite Pokémons'));
+      const pokemonNoFound = getByText('No favorite pokemon found');
+      expect(pokemonNoFound).toBeInTheDocument();
+    },
+  );
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText('More details'));
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    fireEvent.click(getByText('Favorite Pokémons'));
+    const pokemon = getByText('Pikachu');
+    expect(pokemon).toBeInTheDocument();
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um heading h2 com o texto '
+  + 'Page requested not found 😭', () => {
+    const { container, getByText } = render(<NotFound />);
+    const tagH2 = container.querySelector('h2');
+    expect(tagH2).toBeInTheDocument();
+    const textH2 = getByText('Page requested not found');
+    expect(textH2).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem do Pikachu', () => {
+    const { container } = render(<NotFound />);
+    const img = container.querySelector('img');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
