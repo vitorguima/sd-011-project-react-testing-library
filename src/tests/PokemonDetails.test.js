659afa51c7153b9a1d23451610c1a3061dea8029
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-25 13:20:17 -0300

    [filizzola-testing-library] - continuando a 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -3,9 +3,11 @@ import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
+const detail = 'More details';
+
 test('Verifica se tem o Summary renderizado', () => {
   const { getByText, container } = renderWithRouter(<App />);
-  const details = getByText('More details');
+  const details = getByText(detail);
   fireEvent.click(details);
   const getAbout = container.querySelectorAll('h2')[1];
   expect(getAbout.textContent).toBe('Summary');
@@ -13,7 +15,7 @@ test('Verifica se tem o Summary renderizado', () => {
 
 test('Verifica se tem o Game Locations of<Name> renderizado', () => {
   const { getByText, container } = renderWithRouter(<App />);
-  const details = getByText('More details');
+  const details = getByText(detail);
   fireEvent.click(details);
   const getAbout = container.querySelectorAll('h2')[2];
   expect(getAbout.textContent).toBe('Game Locations of Pikachu');
@@ -21,8 +23,15 @@ test('Verifica se tem o Game Locations of<Name> renderizado', () => {
 
 test('Verifica se tem o Pikachu Details renderizado', () => {
   const { getByText, container } = renderWithRouter(<App />);
-  const details = getByText('More details');
+  const details = getByText(detail);
   fireEvent.click(details);
   const getAbout = container.querySelectorAll('h2')[0];
   expect(getAbout.textContent).toBe('Pikachu Details');
+});
+
+test('Verifica se details não é renderizado no pagina details', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const details = getByText('More details');
+  fireEvent.click(details);
+  expect(details).not.toBeInTheDocument();
 });
