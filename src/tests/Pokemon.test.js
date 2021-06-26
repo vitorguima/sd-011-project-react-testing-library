5129192e318b2e4a8e9d56bfd8aef4f92aba435d
Hugo Mouto <hmoutol@gmail.com>
2021-06-25 22:58:35 -0300

    feat: Critério 6 - 75%

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -36,7 +36,6 @@ describe('Test if Pokemon component is being exhibited correctly', () => {
     const pokemonType = queryAllByText('Electric')[0];
     const pokemonWeight = getByText('Average weight: 6.0 kg')
     const imgAltText = 'Pikachu sprite'
-    // const imgUrl = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png'
     const imgPkmn = getByAltText(imgAltText);
     expect(imgPkmn
       && pokemonName
@@ -46,8 +45,27 @@ describe('Test if Pokemon component is being exhibited correctly', () => {
   });
 
   test('checks URL link on \'More Details\' ', () => {
-    const {getByText } = renderWithRouter(<App />)
+    const { getByText } = renderWithRouter(<App />)
     const moreDetailsLink = getByText(/more details/i)
     expect(moreDetailsLink).toHaveAttribute('href','/pokemons/25')
   })
+
+  test('tests click on \`More Details\' ', () => {
+    const {getByText, history } = renderWithRouter(<App />)
+    const moreDetailsLink = getByText(/more details/i)
+    fireEvent.click(moreDetailsLink)
+    const url = history.location.pathname
+    expect(url).toBe('/pokemons/25')
+  })
+
+  test('checks if selected pokemon hast star', () => {
+    const { getByText, getByAltText } = renderWithRouter(<App />);
+    const moreDetails = getByText(/More Details/i);
+    fireEvent.click(moreDetails);
+    const favoritePokemon = getByText('Pokémon favoritado?');
+    fireEvent.click(favoritePokemon);
+    const imgUrl = '/star-icon.svg';
+    const imageSrc = getByAltText('Pikachu is marked as favorite');
+    expect(imageSrc.src).toContain(imgUrl);
+  });
 })
