be48f873d32e7c6600a26be3bb48e53124b0303b
gab-boff <gmeninboff@gmail.com>
2021-06-26 20:49:19 -0300

    Adicionando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import Favorite from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<Favorite />);
+    const noFavorite = getByText('No favorite pokemon found');
+
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  it('Se é exibido todos os cards de pokémons favoritados', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const detailsPokemon = getByText('More details');
+    fireEvent.click(detailsPokemon);
+
+    const favoriteInput = getByRole('checkbox');
+    // expect(favoriteInput).toBeInTheDocument();
+
+    fireEvent.click(favoriteInput);
+
+    const favoritePage = getByText('Favorite Pokémons');
+    fireEvent.click(favoritePage);
+
+    const favoriteText = getByText(/kg/i);
+    expect(favoriteText).toBeInTheDocument();
+  });
+});
