5644c6e03ec0406f57018d33d0b58748f13cd52f
Hugo Mouto <hmoutol@gmail.com>
2021-06-25 23:16:10 -0300

    feat: Critério 6 - 87.50%

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -7,27 +7,8 @@ import Pokedex from '../components/Pokedex';
 import pokemons from '../data';
 
 describe('Test if Pokemon component is being exhibited correctly', () => {
-  test('checks for text on Not Found Page', () => {
-    const { getByText, getByTestId, queryAllByText, getByAltText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const pokemonName = getByText('Pikachu');
-    const pokemonType = queryAllByText('Electric')[0];
-    const pokemonWeight = getByText('Average weight: 6.0 kg')
-    const imgAltText = 'Pikachu sprite'
-    // const imgUrl = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png'
-    const imgPkmn = getByAltText(imgAltText);
-    expect(imgPkmn
-      && pokemonName
-      && pokemonType
-      && pokemonWeight
-      && imgPkmn).toBeInTheDocument()
-  });
-
-  test('checks for text on Not Found Page', () => {
-    const { getByText, getByTestId, queryAllByText, getByAltText } = render(
+  test('checks for pokemon info', () => {
+    const { getByText, queryAllByText, getByAltText } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
@@ -42,6 +23,9 @@ describe('Test if Pokemon component is being exhibited correctly', () => {
       && pokemonType
       && pokemonWeight
       && imgPkmn).toBeInTheDocument()
+    const imageSrc = getByAltText(imgAltText);
+    console.log(imageSrc)
+    expect(imageSrc.src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png')
   });
 
   test('checks URL link on \'More Details\' ', () => {
