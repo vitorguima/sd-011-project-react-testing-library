9420985cdbf0343c301614f94953c78a51236aa1
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-06-26 19:56:14 -0300

    Adiciona requisitos 2 e 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Test About component', () => {
+  it('Checks if the page has info about Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const heading = getByText(/About Pokédex/);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Checks if the page has an h2 with the text About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', { level: 2 }, { name: /About Pokédex/ });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Checks if the page has two paragraphs with the text Pokédex.', () => {
+    const { container } = renderWithRouter(<About />); // https://testing-library.com/docs/example-reach-router/
+    const paragraphs = container.querySelectorAll('p');
+    expect(paragraphs.length).toBe(2); // toHaveLength tb funciona
+  });
+
+  it('Checks if the page has an image', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+// import pokemon from '../data';
+
+// const pokemons = [pokemon[0], pokemon[1]];
+
+describe('Test FavoritePokemons component', () => {
+  it('Checks if it shows No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const notFound = getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', async () => {
+    const { getByText,
+      getByLabelText, getByAltText, history } = renderWithRouter(<App />);
+
+    history.push('/pokemons/25'); // more details
+    fireEvent.click(getByLabelText('Pokémon favoritado?'));
+    fireEvent.click(getByText('Favorite Pokémons'));
+    expect(getByAltText('Pikachu is marked as favorite')).toBeInTheDocument();
+  });
+
+  it('If NOT checked, no card is shown', () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+});
+
+// await expect(userEvent.type(checkbox)).toBeChecked()
+// fireEvent.change('input', { target: { value: 'checked' } });
