689732777932ac209d07c86fa5041e746786360a
Wilquer82 <wilquer.fig@gmail.com>
2021-06-26 12:11:30 -0300

    Ufa, agora o 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './RenderWithRouter';
+import { About } from '../components';
+
+describe('Test component <About.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const p1 = getByText(/This application simulates/i);
+    const p2 = getByText(/One can filter Pokémons by type/i);
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const altimg = getByAltText(/Pokédex/i);
+    expect(altimg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
