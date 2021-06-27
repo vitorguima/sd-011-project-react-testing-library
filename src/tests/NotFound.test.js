536ae4974df86851eea1ad3a5b82d488af034826
Gustavo Mendes <gusm217@gmail.com>
2021-06-27 17:22:32 -0300

    Finaliza requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+
+describe('Tests requisito 4', () => {
+  it('tests if theres a h2 tag with Page requested not found + emoji', () => {
+    const { getByText } = render(<NotFound />);
+
+    const heading = getByText(/page requested not found/i);
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('tests if theres an img with a given path', () => {
+    const { getByRole } = render(<NotFound />);
+
+    const gif = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found' });
+
+    expect((gif).src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
