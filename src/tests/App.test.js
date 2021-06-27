cfd9e0972da1dfdfc8d9e54e74c3ac1d1bf39daa
tomsantos07 <xotmsantosx@live.com>
2021-06-27 19:00:25 -0300

    req 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './RenderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+it('Testa se renderiza a rota "/"', () => {
+  renderWithRouter(<App />);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  const mainText = screen.getByText(/Encountered pokémons/i);
+  expect(mainText).toBeInTheDocument();
+});
+
+it('Testa os links do topo', () => {
+  renderWithRouter(<App />);
+
+  const firstLink = screen.getByText(/Home/i);
+  expect(firstLink).toBeInTheDocument();
+
+  const secondLink = screen.getByText(/About/i);
+  expect(secondLink).toBeInTheDocument();
+
+  const thirdLink = screen.getByText(/Favorite Pokémons/i);
+  expect(thirdLink).toBeInTheDocument();
+});
+
+it('Testa o redirecionamento na url "/" ao clicar em "Home"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkHome = getByText(/Home/);
+  fireEvent.click(linkHome);
+  const url = history.location.pathname;
+  expect(url).toBe('/');
+});
+
+it('Testa se redireciona para a página de "About", na URL "/about"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkAbout = getByText(/about/);
+  fireEvent.click(linkAbout);
+  const url = history.location.pathname;
+  expect(url).toBe('/about');
+});
+
+it('Teste se  redirecionaa página de `Pokémons Favoritados`, na URL `/favorites`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkFavorites = getByText(/favorites/);
+  fireEvent.click(linkFavorites);
+  const url = history.location.pathname;
+  expect(url).toBe('/favorites');
 });
