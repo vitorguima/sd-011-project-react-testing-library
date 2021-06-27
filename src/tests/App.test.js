1d2c6a23eb0daa39033eee89961e65fc5316faa6
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2021-06-27 17:48:19 -0400

    Requisito 1, feito.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
+const favoritePokemons = 'Favorite Pokémons';
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
@@ -21,4 +23,58 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('O primeiro link deve possuir o texto "Home", "About", "Favorite Pokémons"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Home')).toBeInTheDocument();
+  expect(getByText('About')).toBeInTheDocument();
+  expect(getByText(favoritePokemons)).toBeInTheDocument();
+});
+
+test('Teste para página "Home"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkToHome = getByText('Home');
+  userEvent.click(linkToHome);
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Teste para página "About"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkToAbout = getByText('About');
+  userEvent.click(linkToAbout);
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('Teste para página "Favorite Pokémons"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkToFavoritePoke = getByText(favoritePokemons);
+  userEvent.click(linkToFavoritePoke);
+  expect(getByText(favoritePokemons)).toBeInTheDocument();
+});
+
+test('Teste para "Página não encontrada"', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/pagina-nao-encontrada'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText('Page requested not found')).toBeInTheDocument();
 });
