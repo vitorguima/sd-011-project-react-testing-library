39df049c65c7f08a6942c991aecf640b67f650ab
marlon307 <marlon-ramosb@hotmail.com>
2021-06-27 18:48:17 -0300

    Rq 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  it('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
+    const { getByTestId, getByRole } = renderWithRouter(<App />);
+    expect(getByTestId('pokemon-name').textContent).toBe('Pikachu');
+    expect(getByTestId('pokemon-type').textContent).toBe('Electric');
+    expect(getByTestId('pokemon-weight').textContent).toBe('Average weight: 6.0 kg');
+    expect(getByRole('img').src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('Teste se o card do Pokémon indicado na Pokédex contém um link.', () => {
+    const { history, getByText, getByAltText } = renderWithRouter(<App />);
+    fireEvent.click(getByText('More details'));
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    expect(getByAltText(/is marked as favorite/)).toBeInTheDocument();
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  it('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    const { getByAltText } = renderWithRouter(<App />);
+    expect(getByAltText(/is marked as favorite/).src).toContain('/star-icon.svg');
+    expect(getByAltText(/is marked as favorite/).alt).toContain('Pikachu ');
+  });
+});
