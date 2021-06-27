5ba86bf3ad58de5a3743dafc75679a81cca2adf1
israeltonholo <israeltonholo@hotmail.com>
2021-06-27 11:01:08 -0300

    adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import RenderWithRouter from '../RenderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +22,57 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('Teste o componente App', () => {
+  it('verifica os 3 links fixos no topo da página', () => {
+    const { getByText } = RenderWithRouter(
+      <App />,
+    );
+    const home = getByText('Home');
+    expect(home).toBeInTheDocument();
+    const about = getByText('About');
+    expect(about).toBeInTheDocument();
+    const favoritepokemons = getByText('Favorite Pokémons');
+    expect(favoritepokemons).toBeInTheDocument();
+  });
+
+  it('Verifica se a aplicação é redirecionada para a página inicial', () => {
+    const { getByText, history } = RenderWithRouter(
+      <App />,
+    );
+    const home = getByText('Home');
+    expect(home).toBeInTheDocument();
+    fireEvent.click(home);
+    const path = history.location.pathname;
+    expect(path).toBe('/');
+  });
+  it('Verifica se a aplicação é redirecionada para a página de About', () => {
+    const { getByText, history } = RenderWithRouter(
+      <App />,
+    );
+    const about = getByText('About');
+    expect(about).toBeInTheDocument();
+    fireEvent.click(about);
+    const path = history.location.pathname;
+    expect(path).toBe('/about');
+  });
+  it('Verifica se a app é redirecionada para a página de Pokémons Favoritados', () => {
+    const { getByText, history } = RenderWithRouter(
+      <App />,
+    );
+    const favPokemons = getByText('Favorite Pokémons');
+    expect(favPokemons).toBeInTheDocument();
+    fireEvent.click(favPokemons);
+    const path = history.location.pathname;
+    expect(path).toBe('/favorites');
+  });
+  it('Verifica se a aplicação é redirecionada para a página Not Found', () => {
+    const { getByText, history } = RenderWithRouter(
+      <App />,
+    );
+    history.push('/aleatorio');
+    const notFound = getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+  });
 });
