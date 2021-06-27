16f09169b93ec94cce575c14cb215ac9606c41b0
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-06-27 03:26:20 -0300

    Adiciona Requisito 2 e 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testando About.js', () => {
+  it('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { getByText } = render(<About />);
+    const text = getByText(/This application simulates a Pokédex/);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { container } = render(<About />);
+    const checkH2 = container.querySelector('h2');
+    expect(checkH2.textContent).toBe('About Pokédex');
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getAllByText } = render(<About />);
+    const checkP = getAllByText(/Pokémons/);
+    expect(checkP.length).toEqual(2);
+  });
+
+  it('Teste se a página contém dois parágrafos com querySelectorAll', () => {
+    const { container } = render(<About />);
+    const checkP = container.querySelectorAll('p');
+    expect(checkP.length).toEqual(2);
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    const { getByRole } = render(<About />);
+    const chechImg = getByRole('img');
+    expect(chechImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testando FavoritePokemons.js', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const checkNoFound = getByText(/No favorite pokemon found/i);
+    expect(checkNoFound).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const checkFound = getByText(/Favorite pokémons/i);
+    fireEvent.click(checkFound);
+    expect(checkFound).toBeInTheDocument();
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const details = getByText(/More details/i);
+    fireEvent.click(details);
+    const checkInput = getByRole('checkbox');
+    fireEvent.click(checkInput);
+    const ckeckFavorite = getByText(/Favorite Pokémons/);
+    fireEvent.click(ckeckFavorite);
+    const checkName = getByText(/Pikachu/i);
+    expect(checkName).toBeInTheDocument();
+  });
+});
