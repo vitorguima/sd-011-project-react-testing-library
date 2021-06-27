db34322c117e26ace662f614460697e393fe77cc
FredericoCanoeira <fcanoeira@gmail.com>
2021-06-28 00:11:34 +0100

    implements fixed set navigation link testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+describe('if on the application contains a fixed set of navigation links', () => {
+  it('The first  link must have the text Home', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link = getByText('Home').innerHTML;
+    expect(link).toBe('Home');
+  });
+
+  it('The second link must have the text About', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link = getByText('About').innerHTML;
+    expect(link).toBe('About');
+  });
+
+  it('the third link must have the text Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link = getByText('Favorite Pokémons').innerHTML;
+    expect(link).toBe('Favorite Pokémons');
+  });
 });
