d072dac376a7630bf9bf79153c09f02e884bab3f
Michel Pereira <michelbecauro@gmail.com>
2021-06-27 05:27:14 -0300

    feature/Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Testa se página contém um h2', () => {
+  const { container } = render(<NotFound />);
+
+  const img = container.querySelectorAll('img')[0];
+  expect(container.innerHTML).toMatch('Page requested not found');
+  expect(img.src).toMatch('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
