5f0232982187fcca6fc8966822c4278a0d7dac5f
Laura Gusmao <laurargus@gmail.com>
2021-06-27 19:13:37 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('<NotFound.js /> component testing', () => {
+  it('contains h2 heading with the text "Page requested not found 😭"', () => {
+    const { getByRole } = render(<NotFound />);
+    const heading = getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading.textContent).toBe('Page requested not found 😭');
+  });
+
+  it('contains image of Pokédex', () => {
+    const { container } = render(<NotFound />);
+    const image = container.querySelector('img');
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
