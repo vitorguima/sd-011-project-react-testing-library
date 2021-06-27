0aaf0f64854ea160867e60a26614bbbbc9e5aa2e
Renan <renan.vamo@gmail.com>
2021-06-27 14:27:27 -0300

    adiciona os testes do componente pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -10,7 +10,7 @@ describe('testing Pokedex component', () => {
       <Pokedex
         pokemons={ pokemons }
         isPokemonFavoriteById={ {} }
-        isFavorite={ [] }
+        isFavorite
       />,
     );
     const heading = getByText('Encountered pokémons');
@@ -22,14 +22,14 @@ describe('testing Pokedex component', () => {
       <Pokedex
         pokemons={ pokemons }
         isPokemonFavoriteById={ {} }
-        isFavorite={ [] }
+        isFavorite
       />,
     );
     pokemons.forEach((pokemon) => {
       const actualPokemonName = getByText(pokemon.name);
       expect(actualPokemonName).toBeInTheDocument();
 
-      const nextBtn = getByText('Próximo pokémon');
+      const nextBtn = getByText(/Próximo pokémon/);
       fireEvent.click(nextBtn);
 
       if (pokemon.length - 1) {
@@ -44,7 +44,7 @@ describe('testing Pokedex component', () => {
       <Pokedex
         pokemons={ pokemons }
         isPokemonFavoriteById={ {} }
-        isFavorite={ [] }
+        isFavorite
       />,
     );
 
@@ -59,4 +59,80 @@ describe('testing Pokedex component', () => {
       expect(pokemonsDisplayed).toHaveLength(1);
     });
   });
+
+  // eslint-disable-next-line max-len
+  it('verify if each type of pokemons has a button to filter, and if when user clicks, only that type should appear', () => {
+    const { getAllByTestId, getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ {} }
+        isFavorite
+      />,
+    );
+
+    const arrayOfPokemonBtns = getAllByTestId('pokemon-type-button');
+    arrayOfPokemonBtns.forEach((pokemonButton) => {
+      expect(pokemonButton).toBeInTheDocument();
+    });
+
+    const fireBtn = getByText('Fire');
+    fireEvent.click(fireBtn);
+    const charmander = getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+
+    const nextPokemonBtn = getByText('Próximo pokémon');
+    fireEvent.click(nextPokemonBtn);
+
+    const rapidash = getByText('Rapidash');
+    expect(rapidash).toBeInTheDocument();
+  });
+
+  it('verify if the text of filter buttons has the same text as pokemon types', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ {} }
+        isFavorite
+      />,
+    );
+    const arrayOfPokemonBtns = getAllByTestId('pokemon-type-button');
+
+    arrayOfPokemonBtns.forEach((button) => {
+      const buttonText = button.innerHTML;
+      const buttonSearch = pokemons.find(({ type }) => type === buttonText);
+      expect(buttonSearch).toBeTruthy();
+    });
+  });
+
+  // eslint-disable-next-line max-len
+  it('verify if contains a button \'all\' to reset the filter type, if renders all pokemons when this button is selected', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ {} }
+        isFavorite
+      />,
+    );
+    const allBtn = getByText('All');
+    expect(allBtn).toBeInTheDocument();
+
+    pokemons.forEach((pokemon) => {
+      const nextPokemonBtn = getByText(/Próximo pokémon/i);
+      const name = getByText(pokemon.name);
+      expect(name).toBeInTheDocument();
+
+      fireEvent.click(nextPokemonBtn);
+    });
+  });
+
+  // Falta esse teste
+  // it('verify if when the pokedex renders, the filter \'all\' will be selected', () => {
+  //   const { getAllByRole } = renderWithRouter(
+  //     <Pokedex
+  //       pokemons={ pokemons }
+  //       isPokemonFavoriteById={ {} }
+  //       isFavorite
+  //     />,
+  //   );
+  // });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,10 +1,52 @@
-// import React from 'react';
-// import { fireEvent } from '@testing-library/react';
-// import { renderWithRouter } from '../components';
-// import App from '../App';
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Pokemon from '../App';
+import pokemons from '../data';
 
-describe('', () => {
-  it('teste', () => {
+describe('test the Pokemon component', () => {
+  it('tests if render a pokemon card with the info about this pokemon', () => {
+    const { getByTestId, getByRole } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+    />);
+    expect(getByTestId('pokemon-name').innerHTML).toBe('Pikachu');
+    expect(getByTestId('pokemon-type').innerHTML).toBe('Electric');
+    expect(getByTestId('pokemon-weight').innerHTML).toBe('Average weight: 6.0 kg');
+    expect(getByRole('img').src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(getByRole('img').alt).toBe(`${pokemons[0].name} sprite`);
+  });
 
+  // eslint-disable-next-line max-len
+  it('tests if the card rendered has a link to show details about the pokemon and when clicked, render the correct component ', () => {
+    const { getByText, history } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+    />);
+    const moreDetailsBtn = getByText('More details');
+    expect(moreDetailsBtn).toBeInTheDocument();
+    fireEvent.mouseOver(moreDetailsBtn);
+    expect(moreDetailsBtn.href).toBe('http://localhost/pokemons/25');
+
+    fireEvent.click(moreDetailsBtn);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+
+  it('tests if a favorite pokemon renders with a star', () => {
+    const { getAllByRole, getByText } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+    />);
+
+    const moreDetailsBtn = getByText(/More details/);
+    fireEvent.click(moreDetailsBtn);
+
+    const pokemonFavoritado = getByText(/Pokémon favoritado/);
+    fireEvent.click(pokemonFavoritado);
+
+    const homeBtn = getByText('Home');
+    fireEvent.click(homeBtn);
+
+    const imgs = getAllByRole('img');
+
+    expect(imgs[1].src).toBe('http://localhost/star-icon.svg');
+    expect(imgs[1].alt).toBe(`${pokemons[0].name} is marked as favorite`);
   });
 });
