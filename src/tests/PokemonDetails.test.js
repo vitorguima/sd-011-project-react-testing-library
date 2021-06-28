fbc175744159887b55b5d586ae139e1337ea40f2
anaclarabck <ackyotoku@gmail.com>
2021-06-28 13:34:00 -0300

    Refatorei teste PokemonDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import data from '../data';
 
 const moreDetails = 'More details';
 
 describe('Teste componente <PokemonDetails.js />', () => {
   it('Renderiza detalhes do Pokémon selecionado.', () => {
-    const { getByRole, getByText } = renderWithRouter(<App />);
+    const { getByRole, getByText, getByTestId } = renderWithRouter(<App />);
     const link = getByRole('link', { name: moreDetails });
     fireEvent.click(link);
 
-    const title = getByText(/Pikachu Details/);
-    expect(title).toBeInTheDocument();
+    const pokemonName = getByTestId('pokemon-name').innerHTML;
+    const title = `${pokemonName} Details`;
+    const headingDetails = getByRole('heading', { name: title, level: 2 });
+    expect(headingDetails).toBeInTheDocument();
 
-    const heading = getByRole('heading', { name: 'Summary', level: 2 });
-    expect(heading).toBeInTheDocument();
+    const headingSummary = getByRole('heading', { name: 'Summary', level: 2 });
+    expect(headingSummary).toBeInTheDocument();
 
-    const paragraph = (/This intelligent Pokémon roasts hard berries/);
-    const summary = getByText(paragraph);
+    const paragraphPokemon = data[0].summary;
+    const summary = getByText(paragraphPokemon);
     expect(summary).toBeInTheDocument();
   });
 
@@ -27,14 +30,15 @@ describe('Teste componente <PokemonDetails.js />', () => {
     const link = getByRole('link', { name: moreDetails });
     fireEvent.click(link);
 
-    const heading = getByRole('heading', { name: 'Game Locations of Pikachu', level: 2 });
+    const gameLocations = `Game Locations of ${data[0].name}`;
+    const heading = getByRole('heading', { name: gameLocations, level: 2 });
     expect(heading).toBeInTheDocument();
 
-    const maps = getAllByRole('img', { name: 'Pikachu location' });
+    const maps = getAllByRole('img', { name: `${data[0].name} location` });
     expect(maps.length).toBe(2);
-    expect(maps[0].src).toContain('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(maps[0].src).toContain(data[0].foundAt[0].map);
 
-    const mapKanto = getByText(/Kanto Viridian Forest/);
+    const mapKanto = getByText(data[0].foundAt[0].location);
     expect(mapKanto).toBeInTheDocument();
   });
 
@@ -46,7 +50,7 @@ describe('Teste componente <PokemonDetails.js />', () => {
     const btnFavorite = getByLabelText('Pokémon favoritado?', { selector: 'input ' });
 
     fireEvent.click(btnFavorite);
-    const star = getByRole('img', { name: 'Pikachu is marked as favorite' });
+    const star = getByRole('img', { name: `${data[0].name} is marked as favorite` });
     expect(star).toBeInTheDocument();
 
     fireEvent.click(btnFavorite);
