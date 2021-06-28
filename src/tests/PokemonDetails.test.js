49ac18bc82435a7d4c428d19c7038609ff8abca6
marlon307 <marlon-ramosb@hotmail.com>
2021-06-28 14:05:42 -0300

    Rq 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+const mDetailt = 'More details';
+
+describe('Teste o componente <PokemonDetails.js />', () => {
+  it('Teste se as informações detalhadas são mostradas na tela', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(mDetailt));
+    expect(getByText(/Details/).textContent).toContain('Pikachu');
+    expect(getByText('Summary')).toBeInTheDocument();
+  });
+
+  it('Teste se existe na página uma seção com os mapas contendo as localizações', () => {
+    const { getByText, getAllByAltText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(mDetailt));
+    expect(getByText(/Game Locations of /).textContent).toContain('Pikachu');
+    expect(getAllByAltText(/location/)[0].alt).toContain('Pikachu');
+    expect(getAllByAltText(/location/)[0].src).toContain('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  });
+
+  it('Teste se existe na página uma seção com os mapas contendo as localizações', () => {
+    const { getByText, container } = renderWithRouter(<App />);
+    fireEvent.click(getByText(mDetailt));
+    const btn = container.querySelector('#favorite');
+    expect(getByText('Pokémon favoritado?')).toBeInTheDocument();
+    expect(btn.type).toBe('checkbox');
+    const section = container.querySelectorAll('section');
+    const valueText = section[1].childNodes[1].textContent;
+    expect(getByText(valueText)).toBeInTheDocument();
+  });
+});
