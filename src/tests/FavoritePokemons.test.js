5c47f74a12c8bcd0e19367d339485d3a6cda169c
Guinho Anjos <iago.anjos@hotmail.com>
2021-06-28 18:19:24 -0300

    requisito 3 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('requisito 3 - favorite pokemons', () => {
+  test('', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noFavoriteText = getByText(/No favorite pokemon found/i);
+    expect(noFavoriteText).toBeInTheDocument();
+  });
+});
