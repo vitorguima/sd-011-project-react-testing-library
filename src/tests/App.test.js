ed3c04eace32597ecb94a3c5e5eb2bea54315525
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-06-27 21:00:35 -0300

    Requisito 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,82 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testing App component', () => {
+  it('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+  it('test if Home link takes to the route `/`', () => {
+    let testLocation;
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+        <Route
+          path="*"
+          render={ ({ location }) => {
+            testLocation = location;
+            return null;
+          } }
+        />
+      </MemoryRouter>,
+    );
+    const homeLink = getByText(/home/i);
+    fireEvent.click(homeLink);
+    expect(testLocation.pathname).toBe('/');
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('test if About link takes to the route `/about`', () => {
+    let testLocation;
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+        <Route
+          path="*"
+          render={ ({ location }) => {
+            testLocation = location;
+            return null;
+          } }
+        />
+      </MemoryRouter>,
+    );
+    const aboutLink = getByText(/about/i);
+    fireEvent.click(aboutLink);
+    expect(testLocation.pathname).toBe('/about');
+  });
+
+  it('test if Favorite Pókemons takes to the route `/favorites`', () => {
+    let testLocation;
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+        <Route
+          path="*"
+          render={ ({ location }) => {
+            testLocation = location;
+            return null;
+          } }
+        />
+      </MemoryRouter>,
+    );
+    const favoritesLink = getByText('Favorite Pokémons');
+    fireEvent.click(favoritesLink);
+    expect(testLocation.pathname).toBe('/favorites');
+  });
+
+  it('test if any other path render the NotFound component', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/other'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const notFound = getByText(/not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
 });
