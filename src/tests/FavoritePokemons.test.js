7aa3615a0e53a4060985342d93b4fc5f26302a4f
Ghislaine <glenox79@gmail.com>
2021-06-28 10:59:47 -0300

    Correcao FavoritePokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,25 +1,28 @@
 import { fireEvent } from '@testing-library/dom';
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
-/* import App from '../App'; */
+import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testa o componente FavoritePokemons', () => {
   it('Verifica se é exibido a mensagem "No favorite pokemon found"', () => {
-    const { getByText, history } = renderWithRouter(<FavoritePokemons />);
-    history.push('/pagina/que-não-existe/');
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
     const notFoundFavorite = getByText(/No favorite pokemon found/i);
     expect(notFoundFavorite).toBeInTheDocument();
   });
 
   it('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />);
-    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { getByRole } = renderWithRouter(<App />);
+    const details = getByRole('link', { name: /More details/i });
+    fireEvent.click(details);
+    const favoriteCheck = getByRole('checkbox');
+    fireEvent.click(favoriteCheck);
+    const favoritePokemon = getByRole('link', { name: /Favorite Pokémons/i });
+    fireEvent.click(favoritePokemon);
   });
 
   it('Verifica se é exibido a mensagem "No favorite pokemon found"', () => {
-    const { getByText, history } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
-    history.push('/pagina/que-não-existe/');
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
     const notFoundFavorite = getByText(/No favorite pokemon found/i);
     expect(notFoundFavorite).toBeInTheDocument();
   });
