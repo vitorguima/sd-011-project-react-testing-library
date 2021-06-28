617f49b67379ee5c373df4772f7fb2837254527f
theusseveen22 <mateus.krauss@gmail.com>
2021-06-28 13:01:02 -0300

    Adiciona os dois primeiros requisitos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('Testa se existe as informações da Pokedex', () => {
+  const { getByText } = renderWithRouter(<About />);
+
+  const PokedexInfo = getByText(/This application simulates a Pokédex/i);
+  expect(PokedexInfo).toBeInTheDocument();
+});
+
+test('Testa se existe About Pokedex', () => {
+  const { getByText } = renderWithRouter(<About />);
+
+  const heading = getByText(/About Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('Testa se existe Os parágrafos', () => {
+  const { getByText } = renderWithRouter(<About />);
+
+  const textParagraphOne = getByText('This application simulates a Pokédex,'
+    + ' a digital encyclopedia containing all Pokémons');
+  expect(textParagraphOne).toBeInTheDocument();
+
+  const textParagraphTwo = getByText('One can filter Pokémons by type, '
+  + 'and see more details for each one of them');
+  expect(textParagraphTwo).toBeInTheDocument();
+});
+
+test('Testa se existe uma imagem com link https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+  const { getByRole } = render(<About />);
+
+  const img = getByRole('img');
+
+  expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,20 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('Testa se App tem a roda / e se tem os links Home, About e Favorite`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  const url = history.location.pathname;
+  expect(url).toBe('/');
+
+  const home = getByText(/Home/i);
+  expect(home).toBeInTheDocument();
+
+  const about = getByText(/About/i);
+  expect(about).toBeInTheDocument();
+
+  const favorite = getByText(/Favorite Pokémons/i);
+  expect(favorite).toBeInTheDocument();
 });
