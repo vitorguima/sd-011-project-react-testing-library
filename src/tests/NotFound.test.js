5c735f7d3486f06d95015a765724b954a2be13b3
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-06-28 19:10:08 -0300

    Testa o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o NotFound.js', () => {
+
+  test('Testa se a aplicação é a redirecionada a not found', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('/PáginaX');
+    const title = getByRole('heading', { level: 2, name: /Page requested not found/ });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Testa se página mostra a imagem certa', () => {
+    const { getAllByRole, history } = renderWithRouter(<App />);
+    history.push('/PáginaX');
+    const image = getAllByRole('img');
+    expect(image[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
