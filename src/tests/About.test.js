50a7cd2b69cd88ea8138dd4ec3c948d623ff4eb9
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-28 15:06:01 -0300

    task 1 1.0

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { About } from '../components';
+
+describe('Teste o componente About', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    const { getByRole } = render(<About />);
+    const subtitle = getByRole('heading', { level: 2 });
+    expect(subtitle).toHaveTextContent(/About Pokédex/i);
+  });
+});
