864f01fd807899e867e1b5ef98c4edf19ac6fea9
Juliana Happatsch <jhappatsch@gmail.com>
2021-06-28 19:23:53 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente Not Found', () => {
+  it('Verifica se contém um "h2" com o texto "Page requested not found 😭"', () => {
+    const { getByRole } = render(<NotFound />);
+    const heading = getByRole('heading', { level: 2 });
+    const titleText = 'Page requested not found';
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(titleText);
+  });
+
+  it('Verifica se a página mostra uma certa imagem', () => {
+    const { getByAltText } = render(<NotFound />);
+    const img = getByAltText(/pikachu/i);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img.src).toMatch(url);
+  });
+});
