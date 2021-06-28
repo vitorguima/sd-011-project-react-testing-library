faa055d91e3eac416660d0fc308e29511b702a5a
jhonatavbrg <jhonatavbrg@gmail.com>
2021-06-28 09:34:09 -0300

    Finalizado RTL do componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('', () => {
+  test(`Checks if the message No favorite pokemon found is displayed on the screen,
+  if the person does not have favorite pokemons.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    screen.getByText('No favorite pokemon found');
+  });
+
+  test('Checks if all favorite Pokemon cards are displayed.', () => {
+    const { container } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const pokemonsCards = container.querySelectorAll('.pokemon');
+    expect(pokemonsCards.length).toBe(pokemons.length);
+  });
+
+  test('Checks if no Pokemon card is displayed, if it is not favorited.', () => {
+    const { container } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const pokemonsCards = container.querySelectorAll('.pokemon');
+    expect(pokemonsCards.length).toBe(0);
+  });
+});
