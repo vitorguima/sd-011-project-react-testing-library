c1955ea8e4976ac786567fec4c61bc3f9471368f
Tulio Sploradori <tuliospl@gmail.com>
2021-06-28 07:44:19 -0300

    requisito 6 e 7

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import RenderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
+
+describe('testa a aplicação pokemon', () => {
+  it('verifica se o pokemon e renderizado', () => {
+    const { getByText, getAllByText, getByRole } = RenderWithRouter(<App />);
+    expect(getByText(/Pikachu/i)).toBeInTheDocument();
+    expect(getAllByText(/Electric/i)[0]).toBeInTheDocument();
+    expect(getByText(/Average weight: 6.0 kg/i)).toBeInTheDocument();
+    expect(getByRole('img')).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(getByRole('img')).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+  it('possui link para detalhes', () => {
+    const { getByRole, getByTestId, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByRole('button', { name: /Fire/i }));
+    expect(getByRole('link', { name: /More details/i })).toBeInTheDocument();
+    fireEvent.click(getByRole('link', { name: /More details/i }));
+    expect(history.location.pathname).toBe('/pokemons/4');
+    expect(getByTestId(/pokemon-type/i).innerHTML).toBe('Fire');
+  });
+  it('', () => {
+    const { getByRole, getAllByRole } = RenderWithRouter(<App />);
+    fireEvent.click(getByRole('button', { name: /Fire/i }));
+    fireEvent.click(getByRole('link', { name: /More details/i }));
+    fireEvent.click(getByRole('checkbox'));
+    expect(getAllByRole('img')[1].src).toContain('star-icon.svg');
+    expect(getAllByRole('img')[1].alt).toBe('Charmander is marked as favorite');
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import RenderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
+
+describe('teste da aplicação PokemonDetails', () => {
+  it('verifica informações detalhadas', () => {
+    const { getByRole, getByText, queryByText } = RenderWithRouter(<App />);
+    fireEvent.click(getByRole('link', { name: /More details/i }));
+    expect(getByText(/Details/i).innerHTML).toBe('Pikachu Details');
+    expect(queryByText(/More details/i)).not.toBeInTheDocument();
+    expect(getByRole('heading', { name: /Summary/i })).toBeInTheDocument();
+    expect(getByText(/This intelligent/i)).toBeInTheDocument();
+  });
+  it('verifica os mapas', () => {
+    const { getByRole, getAllByRole } = RenderWithRouter(<App />);
+    fireEvent.click(getByRole('link', { name: /More details/i }));
+    expect(getAllByRole('img')[1])
+      .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(getAllByRole('img')[1])
+      .toHaveAttribute('alt', 'Pikachu location');
+    expect(getAllByRole('img')[2])
+      .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(getAllByRole('img')[2])
+      .toHaveAttribute('alt', 'Pikachu location');
+  });
+  it('verifica o checkbox', () => {
+    const { getByRole,
+      getByLabelText, getAllByRole, getByText } = RenderWithRouter(<App />);
+    fireEvent.click(getByRole('link', { name: /More details/i }));
+    expect(getByLabelText(/Pokémon favoritado/i)).toBeInTheDocument();
+    fireEvent.click(getByLabelText(/Pokémon favoritado/i));
+    expect(getAllByRole('img')[1].src)
+      .toContain('star-icon.svg');
+    expect(getAllByRole('img')[1])
+      .toHaveAttribute('alt', 'Pikachu is marked as favorite');
+    expect(getByRole('heading', { name: /Game Locations of Pikachu/i }))
+      .toBeInTheDocument();
+    fireEvent.click(getByRole('link', { name: /Favorite Pokémons/i }));
+    expect(getByText(/Pikachu/i)).toBeInTheDocument();
+  });
+});
