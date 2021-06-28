95d51af4337930603a306f9f6e03feacf07636ff
lucasSCsantos <luc.cristovam10@gmail.com>
2021-06-28 14:52:34 -0300

    inicio

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import App from "../App";
 
-test('renders a reading with the text `Pokédex`', () => {
+test("renders a reading with the text `Pokédex`", () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>,
+    </MemoryRouter>
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test("shows the Pokédex when the route is `/`", () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(getByText("Encountered pokémons")).toBeInTheDocument();
 });
