b1fda14747e350c45ea46a366d1e57c371a0a051
DADA <davimarque2000@gmail.com>
2021-06-28 10:26:20 -0300

    commit 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';a
-import App from '../App';s
+import { render } from '@testing-library/react';
+import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
