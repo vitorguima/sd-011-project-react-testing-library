ad9f10195224d90f7a13233e416d2b2b5e1cc27a
Vinicius <viniciusfod@gmail.com>
2021-06-28 15:14:41 -0300

    requisitos 1-2-3-4-5-6-7

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,64 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App.js component', () => {
+  it('should render a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should render a set of navigation links in the top of the page', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const homeLink = getByText(/Home/i);
+    const aboutLink = getByText(/About/i);
+    const favoritePokemonsLink = getByText(/Favorite Pokémons/i);
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+
+  it('should redirect to home page when the Home link is clicked', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const homeButton = getByText(/Home/i);
+    fireEvent.click(homeButton);
+    const home = getByText(/Encountered pokémons/i);
+    expect(home).toBeInTheDocument();
+  });
+
+  it('should redirect to about page when the About link is clicked', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const aboutButton = getByText(/About/i);
+    fireEvent.click(aboutButton);
+    const aboutPage = getByText(/About Pokédex/i);
+    expect(aboutPage).toBeInTheDocument();
+  });
+
+  it('should redirect to favorites page when the link is clicked', () => {
+    const { getByText, getAllByText } = renderWithRouter(<App />);
+    const favoritesButton = getByText(/Favorite Pokémons/i);
+    fireEvent.click(favoritesButton);
+    const favoritesPage = getAllByText(/Favorite pokémons/i)[1];
+    expect(favoritesPage).toBeInTheDocument();
+  });
+
+  it('should redirect to Not Found page when unknown path is entered', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/unknownpath');
+    const notFoundPage = getByText(/Page requested not found/i);
+    expect(notFoundPage).toBeInTheDocument();
+  });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,103 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+describe('Pokedex.js component', () => {
+  const pokemons = [
+    {
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      foundAt: [{
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      }],
+      id: 25,
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      name: 'Pikachu',
+      summary: 'This intelligent Pokémon...',
+      type: 'Electric',
+    },
+    {
+      averageWeight: {
+        value: '2.9',
+        measurementUnit: 'kg',
+      },
+      foundAt: [{
+        location: 'Johto Route 30',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Ilex Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      }],
+      id: 10,
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      name: 'Caterpie',
+      summary: 'For protection, it releases...',
+      type: 'Bug',
+    },
+  ];
+  const isPokemonFavoriteById = {
+    25: true,
+    10: false,
+  };
+
+  it('should display a heading with specific text', () => {
+    renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />,
+    );
+    const heading = screen.getByText(/Encountered pokémons/i);
+    expect(heading).toBeVisible();
+  });
+
+  it('should show next pokemon when next button is clicked', () => {
+    renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />,
+    );
+    const nextPokemonButton = screen.getByText(/Próximo pokémon/i);
+    fireEvent.click(nextPokemonButton);
+    const nextPokemonName = screen.getByText(/Caterpie/i);
+    expect(nextPokemonName).toBeVisible();
+  });
+
+  it('should display only one pokemon at a time', () => {
+    renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />,
+    );
+    const displayedPokemons = screen.getAllByText(/Average weight/i);
+    expect(displayedPokemons).toHaveLength(1);
+  });
+
+  it('should render the filter buttons', () => {
+    renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />,
+    );
+    const filterButtons = screen.getAllByTestId('pokemon-type-button');
+    expect(filterButtons[0]).toBeVisible();
+    expect(filterButtons[filterButtons.length - 1]).toBeVisible();
+  });
+
+  it('should display only one type of pokemon when filter is applied', () => {
+    renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />,
+    );
+    const bugTypeButton = screen.getByRole('button', { name: /Bug/i });
+    fireEvent.click(bugTypeButton);
+    const bugPokemon = screen.getByText(/Caterpie/i);
+    expect(bugPokemon).toBeVisible();
+
+    const resetFilterButton = screen.getByRole('button', { name: /All/i });
+    fireEvent.click(resetFilterButton);
+    const pikachuPokemon = screen.getByText(/Pikachu/i);
+    expect(pikachuPokemon).toBeVisible();
+  });
+});
