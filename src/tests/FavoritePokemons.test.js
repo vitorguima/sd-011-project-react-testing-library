5bf95a06d1ae925452240af0f74a99ac0b37555f
Savio Moraes <smmsavio@hotmail.com>
2021-06-28 15:54:44 -0300

    Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,17 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
 describe('Requisito 03: Teste o componente <FavoritePokemons.js />', () => {
   it('', () => {
-
+    renderWithRouter(<App />);
+    const link = screen.getByText(/Favorite Pokémons/i);
+    expect(link).toBeInTheDocument();
+    userEvent.click(link);
+    const pokemonNotFound = screen.getByText(/No favorite pokemon found/i);
+    expect(pokemonNotFound).toBeInTheDocument();
   });
 
   it('', () => {
