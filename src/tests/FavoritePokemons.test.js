865a151d79e990f1bbc0f3739c90daaa1c33f029
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-28 16:00:02 -0300

    task 3 1.0!

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente FavoritePokemons', () => {
+  it('Teste caso não haja um pokemon favorito', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+});
