4f3490ebc4e5d38966e562bde6f4548156bb387d
Guinho Anjos <iago.anjos@hotmail.com>
2021-06-28 18:03:49 -0300

    consertando lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,16 +3,16 @@ import About from '../components/About';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Requisto 2 - componente About', () => {
-    test('Contém um heading h2 com o texto About Pokédex', () => {
-        const { getByText } = renderWithRouter(<About />);
-        const aboutText = getByText(/About Pokédex/i);
-        expect(aboutText).toBeInTheDocument();
-    });
-    test('Contém imagem com src específico', () => {
-      // https://github.com/tryber/sd-011-project-react-testing-library/pull/85/files - ana clara  kyotoku
-      const { getByRole } = renderWithRouter(<About />);
-      const image = getByRole('img');
-      const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-      expect(image.src).toContain(url);
-    });
-})
+  test('Contém um heading h2 com o texto About Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const aboutText = getByText(/About Pokédex/i);
+    expect(aboutText).toBeInTheDocument();
+  });
+  test('Contém imagem com src específico', () => {
+    // https://github.com/tryber/sd-011-project-react-testing-library/pull/85/files - ana clara  kyotoku
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toContain(url);
+  });
+});
