a7585ec1c38122eb9ccd4001c08d6b5659651f61
lucasSCsantos <luc.cristovam10@gmail.com>
2021-06-28 18:08:16 -0300

    func 2

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,15 @@ import './about.css';
 
 const About = () => (
   <section>
-    <h2>{ `About ` + `Pokédex` }</h2>
-    <section data-testid="pokedex-infos">
+    <h2 data-testid="about-pokedex">{ `About ` + `Pokédex` }</h2>
+    <section>
       <p>
         This application simulates a Pokédex, a
         digital encyclopedia containing all Pokémons
       </p>
       <p>One can filter Pokémons by type, and see more details for each one of them</p>
       <img
+        role="img"
         className="pokedex-image"
         src={ `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png` }
         alt="Pokédex"
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,5 +4,11 @@ import { About } from '../components';
 
 test('show infos about pokedex', () => {
   const { getByTestId } = render(<About />);
-  expect(getByTestId('pokedex-infos')).toBeInTheDocument();
+  expect(getByTestId('about-pokedex')).toBeInTheDocument();
+  expect(getByTestId('about-pokedex')).toHaveTextContent('About Pokédex');
+});
+
+test('show img', () => {
+  const { getByRole } = render(<About />);
+  expect(getByRole('img').src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
