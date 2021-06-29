31091b054497716e8201fcf8a7e376fba1d7c5d4
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-29 12:40:39 -0300

    done requirement four

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Check the functions of page Not Found', () => {
+  it('check if page contain text "Page requested not found" ', () => {
+    const { getByText, container } = renderWithRouter(<NotFound />);
+    const textHeading = getByText(/Page requested not found/i);
+    const header = container.querySelectorAll('h2');
+    expect(header.length).toBe(1);
+    expect(textHeading).toBeInTheDocument();
+  });
+
+  it('check if page show an image', () => {
+    const { container } = renderWithRouter(<NotFound />);
+    const img = container.querySelector('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
