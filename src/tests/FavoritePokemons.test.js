450901e279acbc2fb43e9e29be15be9e58b3e489
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-29 12:10:24 -0300

    done requirement three

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Check the functions of page pokemons favorites', () => {
+  it('show message No favorite pokemon found on the page', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const text = getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('check the cards favorites', () => {
+    const { getAllByTestId } = renderWithRouter(<FavoritePokemons
+      pokemons={ pokemons }
+    />);
+    expect(getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+  });
+});
