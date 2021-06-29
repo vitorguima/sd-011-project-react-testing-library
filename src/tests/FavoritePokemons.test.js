c740d17c7754f56d64108cc79a328ca2922f965f
lucasSCsantos <luc.cristovam10@gmail.com>
2021-06-28 22:22:53 -0300

    massa

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+test('when theres no pokemon', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
+
+test('when theres no pokemon', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+test('show page note found', () => {
+  const { getByRole } = render(<NotFound />);
+  const text = getByRole('heading', { name: /Page requested not found Crying emoji/i });
+  expect(text).toBeInTheDocument();
+});
+
+test('show image', () => {
+  const { getByAltText } = render(<NotFound />);
+  expect(getByAltText('Pikachu crying because the page requested was not found').src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
