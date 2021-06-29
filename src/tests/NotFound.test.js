835f8a8bb2f59f91d61896f1db7628a92bab6992
Rafael Schmitz <schimitaum@gmail.com>
2021-06-29 20:07:39 -0300

    5 solved

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 import NotFound from '../components/NotFound';
 
 describe('Requisito 4 - Teste o componente <Not Found/>', () => {
