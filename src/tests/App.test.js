f683754cee97cca46f67f67db6c8a9dd841d11e0
IgorMendez <igor.emp@hotmail.com>
2021-06-29 07:14:53 -0300

    Iniciando projeto / Exercício App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('App tests', () => {
+  it('Home must render when click on /', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByRole('link', { name: 'Home' }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('should there link home, about and Favorite Pokémons on the top', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const links = screen.getAllByRole('link');
+    expect(links[0].innerHTML).toBe('Home');
+    expect(links[1].innerHTML).toBe('About');
+    expect(links[2].innerHTML).toBe('Favorite Pokémons');
+  }
+  )
+}
+)
+
