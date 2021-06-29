ea8cecc7b86f88c44cd426dd4870d9c0b77ad4fc
lucasSCsantos <luc.cristovam10@gmail.com>
2021-06-29 17:05:14 -0300

    magulhe

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { Pokemon } from '../components';
+import pokemons from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+test('when theres no pokemon', () => {
+  const { getByTestId, getByAltText, getByText } = renderWithRouter(
+    <Pokemon pokemon={ pokemons[1] } isFavorite />,
+  );
+  const { image, name, averageWeight, type, id } = pokemons[1];
+  const { value, measurementUnit } = averageWeight;
+  expect(getByTestId('pokemon-name')).toHaveTextContent(name);
+  expect(getByTestId('pokemon-type')).toHaveTextContent(type);
+  expect(getByTestId('pokemon-weight')).toHaveTextContent(`Average weight: 
+  ${value} ${measurementUnit}`);
+  expect(getByAltText(`${name} sprite`).src).toBe(image);
+  expect(getByAltText(`${name} is marked as favorite`).src).toBe('http://localhost/star-icon.svg');
+  expect(getByText('More details').href).toBe(`http://localhost/pokemons/${id}`);
+});
