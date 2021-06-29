7ecddf1123ed388105a539faa8a629724745b705
DADA <davimarque2000@gmail.com>
2021-06-29 11:01:45 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Favorite from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste o componente FavoritePokemons.js', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found',
+    () => {
+      const { getByText } = render(<Favorite />);
+      const notFound = getByText(/No favorite pokemon found/);
+      expect(notFound).toBeInTheDocument();
+    });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { getByText, getByRole, history, getAllByTestId } = renderWithRouter(<App />);
+    history.push('/pokemons/143');
+    const checkSnorlax = getByRole('checkbox');
+    const favPokes = getByText(/Favorite Pokémons/);
+    fireEvent.click(checkSnorlax);
+    fireEvent.click(favPokes);
+    const nameCard = getAllByTestId('pokemon-name');
+    expect(nameCard.length).toBe(1);
+    history.push('/pokemons/65');
+    const checkAlakazam = getByRole('checkbox');
+    fireEvent.click(checkAlakazam);
+    fireEvent.click(favPokes);
+    const nameCards = getAllByTestId('pokemon-name');
+    expect(nameCards.length).toBe(2);
+  });
+});
