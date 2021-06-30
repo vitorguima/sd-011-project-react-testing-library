8af8b92a1d4a7e18b9a0be0843e44e8d50f418d7
Guinho Anjos <iago.anjos@hotmail.com>
2021-06-30 15:38:54 -0300

    requisito 5 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,12 +3,12 @@ import NotFound from '../components/NotFound';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Requisito 4 - componente NotFound', () => {
-  test('test1 ', () => {
+  test('Contém h2 com texto ', () => {
     const { getByText } = renderWithRouter(<NotFound />);
     const h2text = getByText(/Page requested not found/i);
     expect(h2text).toBeInTheDocument();
   });
-  test('test2', () => {
+  test('Contém imagem com src especifico', () => {
     const { queryAllByRole } = renderWithRouter(<NotFound />);
     const imagem = queryAllByRole('img');
     expect(imagem[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+const PokemonNameID = 'pokemon-name';
+
+describe('Requisito 5 - componente Pokedex', () => {
+  test('Contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const h2text = getByText(/Encountered pokémons/i);
+    expect(h2text).toBeInTheDocument();
+  });
+  test('É exibido o próximo Pokémon da lista quando o botão', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    const buttonNext = getByText(/Próximo pokémon/i);
+    const pokemonName1 = getByTestId(PokemonNameID);
+    fireEvent.click(buttonNext);
+    const pokemonName2 = getByTestId(PokemonNameID);
+    expect(pokemonName1).not.toContain(pokemonName2);
+  });
+  test('A Pokédex contém um botão para resetar o filtro', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const buttonAll = getByRole('button', { name: 'All' });
+    expect(buttonAll).toBeInTheDocument();
+  });
+  test('Botão All limpa todos os filtros', () => {
+    const { getByRole, getByTestId } = renderWithRouter(<App />);
+    const buttonAll = getByRole('button', { name: 'All' });
+    fireEvent.click(buttonAll);
+    const pokemonName = getByTestId(PokemonNameID);
+    expect(pokemonName).toHaveTextContent('Pikachu');
+  });
+  test('Contém butões de tipo', () => {
+    const { queryAllByTestId } = renderWithRouter(<App />);
+    const buttonsType = queryAllByTestId('pokemon-type-button');
+    buttonsType.forEach((buttonType) => {
+      expect(buttonType).toBeInTheDocument();
+    });
+  });
+  test('', () => {
+    const { queryAllByTestId, getByTestId } = renderWithRouter(<App />);
+    const buttonsType = queryAllByTestId('pokemon-type-button');
+    const buttonNext = getByTestId('next-pokemon');
+    buttonsType.forEach((button) => {
+      if (button.innerHTML === 'Electric') {
+        fireEvent.click(button);
+      }
+    });
+    expect(buttonNext.disabled).toBe(true);
+  });
+});
