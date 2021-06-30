44e3543b28e768ad12ec9b8645e83a05a5671a78
nandovbr <nandovbr15@hotmail.com>
2021-06-30 11:48:08 -0300

    adiciona os testes do App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
-import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('testes componente App.js', () => {
@@ -18,5 +18,39 @@ describe('testes componente App.js', () => {
     expect(home).toBeInTheDocument();
     expect(about).toBeInTheDocument();
     expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  it('testa se a aplicação é direcionada para "/" ao clicar em Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText(/home/i);
+
+    fireEvent.click(home);
+    const urlHome = history.location.pathname;
+    expect(urlHome).toBe('/');
+  });
+
+  it('testa se a aplicação é direcionada para "/about" ao clicar em About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText(/about/i);
+
+    fireEvent.click(about);
+    const urlAbout = history.location.pathname;
+    expect(urlAbout).toBe('/about');
+  });
+
+  it('testa se é direcionada para "/favorites" ao clicar em Pokémons Favoritados', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoritePokemons = getByText(/favorite pokémons/i);
+
+    fireEvent.click(favoritePokemons);
+    const urlfavorite = history.location.pathname;
+    expect(urlfavorite).toBe('/favorites');
+  });
+
+  it('testa se é direcionada para Not Found ao entrar em uma URL desconhnecida', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe');
+    const notFound = getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
   });
 });
