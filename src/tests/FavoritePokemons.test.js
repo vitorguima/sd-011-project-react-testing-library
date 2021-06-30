18bf05646991eddd6d3ac45a48820af120249fc7
Nikolas <nikolas.silva1990@gmail.com>
2021-06-30 18:16:05 -0300

    Requisito 3 concluído

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+test('Testa se é exibido No favorite pokemon found, se não tiver favoritos.', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const favoritePoke = getByText(/Favorite pokémons/i);
+  fireEvent.click(favoritePoke);
+  const message = getByText('No favorite pokemon found');
+  expect(message).toBeInTheDocument();
+});
+
+test('Testa se são exibidos todos os cards de pokémons favoritados', () => {
+  const { container } = render(
+    <FavoritePokemons />,
+  );
+  const favorites = container.querySelectorAll('.favorite-pokemons');
+  expect(favorites.length === 0).toBe(true);
+});
+
+test('Testa se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+  const { container } = render(
+    <FavoritePokemons />,
+  );
+  const favorites = container.querySelectorAll('.favorite-pokemon');
+  expect(favorites.length === 0).toBe(true);
+});
