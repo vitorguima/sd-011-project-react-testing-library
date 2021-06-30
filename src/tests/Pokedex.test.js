55cffae3fb652e524d355b93f2718ae3c8afb111
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-06-30 12:11:17 -0300

    atualizando packagejson

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,35 +1,97 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-// import renderWithRouter from '../services/renderWithRouter';
-import Pokedex from '../components/Pokedex';
-// import App from '../App';
+import { MemoryRouter } from 'react-router-dom';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
 
-// describe('Teste o componente <Pokedex.js />', () => {
-test('se página contém um heading h2 com o texto Encountered pokémons', () => {
-  const { getByText, getByRole } = render(<Pokedex />);
-  const heading = getByRole(/heading/);
-  const headingENcPokemons = getByText(/Encountered pokémons/);
-  expect(headingENcPokemons).toBeInTheDocument();
-  expect(heading).toBeInTheDocument();
+describe('Teste o componente <Pokedex.js />', () => {
+  test('se página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByText, getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getAllByRole(/heading/)[1];
+    const headingEncPokemons = getByText(/Encountered pokémons/);
+    expect(headingEncPokemons).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('se é exibido o próximo Pokémon quando o botão Próximo pokémon é clicado.', () => {
+    const { getByAltText, getByTestId } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    // vê qual é o pokemon atual
+    const pikachu = getByAltText(/Pikachu sprite/);
+    expect(pikachu).toBeInTheDocument();
+    // localiza o botão
+    const linkElement = getByTestId(/next-pokemon/);
+    // clica no mesmo
+    fireEvent.click(linkElement);
+    // verifica se o Pikachu foi substituido pelo Charmander
+    const charmander = getByAltText(/Charmander sprite/);
+    expect(charmander).toBeInTheDocument();
+    // clica no mesmo
+    fireEvent.click(linkElement);
+    // verifica se o Pikachu foi substituido pelo Charmander
+    const caterpie = getByAltText(/Caterpie sprite/);
+    expect(caterpie).toBeInTheDocument();
+  });
+
+  test('se é mostrado apenas um Pokémon por vez', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    // vê se o pokemon atual é o pikachu
+    const pikachu = getByText(/Pikachu/);
+    expect(pikachu).toBeInTheDocument();
+  });
+
+  test('se a Pokédex tem os botões de filtro', () => {
+    const { queryAllByTestId, history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    const buttons = queryAllByTestId(/pokemon-type-button/);
+    const SEVEN = 7;
+    expect(buttons.length).toBe(SEVEN);
+  });
+
+  test('se a Pokédex contém um botão para resetar o filtro', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const allButton = getByText(/All/);
+    expect(allButton).toBeInTheDocument();
+    expect(allButton).not.toBeDisabled();
+  });
+
+  test('se é criado, dinamicamente, um botão de filtro para cada tipo de Pokémon', () => {
+    const allTypes = [
+      'Electric',
+      'Fire',
+      'Bug',
+      'Poison',
+      'Psychic',
+      'Normal',
+      'Dragon',
+    ];
+    allTypes.forEach((type, index) => {
+      const { queryAllByText } = renderWithRouter(<App />);
+      const pokemonType = queryAllByText(`${type}`)[index];
+      expect(pokemonType).toBeInTheDocument();
+      expect(pokemonType).not.toBeDisabled();
+    });
+  });
+
+  test('se o botão de "Próximo" é desabilitado quando a lista tiver um pokémon', () => {
+    const { getByText } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    const nextPokemon = getByText(/Próximo pokémon/);
+    expect(nextPokemon).toBeDisabled();
+  });
 });
-test('se é exibido o próximo Pokémon quando o botão Próximo pokémon é clicado.', () => {
-  const { getByText, getByRole } = render(<Pokedex />);
-  // vê qual é o pokemon atual
-  const pikachu = getByText(/Pikachu/);
-  expect(pikachu).toBeInTheDocument();
-  // localiza o botão
-  const linkElement = getByRole(/Próximo pokémon/);
-  // clica no mesmo
-  fireEvent.click(linkElement);
-  // verifica se o Pikachu foi substituido pelo Charmander
-  expect(pikachu).not.toBeInTheDocument();
-  const charmander = getByText(/Charmander/);
-  expect(charmander).toBeInTheDocument();
-  // clica no mesmo
-  fireEvent.click(linkElement);
-  // verifica se o Pikachu foi substituido pelo Charmander
-  expect(charmander).not.toBeInTheDocument();
-  const caterpie = getByText(/Caterpie/);
-  expect(caterpie).toBeInTheDocument();
-});
-// });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,9 +4,59 @@ import renderWithRouter from '../services/renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
 
-describe('Teste o componente <FavoritePokemons.js />', () => {
-  test('se é exibido "No favorite pokemon found" se não tiver pokémons favoritos', () => {
-    const { getByText } = render(<FavoritePokemons />);
-    const noFavorite = getByText(/No favorite pokemon found/i);
-    expect(noFavorite).toBeInTheDocument();
+describe('Teste o componente <Pokemon.js />', () => {
+  test('se é renderizado um card com as informações de determinado pokémon', () => {
+    const { getByText, getByTestId, getAllByTestId } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    const snorlax = getByTestId(/pokemon-name/);
+    expect(snorlax).toBeInTheDocument();
+    const type = getAllByTestId(/pokemon-type/)[0];
+    expect(type).toBeInTheDocument();
+    const weight = getByTestId(/pokemon-weight/);
+    expect(weight).toBeInTheDocument();
   });
+
+  test('se o card do Pokémon contém um link de navegação para exibir detalhes', () => {
+    const { getByText } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    // localiza o botão de detalhes
+    const moreDetails = getByText(/More details/);
+    expect(moreDetails).toBeInTheDocument();
+  });
+
+  test('se ao clicar no link é redirecionado para a página de detalhes', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    // localiza o botão de detalhes
+    const moreDetails = getByText(/More details/);
+    fireEvent.click(moreDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/143');
+    const details = getByText(/Snorlax Details/);
+    expect(details).toBeInTheDocument();
+  });
+
+  test('se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    // localiza o botão de detalhes
+    const moreDetails = getByText(/More details/);
+    fireEvent.click(moreDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/143');
+    const details = getByText(/Snorlax Details/);
+    expect(details).toBeInTheDocument();
+  });
+});
