3f3c5b8b5e9a01775c2e8fa34042ecb3ae6d73b5
tomsantos07 <xotmsantosx@live.com>
2021-06-30 11:15:54 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+// import { getByRole } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './RenderWithRouter';
+
+test('Testa se página contém um `h2` com o texto `Page requested not found 😭`', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const heading2 = getByRole('heading', {
+    level: 2,
+  });
+  expect(heading2).toHaveTextContent('Page requested not found');
+});
+
+test('Testa se página mostra a imagem `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`', () => {
+  renderWithRouter(<NotFound />);
+  const notFoundImage = document.querySelector('img');
+
+  expect(notFoundImage.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
