ca10a4b55a13967a0aab29a5c68dd8e7f4aa3cd8
DADA <davimarque2000@gmail.com>
2021-06-30 16:24:50 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste dos componentes do <Pokedex.js/>',
+  () => {
+    it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+      const { getByRole } = renderWithRouter(<App />);
+      const h2 = getByRole('heading', { level: 2, name: /Encountered pokémons/ });
+      expect(h2).toBeInTheDocument();
+    });
+
+    it('É exibido o próximo Pokémon da lista quando o botão Próximo é clicado', () => {
+      const { getByTestId, getByAltText } = renderWithRouter(<App />);
+      const button = getByTestId('next-pokemon');
+      fireEvent.click(button);
+      const charmander = getByAltText('Charmander sprite');
+      expect(charmander).toBeInTheDocument();
+
+      fireEvent.click(button);
+      const caterpie = getByAltText('Caterpie sprite');
+      expect(caterpie).toBeInTheDocument();
+
+      fireEvent.click(button);
+      const ekans = getByAltText('Ekans sprite');
+      expect(ekans).toBeInTheDocument();
+
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+
+      const pikachu = getByAltText('Pikachu sprite');
+      expect(pikachu).toBeInTheDocument();
+    });
+
+    it('Teste se é mostrado apenas um Pokémon por vez', () => {
+      const { getAllByTestId } = renderWithRouter(<App />);
+      const pokemon = getAllByTestId('pokemon-name');
+      expect(pokemon.length).toBe(1);
+    });
+
+    it('Teste se a Pokédex tem os botões de filtro', () => {
+      function verrifyFilter(element) {
+        const { getAllByRole, getAllByTestId } = renderWithRouter(<App />);
+        const fireButton = getAllByRole('button', { name: element });
+        fireEvent.click(fireButton[0]);
+        const fireType = getAllByTestId('pokemon-type');
+        expect(fireType[0].innerHTML).toBe(element);
+      }
+      verrifyFilter('Fire');
+      verrifyFilter('Poison');
+      verrifyFilter('Bug');
+      verrifyFilter('Dragon');
+      verrifyFilter('Psychic');
+      verrifyFilter('Electric');
+      verrifyFilter('Normal');
+    });
+  });
