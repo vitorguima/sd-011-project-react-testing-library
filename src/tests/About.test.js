219bd0a13909ad4881eefc984d64e874e697e6d8
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-07-01 14:45:26 -0300

    Requisito 02 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('page contains information about Pokédex', () => {
+  const { getByText } = render(<About />);
+
+  const informations = 'This application simulates a Pokédex, '
+  + 'a digital encyclopedia containing all Pokémons';
+
+  const informations2 = 'One can filter Pokémons by type, '
+  + 'and see more details for each one of them';
+
+  const pokedexInformations = getByText(informations);
+  const pokedexInformations2 = getByText(informations2);
+
+  expect(pokedexInformations).toBeInTheDocument();
+  expect(pokedexInformations2).toBeInTheDocument();
+});
+
+test('contains `h2` with text `About Pokédex`', () => {
+  const { getByText } = render(<About />);
+
+  const titleAbout = getByText(/About Pokédex/);
+
+  expect(titleAbout).toBeInTheDocument();
+});
+
+test('image with correct URL', () => {
+  const { getByAltText } = render(<About />);
+
+  const image = getByAltText(/Pokédex/i);
+  const imageUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+  expect(image).toHaveAttribute('src', imageUrl);
+});
