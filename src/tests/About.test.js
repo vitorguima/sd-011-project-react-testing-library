20e2fc3e308b994e2be81621d4a4b0c6135968f5
Michelle <mls.srbek@gmail.com>
2021-07-01 17:16:29 -0300

    req 2 About component

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+it('About page has two paragraphs and informations about Pokédex', () => {
+  const { getByText } = renderWithRouter(<About />);
+  expect(getByText(/This application simulates a Pokédex, a digit/i)).toBeInTheDocument();
+  expect(getByText(/One can filter Pokémons by type, and see more/i)).toBeInTheDocument();
+});
+
+it('About page contains title with `About Pokédex`', () => {
+  const { getByText } = renderWithRouter(<About />);
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
