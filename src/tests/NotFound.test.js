21b25f6f5c95c316cc545edfdf89cc6f50c54e67
Michelle <mls.srbek@gmail.com>
2021-07-01 18:23:32 -0300

    req 4 NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+it('contains message `Page requested not found`', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+});
+
+it('cotains gif', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  expect(getByAltText('Pikachu crying because the page requested was not found')).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
