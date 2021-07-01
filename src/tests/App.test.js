079156a9f786f04ac35389647d81bff266a07049
Michelle <mls.srbek@gmail.com>
2021-07-01 16:31:38 -0300

    render links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,5 +12,21 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+it('The page should render the home, About and Favorite Pokemons links.', () => {
+  const { getByText } = renderWithRouter(<App />);
+  expect(getByText('Home')).toBeInTheDocument();
+  expect(getByText('About')).toBeInTheDocument();
+  expect(getByText('Favorite Pokémons')).toBeInTheDocument();
 });
