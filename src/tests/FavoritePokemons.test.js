07ec997f87fd2790becd93d5b378a1b780091e91
Cris Lustosa <cmlustosa@gmail.com>
2021-07-01 18:33:55 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+test('No favorite pokemon found deve aparecer na tela', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
+
+test('A página deve exibir todos os cards de pokémons favoritados', () => {
+  const { getByText, getByLabelText, getAllByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  localStorage.clear();
+  fireEvent.click(getByText(/bug/i));
+  fireEvent.click(getByText(/more details/i));
+  fireEvent.click(getByLabelText(/pokémon favoritado/i));
+  fireEvent.click(getByText(/home/i));
+  fireEvent.click(getByText(/fire/i));
+  fireEvent.click(getByText(/more details/i));
+  fireEvent.click(getByLabelText(/pokémon favoritado/i));
+  fireEvent.click(getByText(/Favorite pokémons/i));
+  expect(getAllByText(/More details/i).length).toBe(2);
+});
