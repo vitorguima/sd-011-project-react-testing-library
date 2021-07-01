ce038b751bf9687fc6bec6070a6cd5f82c6be04d
JoaoCabrera <joaocabrerati@gmail.com>
2021-07-01 17:17:12 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Test Not Found page', () => {
+  const { getByRole, getAllByRole } = render(<NotFound />);
+
+  expect(getByRole('heading',
+    { level: 2, name: /Page requested not found/i })).toBeInTheDocument();
+
+  expect(getAllByRole('img')[1].src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
