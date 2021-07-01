3acb829741d0718b12a02e04a5886f341c9d2462
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-07-01 18:36:25 -0300

    start project | RTL

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,3 +2,13 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
