f018f043f2a4b3e8745f555bd7941f437334ac0c
Michelle <mls.srbek@gmail.com>
2021-07-01 17:40:43 -0300

    FavoritePokemons message not found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+import React from 'react';
+import FavotitePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+it('render message `No favorite pokemon found`', () => {
+  const { getByText } = renderWithRouter(<FavotitePokemons />);
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
