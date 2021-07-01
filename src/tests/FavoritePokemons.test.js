b80db97c19703b4c9ffd50cba5ef7f149c4e1f6f
JoaoCabrera <joaocabrerati@gmail.com>
2021-07-01 16:27:18 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('test page favorite pokemons', () => {
+  const { getByText } = render(<FavoritePokemons />);
+
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
