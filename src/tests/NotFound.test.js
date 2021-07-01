4ffb1de0b28b7a06c7a57ed997e607149f631947
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-07-01 16:50:48 -0300

    Requisito 04 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('the `h2` with text `Page requested not found 😭`', () => {
+  const { getByText } = render(<NotFound />);
+  const heading = getByText(/Page requested not found/);
+
+  expect(heading).toBeInTheDocument();
+});
+
+test('gif with correct URL', () => {
+  const { getByAltText } = render(<NotFound />);
+  const gif = getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(gif).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
