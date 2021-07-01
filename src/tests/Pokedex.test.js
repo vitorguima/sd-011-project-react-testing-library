a2a5e05bfa28d9951a10fa739453c7faebb64b68
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-01 11:03:03 -0300

    pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -11,33 +11,39 @@ describe('Teste o componente <Pokedex.js />', () => {
         <App />
       </MemoryRouter>,
     );
-    const heading = getAllByRole(/heading/)[1];
-    const headingEncPokemons = getByText(/Encountered pokémons/);
-    expect(headingEncPokemons).toBeInTheDocument();
-    expect(heading).toBeInTheDocument();
+    const heading = getAllByRole(/heading/i);
+    const title = getByText(/Encountered pokémons/i);
+    expect(title).toBeInTheDocument();
+    expect(heading[1]).toBeInTheDocument();
   });
 
-  test('se é exibido o próximo Pokémon quando o botão Próximo pokémon é clicado.', () => {
+  test('se é exibido o próximo Poke. quando o "botão Próximo pokémon" é clicado.', () => {
     const { getByAltText, getByTestId } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
     // vê qual é o pokemon atual
-    const pikachu = getByAltText(/Pikachu sprite/);
+    const pikachu = getByAltText(/Pikachu sprite/i);
     expect(pikachu).toBeInTheDocument();
     // localiza o botão
-    const linkElement = getByTestId(/next-pokemon/);
+    const linkElement = getByTestId(/next-pokemon/i);
     // clica no mesmo
     fireEvent.click(linkElement);
     // verifica se o Pikachu foi substituido pelo Charmander
-    const charmander = getByAltText(/Charmander sprite/);
+    const charmander = getByAltText(/Charmander sprite/i);
     expect(charmander).toBeInTheDocument();
     // clica no mesmo
     fireEvent.click(linkElement);
-    // verifica se o Pikachu foi substituido pelo Charmander
-    const caterpie = getByAltText(/Caterpie sprite/);
+    // verifica se o Charmander foi substituido pelo Caterpie
+    const caterpie = getByAltText(/Caterpie sprite/i);
     expect(caterpie).toBeInTheDocument();
+    // verifica se o Charmander foi substituido pelo Caterpie
+    const SIX = 6;
+    for (let i = 0; i < SIX; i += 1) {
+      fireEvent.click(linkElement);
+    }
+    expect(pikachu).toBeInTheDocument();
   });
 
   test('se é mostrado apenas um Pokémon por vez', () => {
@@ -47,7 +53,7 @@ describe('Teste o componente <Pokedex.js />', () => {
       </MemoryRouter>,
     );
     // vê se o pokemon atual é o pikachu
-    const pikachu = getByText(/Pikachu/);
+    const pikachu = getByText(/Pikachu/i);
     expect(pikachu).toBeInTheDocument();
   });
 
@@ -55,16 +61,19 @@ describe('Teste o componente <Pokedex.js />', () => {
     const { queryAllByTestId, history } = renderWithRouter(<App />);
     const { pathname } = history.location;
     expect(pathname).toBe('/');
-    const buttons = queryAllByTestId(/pokemon-type-button/);
+    const buttons = queryAllByTestId(/pokemon-type-button/i);
     const SEVEN = 7;
     expect(buttons.length).toBe(SEVEN);
   });
 
   test('se a Pokédex contém um botão para resetar o filtro', () => {
-    const { getByText } = renderWithRouter(<App />);
-    const allButton = getByText(/All/);
+    const { getByText, getByAltText } = renderWithRouter(<App />);
+    const allButton = getByText(/All/i);
     expect(allButton).toBeInTheDocument();
     expect(allButton).not.toBeDisabled();
+    fireEvent.click(allButton);
+    const pikachu = getByAltText(/Pikachu sprite/i);
+    expect(pikachu).toBeInTheDocument();
   });
 
   test('se é criado, dinamicamente, um botão de filtro para cada tipo de Pokémon', () => {
@@ -88,7 +97,7 @@ describe('Teste o componente <Pokedex.js />', () => {
   test('se o botão de "Próximo" é desabilitado quando a lista tiver um pokémon', () => {
     const { getByText } = renderWithRouter(<App />);
     // localiza o botão
-    const normaltype = getByText(/Normal/);
+    const normaltype = getByText(/Normal/i);
     // clica no mesmo
     fireEvent.click(normaltype);
     const nextPokemon = getByText(/Próximo pokémon/i);
