7c04f1d928105c9f91dd191de563d6aca7bf6212
Reginara Gomes <refagomes@gmail.com>
2021-06-30 21:57:14 -0300

    Adiciona req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React, { PureComponent } from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import { FavoritePokemons } from '../components';
+
+describe ('Teste componente <FavoritePokemons.js />', () => {
+  test('Se é exibido a msg No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const favPokemons = getByText('No favorite pokemon found');
+    expect(favPokemons).toBeInTheDocument();
+  });
+
+  test('Se são exibidos todos os cards favoritados', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favPokemons = getByText('Favorite Pokémons');
+    fireEvent.click(favPokemons);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+
+})
