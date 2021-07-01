50be84e65a4cfabd5eba60c7faa363d91718f729
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-01 12:09:12 -0300

    pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -11,8 +11,11 @@ describe('Teste o componente <Pokemon.js />', () => {
     const normaltype = getByText(/Normal/);
     // clica no mesmo
     fireEvent.click(normaltype);
+    const name = getByText(/Snorlax/);
+    expect(name).toBeInTheDocument();
     const snorlax = getByTestId(/pokemon-name/);
     expect(snorlax).toBeInTheDocument();
+    expect(normaltype).toBeInTheDocument();
     const type = getAllByTestId(/pokemon-type/)[0];
     expect(type).toBeInTheDocument();
     const weight = getByTestId(/pokemon-weight/);
@@ -45,9 +48,9 @@ describe('Teste o componente <Pokemon.js />', () => {
     expect(details).toBeInTheDocument();
   });
 
-  test('se existe um ícone de estrela nos Pokémons favoritadoso', () => {
+  test('se a URL muda pra /pokemon/<id>, id sendo do Pokémon cujos se deseja ver', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    // localiza o botão0
+    // localiza o botão
     const normaltype = getByText(/Normal/);
     // clica no mesmo
     fireEvent.click(normaltype);
@@ -55,8 +58,67 @@ describe('Teste o componente <Pokemon.js />', () => {
     const moreDetails = getByText(/More details/);
     fireEvent.click(moreDetails);
     const { pathname } = history.location;
-    expect(pathname).toBe('/pokemoĩkns/143');
-    const details = getByText(/Snorlax Details/);
-    expect(details).toBeInTheDocument();
+    expect(pathname).toBe('/pokemons/143');
+  });
+
+  test('se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { getByText, getByAltText, getByRole } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/i);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    // localiza o botão de detalhes
+    const moreDetails = getByText(/More details/i);
+    fireEvent.click(moreDetails);
+    // marca como favorito
+    const checkbox = getByRole(/checkbox/i);
+    expect(checkbox).toBeInTheDocument();
+    fireEvent.click(checkbox);
+    // localiza a estrela de favorito
+    const favoritePokemon = getByAltText(/Snorlax is marked as favorite/i);
+    expect(favoritePokemon).toBeInTheDocument();
+    expect(favoritePokemon.src).toContain('/star-icon.svg');
+  });
+
+  test('se aparece a imagem do pokemon', () => {
+    const { getByText, getByAltText } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/i);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    // localiza o botão de detalhes
+    const moreDetails = getByText(/More details/i);
+    fireEvent.click(moreDetails);
+    // verifica a presença da imagem pelo alt
+    const image = getByAltText(/Snorlax sprite/);
+    expect(image).toBeInTheDocument();
+  });
+
+  test('se aparece a imagem do pokemon a partir do src', () => {
+    const { getByText, getByAltText } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/i);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    // localiza o botão de detalhes
+    const moreDetails = getByText(/More details/i);
+    fireEvent.click(moreDetails);
+    // verifica o src pelo alt
+    const image = getByAltText(/Snorlax sprite/);
+    expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png');
+  });
+
+  test('se aparece o Average weight', () => {
+    const { getByText } = renderWithRouter(<App />);
+    // localiza o botão
+    const normaltype = getByText(/Normal/i);
+    // clica no mesmo
+    fireEvent.click(normaltype);
+    // localiza o botão de detalhes
+    const moreDetails = getByText(/More details/i);
+    fireEvent.click(moreDetails);
+    // verifica o src pelo alt
+    const AverageWeight = getByText(/Average weight: 460.0 kg/);
+    expect(AverageWeight).toBeInTheDocument();
   });
 });
