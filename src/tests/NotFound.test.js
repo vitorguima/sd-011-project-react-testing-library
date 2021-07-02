9f004fa0f16829c6352d327a85072c972623f979
FredericoCanoeira <fcanoeira@gmail.com>
2021-07-02 17:37:09 +0100

    implement <NotFound.js /> component test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Test the <NotFound.js /> component', () => {
+  it('Test if page contains an h2 and the text Page requested not found', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const messageDisplay = getByRole('heading', { level: 2 });
+
+    expect(messageDisplay).toHaveTextContent(/Page requested not found/i);
+  });
+
+  it('Test if image not found', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const notFoundIcon = getByAltText(/Pikachu crying/i);
+
+    expect(notFoundIcon).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
