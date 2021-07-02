226bdb0cd065790c3111d09a41088ce0f660a0e3
Diego Figueirêdo <8kball@gmail.com>
2021-07-02 13:13:34 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Component <App />', () => {
+  it('Testa se a página inicial é renderizada no endpoint "/"', () => {
+    const { history: { location: { pathname } } } = renderWithRouter(<App />);
+    expect(pathname).toBe('/');
+  });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  it('Testa se página possui links específicos', () => {
+    const { getByText } = renderWithRouter(<App />);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  it('Testa se a página muda para / ao clicar no link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText('Home');
+
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Testa se a página muda para /about ao clicar no link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText('About');
+
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Testa se a página muda para /favorites ao clicar no link Favorite Pokémons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoritePokemons = getByText('Favorite Pokémons');
+
+    fireEvent.click(favoritePokemons);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Testa se a página renderiza NotFound ao digitar um endpoint desconhecido', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('asd');
+
+    const notFound = getByText(/Page requested not found/);
+    expect(notFound).toBeInTheDocument();
+  });
 });
