d0ce86c4392dac245569927151274d65015742a6
Priscila Scapin <prih.scapin@gmail.com>
2021-07-02 12:30:45 -0300

    Bora pro redux

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import { Pokedex } from '../components';
+import pokemons from '../data';
+
+const pokeMocks = {};
+
+const length = 7;
+
+test('Teste 1: testa renderização do h2', () => {
+  // é necessário mockar o objeto que seria retornado (com pokémons encontrados) pois a renderização só acontece após o retorno.
+  // const pokeMocks = {};
+  const { getByRole } = renderWithRouter(
+    <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ pokeMocks } />,
+  );
+
+  const headling = getByRole('heading',
+    { name: /Encountered pokémons/i });
+
+  expect(headling).toBeInTheDocument();
+});
+
+test('Teste 2: textos e primeiro teste de botão - sem fire event', () => {
+  const { getByText, getAllByTestId } = renderWithRouter(
+    <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ pokeMocks } />,
+  );
+  expect(getByText('All')).toBeInTheDocument();
+  expect(getByText('Próximo pokémon')).toBeInTheDocument();
+  // foi necessário mockar um length fake pro teste pensar que estou percorrendo os pokemons de verdade.
+  expect(getAllByTestId('pokemon-type-button').length).toBe(length);
+});
+
+// ***refazer*/
+test('Teste 3: fire event', () => {
+  const { getByRole, getByText } = renderWithRouter(
+    <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ pokeMocks } />,
+  );
+  pokemons.map((pokemon) => {
+    const { type } = pokemon;
+    expect(getByRole('button', { name: type })).toBeInTheDocument();
+    return true;
+  });
+  fireEvent.click(getByText('All'));
+//   // resultado da ação:
+//   expect (pokelista[1]).toBe(segundoPokemon);
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,117 @@
-test('', () => {});
+import React from 'react';
+
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+// import { fireEvent } from '@testing-library/react';
+// import { getByRole } from '@testing-library/dom';
+import renderWithRouter from '../components/renderWithRouter';
+import { Pokemon } from '../components';
+
+import pokemons from '../data';
+
+test('Teste 1: testa renderização das infos (nome, peso, tipo)', () => {
+  const pokefake = {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon.',
+  };
+  const isFavorite = true;
+
+  const { getByTestId, getByText } = renderWithRouter(
+    <Pokemon pokemon={ pokefake } isFavorite={ isFavorite } />,
+  );
+
+  const nome = getByText('Pikachu');
+  expect(nome).toBeInTheDocument();
+  const tipo = getByText('Electric');
+  expect(tipo).toBeInTheDocument();
+  const peso = getByTestId('pokemon-weight');
+  expect(peso).toHaveTextContent('Average weight: 6.0 kg');
+});
+
+test('Teste 2: testa renderização da imagem do pokémon', () => {
+  const pokefake = {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon.',
+  };
+  const isFavorite = true;
+
+  const { getByAltText } = renderWithRouter(
+    <Pokemon pokemon={ pokefake } isFavorite={ isFavorite } />,
+  );
+
+  const imageAdress = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+  const image = getByAltText('Pikachu sprite');
+
+  expect(image).toHaveAttribute('src', imageAdress);
+  expect(image).toBeInTheDocument();
+});
+
+it(' Teste 3: verifica link', () => {
+  const { getByRole } = renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite={ false }
+  />);
+  const details = getByRole('link', { name: 'More details' });
+  expect(details.href).toBe(`http://localhost/pokemons/${pokemons[0].id}`);
+});
+
+test('Teste 2: testa renderização da imagem do pokémon FAVORITO', () => {
+  const { getByAltText } = renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite
+  />);
+
+  const imageAdress = '/star-icon.svg';
+  const image = getByAltText(`${pokemons[0].name} is marked as favorite`);
+
+  expect(image).toHaveAttribute('src', imageAdress);
+  expect(image).toBeInTheDocument();
+});
+
+// test('favorite pokemon icon', () => {
+//   const { getAllByRole } = renderWithRouter(<Pokemon
+//     pokemon={ pokemons[0] }
+//     isFavorite
+//   />);
+//   const allIcons = getAllByRole('img');
+//   expect(allIcons[1].src).toBe('http://localhost/star-icon.svg');
+//   expect(allIcons[1].alt).toBe(`${pokemons[0].name} is marked as favorite`);
+// });
+
+// sintax => // getByRole('coisa da page (button, heading, etc...)', {name: 'oquetemnacoisadapage'}
+// npx stryker run ./stryker/Pokemon.conf.json
