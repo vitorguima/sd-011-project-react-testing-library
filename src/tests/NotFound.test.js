39d378f8c7673f536ddacd3aa79290621de94e91
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-01 22:20:20 -0300

    requisito 4 resolvido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+    const heading = getByRole('heading');
+    expect(heading.textContent).toBe('Page requested not found 😭');
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const { container } = render(<NotFound />);
+    const image = container.querySelector('.not-found-image');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
