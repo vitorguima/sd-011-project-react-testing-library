a6947f26eef84c9f60412820c2b85731ca79d6f5
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-07-01 23:13:38 -0300

    teste App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Testando componente App', () => {
+  it('Página principal é renderizada ao carregar a aplicação no caminho de URL /', () => {
+    const { history } = renderWithRouter(<App />);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    const pathHome = history.location.pathname;
+    expect(pathHome).toBe('/');
+  });
+  it('Topo da aplicação contém esses conjunto de link de navegação', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const linkHome = getByText(/home/i);
+    const linkAbout = getByText(/about/i);
+    const linkFavorite = getByText(/favorite pokémons/i);
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+  it('A aplicação é redirecionada para página inicial, na URL / ao clicar no link Home',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const linkHome = getByText(/home/i);
+      fireEvent.click(linkHome);
+
+      const pathHome = history.location.pathname;
+      expect(pathHome).toBe('/');
+    });
+  it('A aplicação é redirecionada para page About, na URL /about ao clicar no link About',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const linkAbout = getByText(/about/i);
+      fireEvent.click(linkAbout);
+
+      const pathAbout = history.location.pathname;
+      expect(pathAbout).toBe('/about');
+    });
+  it('App muda p/ pag Pokémons Favoritados, na URL /favorites ao clicar no link Favorite',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const linkFavorite = getByText(/favorite pokémons/i);
+      fireEvent.click(linkFavorite);
+
+      const pathFavorite = history.location.pathname;
+      expect(pathFavorite).toBe('/favorites');
+    });
+  it('App é redirecionada para a página Not Found ao entrar em uma URL desconhecida',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+
+      history.push('/vaiferrar/oteste');
+      const noMatch = getByText(/page requested not found/i);
+
+      expect(noMatch).toBeInTheDocument();
+    });
 });
