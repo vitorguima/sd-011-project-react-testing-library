d27df3a75ff1902af7ec99d54c2742fa3485afc1
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-07-01 21:13:52 -0300

    task 5 1.0

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { Pokedex } from '../components';
+import pokemons from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+const mockFavorite = {
+  4: false,
+  10: false,
+  23: false,
+  25: true,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+test('Teste o componente Pokedex.js', () => {
+  const { getByText } = renderWithRouter(
+  <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ mockFavorite } />,
+  );
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
