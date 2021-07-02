a0b31ae2862346ae4fe9c34032e191c71a154cae
nandovbr <nandovbr15@hotmail.com>
2021-07-01 22:51:47 -0300

    adiciona os testes do componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound.js', () => {
+  it('testa se página contém "Page requested not found 😭" em h2', () => {
+    const { getByRole } = render(<NotFound />);
+    const texth2 = getByRole('heading', { level: 2 });
+    const title = 'Page requested not found 😭';
+    expect(texth2).toBeInTheDocument();
+    expect(texth2).toHaveTextContent(title);
+  });
+
+  it('testa se página mostra a imagem', () => {
+    const { getAllByRole } = render(<NotFound />);
+    const img = getAllByRole('img');
+    expect(img[1].src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
