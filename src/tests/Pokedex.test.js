ab73e2bfa6643e79daedb18de41badc7e14bcc50
Michelle <mls.srbek@gmail.com>
2021-07-02 08:22:09 -0300

    req 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import App from '../App';
+import data from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+it('contains `Encountered pokémons`, `Próximo pokémon` button and each pokemon', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const next = getByText('Próximo pokémon');
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(next).toBeInTheDocument();
+  const pokemonsArr = data.map((pokemon) => pokemon.name);
+  pokemonsArr.forEach((pokemon) => {
+    expect(getByText(pokemon)).toBeInTheDocument();
+    fireEvent.click(next);
+  });
+});
+
+it('contains all pokemons type buttons', () => {
+  const { getAllByTestId } = renderWithRouter(<App />);
+  const buttonsArr = getAllByTestId('pokemon-type-button');
+  expect(buttonsArr[0]).toHaveTextContent('Electric');
+  expect(buttonsArr[1]).toHaveTextContent('Fire');
+  expect(buttonsArr[2]).toHaveTextContent('Bug');
+  expect(buttonsArr[3]).toHaveTextContent('Poison');
+  expect(buttonsArr[4]).toHaveTextContent('Psychic');
+  expect(buttonsArr[5]).toHaveTextContent('Normal');
+  expect(buttonsArr[6]).toHaveTextContent('Dragon');
+});
+
+it('render all pokemons when `All` clicked', () => {
+  const { getByText } = renderWithRouter(<App />);
+  fireEvent.click(getByText('All'));
+  const pokemonsArr = data.map((pokemon) => pokemon.name);
+  pokemonsArr.forEach((pokemon) => {
+    expect(getByText(pokemon)).toBeInTheDocument();
+    fireEvent.click(getByText('Próximo pokémon'));
+  });
+});
