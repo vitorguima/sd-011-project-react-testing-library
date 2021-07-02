a20adf1212caca270a52ef677f258df2f238b041
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-07-02 12:00:21 -0300

    requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando NotFound.js', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const { container } = render(<NotFound />);
+    const checkH2 = container.querySelector('h2');
+    expect(checkH2.textContent).toBe('Page requested not found 😭');
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getAllByRole } = render(<NotFound />);
+    const chechImg = getAllByRole('img');
+    expect(chechImg[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
