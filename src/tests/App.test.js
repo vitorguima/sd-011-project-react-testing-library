0e3aa11c1227cbe4249253a255333dfec0b4f356
AlexSmith7192 <alexsmith7192@gmail.com>
2021-07-02 19:34:28 -0300

    1 - Testa o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testes do componente App.js', () => {
+  it('Página principal é redenrizada na url / ', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const subtitle = getByText(/Encountered pokémons/);
+    expect(subtitle).toBeInTheDocument();
+  });
+
+  it('Topo da página contém 3 links fixos de navegação', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const links = getAllByRole('link');
+    expect(links[0]).toBeInTheDocument();
+    expect(links[1]).toBeInTheDocument();
+    expect(links[2]).toBeInTheDocument();
+  });
+
+  it('Redireciona para a URL / ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    userEvent.click(getByText(/Home/));
+    const url = history.location.pathname;
+    expect(url).toBe('/');
+  });
+
+  it('Redireciona para a URL /about ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    userEvent.click(getByText(/About/));
+    const url = history.location.pathname;
+    expect(url).toBe('/about');
+  });
+
+  it('Redireciona para a URL /favorites ', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    userEvent.click(getByText(/Favorite Pokémons/));
+    const url = history.location.pathname;
+    expect(url).toBe('/favorites');
+  });
+
+  it('Renderiza NotFound', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const erroPage = 'silas';
+    history.push(erroPage);
+    const notFound = getByText(/Page requested not found/);
+    expect(notFound).toBeInTheDocument();
+  });
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
+// Acessar os elementos da tela
+// Interagir com eles (se houver necessidade)
+// Fazer teste
