3bd7a94745fbc34eb00cd1c6cc9c09e13d0f8ab8
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-07-01 22:37:28 -0300

    Exercise 2 completed

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+test('tests information about "Pokédex"', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const about = getByText('About Pokédex');
+  expect(about).toBeInTheDocument();
+});
+test('tests two paragraphs', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const paragraphOne = getByText(/This application/i);
+  expect(paragraphOne).toBeInTheDocument();
+  const paragraphTwo = getByText(/One can filter/i);
+  expect(paragraphTwo).toBeInTheDocument();
+});
+
+test('tests have image', () => {
+  const { getByAltText } = renderWithRouter(<About />);
+  const image = getByAltText('Pokédex');
+  expect(image).toBeInTheDocument();
+  expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
