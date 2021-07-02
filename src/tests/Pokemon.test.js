0c2661327981b67c52ba6297e761d3a6ae433e70
Michelle <mls.srbek@gmail.com>
2021-07-02 08:40:51 -0300

    card with all pokemons informations

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import data from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+it('cotains card with all pokemons informations', () => {
+  const { getByTestId, getByText, getByAltText } = renderWithRouter(<App />);
+  data.forEach((pokemon) => {
+    expect(getByTestId('pokemon-name')).toHaveTextContent(pokemon.name);
+    expect(getByTestId('pokemon-type')).toHaveTextContent(pokemon.type);
+    expect(getByTestId('pokemon-weight')).toHaveTextContent(`Average weight: ${pokemon
+      .averageWeight.value} ${pokemon.averageWeight.measurementUnit}`);
+    expect(getByAltText(`${pokemon.name} sprite`)).toHaveAttribute('src', pokemon.image);
+    fireEvent.click(getByText('Próximo pokémon'));
+  });
+});
