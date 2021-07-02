35247e827fa87f7ba708e66404e33d9906f71c37
Priscila Scapin <prih.scapin@gmail.com>
2021-07-01 22:35:38 -0300

    Requisito 3/ menos 1 mutant \m/

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('Teste 1: testa renderização da mensagem', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const info = getByText('No favorite pokemon found');
+
+  expect(info).toBeInTheDocument();
+});
