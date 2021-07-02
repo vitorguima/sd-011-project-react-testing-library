fe3bcfda862792d1bca3694eee269ca2ab09a1ec
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-07-01 21:42:21 -0300

    task 5 1.1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Pokedex } from '../components';
+import { fireEvent } from '@testing-library/react';
 import pokemons from '../data';
 import renderWithRouter from '../renderWithRouter';
 
@@ -17,7 +18,26 @@ const mockFavorite = {
 
 test('Teste o componente Pokedex.js', () => {
   const { getByText } = renderWithRouter(
-  <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ mockFavorite } />,
+    <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ mockFavorite } />
   );
+
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Teste o componente Pokedex.js', () => {
+  const { getByText, getAllByTestId, getByRole } = renderWithRouter(
+    <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ mockFavorite } />
+  );
+
+  expect(getByText('All')).toBeInTheDocument();
+  expect(getByText('Próximo pokémon')).toBeInTheDocument();
+  expect(getAllByTestId('pokemon-type-button').length).toBe('7');
+
+  pokemons.map((pokemon) => {
+    const { type } = pokemon;
+    expect(getByRole('button', { name: type })).toBeInTheDocument();
+    return true;
+  });
+
+  fireEvent.click(getByText('All'));
 });
