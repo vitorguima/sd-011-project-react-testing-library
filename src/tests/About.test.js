acd21859bc88dea0e8c635adcb8490ee01a9ac89
FredericoCanoeira <fcanoeira@gmail.com>
2021-07-02 17:17:20 +0100

    refactoring II requirement 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { About } from '../components';
 import renderWithRouter from '../components/renderWithRouter';
 
-describe('Renders <About /> component with information about Pokédex', () => {
-  it('renders a header with the text `About Pokédex`', () => {
+describe('Renders About component with information about Pokédex', () => {
+  it('renders a header with the text About Pokédex', () => {
     const { getByRole } = renderWithRouter(<About />);
     const header = getByRole('heading', { level: 2 });
 
@@ -21,8 +21,8 @@ describe('Renders <About /> component with information about Pokédex', () => {
 
   it('Test the existence of a Pokédex image on the page', () => {
     const { getByRole } = renderWithRouter(<About />);
-    const ImgPokedex = getByRole('img');
+    const imgPokedex = getByRole('img');
 
-    expect(ImgPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(imgPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../components/renderWithRouter';
+import React from 'react';
+import App from '../App';
+import { FavoritePokemons } from '../components';
+
+
+describe('Renders favorites Pokémons', () => {
+  it('renders a text "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const message = getByText(/No favorite pokemon found/i);
+
+    expect(message).toBeInTheDocument();
+  });
+
+  it('renders favorited pokemon when requested"', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+
+    userEvent.click(getByText('Home'));
+    userEvent.click(getByText('More details'));
+    userEvent.click(getByText('Pokémon favoritado?'));
+    userEvent.click(getByText('Favorite Pokémons'));
+
+    const pokemon = getByTestId('pokemon-name');
+
+    expect(pokemon).toBeInTheDocument();
+  });
+
+  it('renders a message when no Pokémon is favorited', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
