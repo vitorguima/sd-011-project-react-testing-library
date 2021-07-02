b7d0e2fa6f20fee2839a9852890b3a5e9feb7604
Priscila Scapin <prih.scapin@gmail.com>
2021-07-01 22:30:04 -0300

    Resquisito 4 -*** revisar matchers

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -11,6 +11,8 @@ test('2/1: Identifica renderização das infos sobre a Pokédex.', () => {
   expect(info).toBeInTheDocument();
 });
 
+// Thanks Diegão *---*
+
 test('2/2: Identifica renderização da imagem.', () => {
   const imageAdress = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
   const { getByAltText } = renderWithRouter(<About />);
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import { NotFound } from '../components';
+
+// Articles: https://github.com/testing-library/jest-dom#tohavetextcontent
+// https://www.robinwieruch.de/react-testing-library
+// https://stackoverflow.com/questions/63838716/react-testing-library-getbyroleheading-how-to-get-node-with-a-specific-headi
+
+test('Teste 1: testa a rendereização do headling <h2>', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  // sintax => // getByRole('coisa da page (button, heading, etc...)', {name: 'oquetemnacoisadapage'}
+  const headling = getByRole('heading',
+    { name: /Page requested not found Crying emoji/i });
+
+  expect(headling).toBeInTheDocument();
+});
+
+test('Test 2: testa a rendereização da imagem no headling <h2>', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+
+  const imageAdress = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const image = getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(image).toHaveAttribute('src', imageAdress);
+  expect(image).toBeInTheDocument();
+});
