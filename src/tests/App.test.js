a7873d189688fc73d269f402d0c65bf48d657ff9
Anajulia Brito <juliehindius@gmail.com>
2021-07-02 09:43:37 -0300

    Finalizando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste dos componentes do <App.js/>', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se a página principal da Pokédex é renderizada no caminho de URL', () => {
+    const { history } = renderWithRouter(<App />);
+    const url = history.location.pathname;
+    expect(url).toBe('/');
+  });
+
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const link = getAllByRole('link');
+    expect(link[0].innerHTML).toBe('Home');
+    expect(link[1].innerHTML).toBe('About');
+    expect(link[2].innerHTML).toBe('Favorite Pokémons');
+  });
+
+  it('A aplicação redireciona para a página inicial, na URL /  ao clicar em Home', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const home = getByRole('link', { name: 'Home' });
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('A aplicação redireciona para a página About, na URL /  ao clicar em About', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const about = getByRole('link', { name: 'About' });
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Redireciona para Pokémons Favoritados, na URL /  ao clicar em favorites', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const favorites = getByRole('link', { name: 'Favorite Pokémons' });
+    fireEvent.click(favorites);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Redireciona para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    history.push('*');
+    const text = getByText(/not found/);
+    expect(text).toBeInTheDocument();
+  });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Favorite from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste dos componentes do <FavoritePokemons.js/>', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = render(<Favorite />);
+    const text = getByText(/No favorite pokemon found/);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history, getAllByText, getByRole } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favoritado = getByRole('checkbox');
+    fireEvent.click(favoritado);
+    history.push('/pokemons/4');
+    fireEvent.click(favoritado);
+    history.push('/favorites');
+    const pokemon = getAllByText(/kg/i);
+    expect(pokemon.length).toEqual(2);
+  });
+});
