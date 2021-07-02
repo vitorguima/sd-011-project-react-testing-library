dbd1c14143cf3f929b3ac4b5e802ffaf81644a17
nandovbr <nandovbr15@hotmail.com>
2021-07-01 22:08:33 -0300

    iniciando testes do componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons.js', () => {
+  it('testa se "No favorite pokemon found" aparece se não houver favoritos', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const textNoFavorite = getByText(/No favorite pokemon found/i);
+    expect(textNoFavorite).toBeInTheDocument();
+  });
+});
