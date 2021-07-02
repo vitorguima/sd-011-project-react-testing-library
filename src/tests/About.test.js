29857f842ef552a3ba0aa771d9a32f862d1fdb64
Diego Figueirêdo <8kball@gmail.com>
2021-07-02 12:08:57 -0300

    Adiciona renderWithRouter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+it('Verifica se a página contém um h2 com o texto "Page requested not found 😭"', () => {
+  return 1;
+});
+
+it('Verifica se a página contém uma imagem específica', () => {
+  return 1;
+});
