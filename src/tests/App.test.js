08bf21808766e6f3fbea81bc65c7db57bd281222
Nick-713 <pereiranicolas713@gmail.com>
2021-07-02 03:19:01 -0300

    finalizando

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,23 +3,25 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Test App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-test('Pokédex is rendered when loading application in URL path /', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  expect(getByText('Home')).toBeInTheDocument();
-  expect(getByText('About')).toBeInTheDocument();
-  expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  test('Pokédex is rendered when loading application in URL path /', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import NotFound from '../components/NotFound';
 
-test('page contains a heading h2 with the text Page requested not found', () => {
-  const { getByRole } = render(<NotFound />);
-  const heading = getByRole('heading', {
-    level: 2,
-    name: 'Page requested not found Crying emoji',
+describe('Test NotFound', () => {
+  test('page contains a heading h2 with the text Page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(heading).toBeInTheDocument();
   });
-  expect(heading).toBeInTheDocument();
-});
 
-test('page shows the image', () => {
-  render(<NotFound />);
-  expect(screen.getByAltText('Pikachu crying because the page requested was not found'))
-    .toHaveAttribute('src', expect.stringContaining('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'));
+  test('page shows the image', () => {
+    render(<NotFound />);
+    expect(screen.getByAltText('Pikachu crying because the page requested was not found'))
+      .toHaveAttribute('src', expect.stringContaining('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'));
+  });
 });
