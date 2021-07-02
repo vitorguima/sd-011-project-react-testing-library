026cc375c0aa1fc8304222bbeed1ad34e6a85cb2
Pedro Castro <pa.coelho.castro@gmail.com>
2021-07-02 09:34:46 -0300

    Req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('if there is a message for empty favorite pokemons', () => {
+  const { getByText } = render(<FavoritePokemons />);
+
+  const pokemonNotFoundMessage = getByText('No favorite pokemon found');
+
+  expect(pokemonNotFoundMessage).toBeInTheDocument();
+});
