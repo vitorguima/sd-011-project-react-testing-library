c8944853daf0dc393d8a4091b03bd2b34a03000a
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-07-02 00:02:07 -0300

    Exercise 4 completed

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+test('Test render "Page request not found"', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const checkHeader = getByText('Page requested not found');
+  expect(checkHeader).toBeInTheDocument();
+});
+
+test('', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const notShow = getByAltText('/pikachu crying/i');
+  expect(notShow).toBeInTheDocument();
+  expect(notShow.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
