ba70dddc9fad8ae708217553290ff35c5abf84c4
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-07-02 17:13:28 -0300

    req 4:NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      render(<NotFound />);
+
+      const heading = screen.getByRole('heading', {
+        name: /page requested not found/i,
+        level: 2,
+      });
+      expect(heading).toBeInTheDocument();
+    });
+  it('Teste se a página tem essa imagem: ', () => {
+    render(<NotFound />);
+
+    const img = screen.queryByRole('img', {
+      name: /pikachu crying /i,
+    });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
