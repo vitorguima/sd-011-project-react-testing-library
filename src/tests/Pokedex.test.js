8baf7f44ec6516c6e944e38a41eb95707ede3c5f
nandovbr <nandovbr15@hotmail.com>
2021-07-02 11:13:30 -0300

    adiciona os testes do componente Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -31,4 +31,39 @@ describe('Testa o componente Pokedex.js', () => {
     const pokemons = getAllByText(/Average weight/i);
     expect(pokemons.length).toEqual(1);
   });
+
+  it('testa se a Pokédex tem os botões de filtro', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const all = getByRole('button', { name: /All/i });
+    const fire = getByRole('button', { name: /Fire/i });
+    const psychic = getByRole('button', { name: /Psychic/i });
+    const electric = getByRole('button', { name: /Electric/i });
+    const bug = getByRole('button', { name: /Bug/i });
+    const poison = getByRole('button', { name: /Poison/i });
+    const dragon = getByRole('button', { name: /Dragon/i });
+    const normal = getByRole('button', { name: /Normal/i });
+    expect(all).toBeInTheDocument();
+    expect(fire).toBeInTheDocument();
+    expect(psychic).toBeInTheDocument();
+    expect(electric).toBeInTheDocument();
+    expect(bug).toBeInTheDocument();
+    expect(poison).toBeInTheDocument();
+    expect(dragon).toBeInTheDocument();
+    expect(normal).toBeInTheDocument();
+  });
+
+  it('testa se a Pokédex contém um botão para resetar o filtro', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const button = getByRole('button', { name: 'All' });
+    const unfiltered = getByText('Pikachu');
+    fireEvent.click(button);
+    expect(unfiltered).toBeInTheDocument();
+  });
+
+  it('testa se é criado, dinamicamente, botão filtro para cada tipo de Pokémon', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const buttons = getAllByTestId('pokemon-type-button');
+    const allButtons = 7;
+    expect(buttons.length).toBe(allButtons);
+  });
 });
