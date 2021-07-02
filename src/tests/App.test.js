24aa41be6c7a54a0c4684cc64671ee8bbf75a710
Alan Gonçalves <vekio.soft@gmail.com>
2021-07-02 11:00:03 -0300

    conclusão requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,67 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderRouter from '../renderRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste do componente App.js', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    // Acessar os elementos da tela
+    const { getByText } = renderRouter(<App />);
+    const headingText = getByText(/Pokédex/i);
+    // Fazer o teste
+    expect(headingText).toBeInTheDocument();
+  });
+
+  it('Teste se a página principal da Pokédex é renderizada ao carregar a app', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderRouter(<App />);
+    const urlPath = history.location.pathname;
+    const foundPokemons = getByText('Encountered pokémons');
+    // Fazer os testes
+    expect(urlPath).toBe('/');
+    expect(foundPokemons).toBeInTheDocument();
+  });
+
+  it('Teste se a app é redirec. p/ a page inicial na URL / ao clicar em Home', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderRouter(<App />);
+    const home = getByText(/home/i);
+    // Interagir com a aplicação
+    fireEvent.click(home);
+    const homeURL = history.location.pathname;
+    // Fazer os testes
+    expect(homeURL).toBe('/');
+  });
+
+  it('Teste se a app é redirec. p/ a page About na URL /about ao clicar em About', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderRouter(<App />);
+    const about = getByText(/About/i);
+    // Interagir com a aplicação
+    fireEvent.click(about);
+    const aboutURL = history.location.pathname;
+    // Fazer os testes
+    expect(aboutURL).toBe('/about');
+  });
+
+  it('Teste se a app é redirec. p/ a page Pokémons Favoritados na URL /favorites', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderRouter(<App />);
+    const favorites = getByText(/favorite pokémons/i);
+    // Interagir com a aplicação
+    fireEvent.click(favorites);
+    const pathName = history.location.pathname;
+    // Fazer os testes
+    expect(pathName).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página "Not Found"', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderRouter(<App />);
+    // Interagir com a aplicação
+    history.push('/pagina/que-nao-existe/');
+    const notFoundMsg = getByText(/Page requested not found/i);
+    // Fazer os testes
+    expect(notFoundMsg).toBeInTheDocument();
+  });
 });
