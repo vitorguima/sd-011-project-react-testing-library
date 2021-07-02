26b750295acd50e7ee1ff107d4954ee0b47a9c45
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-07-01 23:08:37 -0300

    Exercise 3 completed

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Test screen favorite message pokemon found', () => {
+  const { queryByText, getByText } = renderWithRouter(<FavoritePokemons />);
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  expect(queryByText('Pikachu')).not.toBeInTheDocument();
+  expect(queryByText('Charmander')).not.toBeInTheDocument();
+  expect(queryByText('Caterpie')).not.toBeInTheDocument();
+});
+
+test('Test show cards pokemons favorite', () => {
+  const { history, getAllByText, getByRole } = renderWithRouter(<App />);
+  history.push('/pokemons/25');
+  const favoritado = getByRole('checkbox');
+  fireEvent.click(favoritado);
+  history.push('/pokemons/4');
+  fireEvent.click(favoritado);
+  history.push('/favorites');
+  const pokemon = getAllByText(/kg/i);
+  expect(pokemon.length).toEqual(2);
+});
