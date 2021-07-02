c6f4a896c0a628cfc1d867f46f29490debddb28e
Cris Lustosa <cmlustosa@gmail.com>
2021-07-02 08:36:01 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Test componnent Not Found', () => {
+  it('Test if the page contains a heading (h2) with the text Page requested not found',
+    () => {
+      const { getByText } = render(<NotFound />);
+      const notFound = getByText('Page requested not found');
+
+      expect(notFound).toBeInTheDocument();
+    });
+
+  it('', () => {
+    const { getByAltText } = render(<NotFound />);
+    const imgNotFound = getByAltText(/Pikachu crying because/i);
+
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,83 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+test('A página deve exibir o próximo pokémon da lista', () => {
+  const { getBytestId, getByText, getAllByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  // botao
+  const btnNext = getBytestId('next-pokemon');
+  expect(btnNext).toBeInTheDocument();
+  const pokemonsNames = [
+    'Charmander',
+    'Caterpie',
+    'Ekans',
+    'Alakazam',
+    'Mew',
+    'Rapidash',
+    'Snorlax',
+    'Dragonair',
+    'Pikachu',
+  ];
+  for (let i = 0; i < pokemonsNames.length; i += 1) {
+    fireEvent.click(getByText(/próximo pokémon/i));
+    expect(getByText(pokemonsNames[i])).toBeInTheDocument();
+    expect(getAllByText(/Average weight/i)).toHaveLength(1);
+  }
+});
+
+test('A Pokedex deve conter botões de filtro', () => {
+  const { getByText, getAllByTextId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const btnFilter = getAllByTextId('pokemon-type-button');
+  const number = 7;
+  expect(btnFilter.length).toBe(number);
+  fireEvent.click(getByText(/Psychic/i));
+  expect(getByText(/Alakazam/i)).toBeInTheDocument();
+  fireEvent.click(getByText(/próximo pokémon/i));
+  fireEvent.click(getByText(/próximo pokémon/i));
+  expect(getByText(/Alakazam/i)).toBeInTheDocument();
+});
+
+test('A Pokédex deve conter um botão para resetar o filtro', () => {
+  const { getByText, getAllByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const btnAll = getByText('All');
+  fireEvent.click(getByText('Fire'));
+  fireEvent.click(btnAll);
+  const allPokeNames = [
+    'Charmander',
+    'Caterpie',
+    'Ekans',
+    'Alakazam',
+    'Mew',
+    'Rapidash',
+    'Snorlax',
+    'Dragonair',
+    'Pikachu',
+  ];
+  for (let i = 0; i < allPokeNames.length; i += 1) {
+    fireEvent.click(getByText(/próximo pokémon/i));
+    expect(getByText(allPokeNames[i])).toBeInTheDocument();
+    expect(getAllByText(/More details/i).length).toBe(1);
+  }
+});
+
+test('Killing the h2', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText(/Encounterd pokémons/i)).toBeInTheDocument();
+});
