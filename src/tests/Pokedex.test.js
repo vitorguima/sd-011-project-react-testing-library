e2b4f76bcc0744fc5ce37d60b9b543d949aeac54
gabrielfndz <gabriel.omercador@hotmail.com>
2021-07-03 21:42:13 -0300

    Requisito 5 e parte do requisito 7

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,56 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Testa se contém um heading com Encountered Pokémons', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const pokeHeading = getByText(/Encountered pokémons/);
+  expect(pokeHeading).toBeInTheDocument();
+});
+
+test('Se é exibido o próximo pokémon quando clica', () => {
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+  const firstPoke = getByText(/Pikachu/);
+  expect(firstPoke).toBeInTheDocument();
+  const nextButton = getByTestId(/next-pokemon/);
+  fireEvent.click(nextButton);
+  const nextPoke = getByText(/Charmander/);
+  expect(nextPoke).toBeInTheDocument();
+});
+
+test('Se é exibido apenas um pokemon por vez', () => {
+  const { container, getByText } = renderWithRouter(<App />);
+  const pokeLen = container.querySelectorAll('.pokedex');
+  expect(pokeLen.length).toBe(1);
+  const nextButton = getByText('Próximo pokémon');
+  const type = container.querySelectorAll('.pokemon-type');
+
+  if (type.length === 1) {
+    expect(nextButton).toBeDisabled();
+  }
+});
+
+test('Se existem os botões de filtro', () => {
+  const { getAllByTestId, getByText } = renderWithRouter(<App />);
+  const buttonLen = getAllByTestId(/pokemon-type-button/);
+  expect(buttonLen[0].innerHTML).toBe('Electric');
+  expect(buttonLen[1].innerHTML).toBe('Fire');
+  expect(buttonLen[2].innerHTML).toBe('Bug');
+  expect(buttonLen[3].innerHTML).toBe('Poison');
+  expect(buttonLen[4].innerHTML).toBe('Psychic');
+  expect(buttonLen[5].innerHTML).toBe('Normal');
+  expect(buttonLen[6].innerHTML).toBe('Dragon');
+
+  const allButton = getByText('All');
+  expect(allButton).toBeInTheDocument();
+});
+
+test('Se contem um botao para resetar filtro', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const allButton = getByText('All');
+  expect(allButton).toBeInTheDocument();
+  fireEvent.click(allButton);
+  const initialPokemon = getByText(/Pikachu/);
+  expect(initialPokemon).toBeInTheDocument();
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Teste se as infos detalhadas do Pokémon selecionado são mostradas', () => {
+  const { container, getByText } = renderWithRouter(<App />);
+  const pokeDetails = getByText(/More details/);
+  fireEvent.click(pokeDetails);
+  const pokeName = getByText(/Pikachu Details/);
+  expect(pokeName).toBeInTheDocument();
+  const pokeSummary = getByText(/Summary/);
+  expect(pokeSummary).toBeInTheDocument();
+  const pokeHeading = container.querySelector('p');
+  expect(pokeHeading).toBeInTheDocument();
+});
+
+test('Se existe uma seção com o mapa', () => {
+  const { container, getByText } = renderWithRouter(<App />);
+  const pokeDetails = getByText(/More details/);
+  fireEvent.click(pokeDetails);
+  const locHeading = getByText(/Game Locations of Pikachu/);
+  expect(locHeading).toBeInTheDocument();
+  const mapLocation = container.querySelectorAll('.pokemon-habitat img');
+  console.log(mapLocation);
+  expect(mapLocation.length).toBe(2);
+  const mapDiv = container.querySelectorAll('em');
+  expect(mapDiv.length).toEqual(mapLocation.length);
+});
