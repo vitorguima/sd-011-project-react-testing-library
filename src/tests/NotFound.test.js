e18eca26921feb60ab8186b86c56291a94740613
camiloLelis <lafirmafuerte@gmail.com>
2021-07-05 00:07:09 -0300

    requisito-4-tentat-2

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,7 +4,7 @@ import NotFound from '../components/NotFound';
 
 test('testa se contem h2 com texto', () => {
   const { getByRole } = render(<NotFound />);
-  const h2 =  getByRole('heading', {level: 2});
+  const h2 = getByRole('heading', { level: 2 });
   // console.log(h2.innerHTML);
   expect(h2.innerHTML).toMatch(/Page requested not found/i);
 });
@@ -13,6 +13,6 @@ test('testa se aparece a imagem', () => {
   const { getAllByRole } = render(<NotFound />);
   const img = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
   const imagem = getAllByRole('img');
-  //console.log(imagem);
+  // console.log(imagem);
   expect(imagem[1].src).toContain(img);
-}); 
+});
