0df91966f6ba64a7773f50a5fc45f5a90ec29785
Matheus Costa <mathws.costag@gmail.com>
2021-07-05 23:59:19 -0300

    Cria os testes referentes ao requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('Test the <NotFound.js /> component', () => {
+  const { getByAltText, getByText } = renderWithRouter(<NotFound />);
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+  expect(getByAltText('Pikachu crying because the page requested was not found')).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
