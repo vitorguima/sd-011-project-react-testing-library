85622ea2aa6ab422d3ac6ba62a8c7540cea9ec9c
Matheus Costa <mathws.costag@gmail.com>
2021-07-06 10:51:52 -0300

    Cria os testes referentes ao requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Test if the page contains a heading', () => {
+  const { getByText } = renderWithRouter(<App />);
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Test whether the next Pokémon in the list is displayed', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const btn = getByText('Próximo pokémon');
+  fireEvent.click(btn);
+  expect(getByText('Charmander')).toBeInTheDocument();
+});
+
+test('Test if Pokédex has filter buttons', () => {
+  const { getByText, getAllByTestId } = renderWithRouter(<App />);
+  getAllByTestId('pokemon-type-button');
+  const btnPsychic = getByText('Psychic');
+  const btnAll = getByText('All');
+  fireEvent.click(btnPsychic);
+  expect(getByText('Alakazam')).toBeInTheDocument();
+  fireEvent.click(btnAll);
+  const btn = getByText('Próximo pokémon');
+  fireEvent.click(btn);
+  expect(getByText('Charmander')).toBeInTheDocument();
+});
