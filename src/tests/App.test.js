94a5066f6faba839809052ebcdac35ae01ec817c
IgorMendez <igor.emp@hotmail.com>
2021-07-06 00:54:21 -0300

    Exercício 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,13 +5,14 @@ import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('App tests', () => {
-  it('Home must render when click on /', () => {
+  test('Testa se a página renderiza no "/"', () => {
     const { history } = renderWithRouter(<App />);
     fireEvent.click(screen.getByRole('link', { name: 'Home' }));
     const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
-  it('should there link home, about and Favorite Pokémons on the top', () => {
+
+  test('Deve haver três links, Home, About e Favorite Pokémons', () => {
     render(
       <MemoryRouter>
         <App />
@@ -22,8 +23,37 @@ describe('App tests', () => {
     expect(links[0].innerHTML).toBe('Home');
     expect(links[1].innerHTML).toBe('About');
     expect(links[2].innerHTML).toBe('Favorite Pokémons');
-  }
-  )
-}
-)
+  });
 
+  test('Home muda para "/" ao ser clicado', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText('Home');
+
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('About muda para "/about" ao ser clicado', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText('About');
+
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Favorite Pokémons muda para "/favorites" ao ser clicado', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favorites = getByText('Favorite Pokémons');
+
+    fireEvent.click(favorites);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  test('Testa se a redireciona para "Not Found"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('not-found');
+
+    const notFound = getByText(/Page requested not found/);
+    expect(notFound).toBeInTheDocument();
+  });
+});
