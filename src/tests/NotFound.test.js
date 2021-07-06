1f2d63ea25347f13a5582eb9f0babe0b2a74529a
IgorMendez <igor.emp@hotmail.com>
2021-07-06 01:34:02 -0300

    Exercício 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Verifica página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+    const headingH2 = getByRole('heading');
+
+    expect(headingH2.textContent).toBe('Page requested not found 😭');
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const { container } = render(<NotFound />);
+    const image = container.querySelector('.not-found-image');
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
