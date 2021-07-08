040951b13d6348f923658a1e0aa97f0d456618bd
Wendell Costa <wendellcosta9141@gmail.com>
2021-07-08 18:11:51 -0300

    [Requisito 3 - Concluido]

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('FavoritePokemons tests', () => {
+  test('Tests if "No favorite pokemon found" in case the user have no favorite pokemons.',
+    () => {
+      const { getByText } = render(<FavoritePokemons />);
+      const noFavoritePokemonMessage = getByText('No favorite pokemon found');
+
+      expect(noFavoritePokemonMessage).toBeInTheDocument();
+    });
+
+  test('Tests if all favorite Pokemon cards are displayed.',
+    () => {
+      const { getByText, container } = renderWithRouter(<App />);
+      const MoreDetails = getByText('More details');
+      fireEvent.click(MoreDetails);
+
+      const favoritePokemon = getByText('Pokémon favoritado?');
+      fireEvent.click(favoritePokemon);
+
+      const favoritePokemonsB = getByText('Favorite Pokémons');
+      fireEvent.click(favoritePokemonsB);
+
+      const pokemonOverview = container.querySelector('.pokemon-overview');
+      expect(pokemonOverview).toBeInTheDocument();
+    });
+});
