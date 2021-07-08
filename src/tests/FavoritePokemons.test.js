980750a8f8f6d06d7e65c539b22ba644694b56c1
Taís Carvalho <taispcarvalho@gmail.com>
2021-07-08 10:37:08 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('testing Favorite Pokemons Page', () => {
+  it('testing if "No favorite pokemon found" appears if theres no favorite cards', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const pageInfo = getByText(/No favorite pokemon found/i);
+    expect(pageInfo).toBeInTheDocument();
+  });
+
+  it('testing if favorite cards are appearing in the Page', () => {
+    const { history, getAllByText, getByRole } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favorite = getByRole('checkbox');
+    fireEvent.click(favorite);
+    history.push('/pokemons/4');
+    fireEvent.click(favorite);
+    history.push('/favorites');
+    const pokemonCards = getAllByText(/details/i);
+    expect(pokemonCards.length).toEqual(2);
+  });
+});
