22d72437330f4f78344843df3803db74c7bebfef
Wendell Costa <wendellcosta9141@gmail.com>
2021-07-08 16:59:05 -0300

    [Requisito 1 - Concluido]

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+describe('App tests', () => {
+  test('Tests if pokedex homepage renders in "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByRole('link', { name: 'Home' }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  test('Tests if the top of the application contains a fixed set of navigation links',
+    () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const innerText = screen.getAllByRole('link');
+      expect(innerText[0].innerHTML).toBe('Home');
+      expect(innerText[1].innerHTML).toBe('About');
+      expect(innerText[2].innerHTML).toBe('Favorite Pokémons');
+    });
+
+  test('Tests if clicking the Home Link redirects to the home page at "/"',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const home = getByText('Home');
+      fireEvent.click(home);
+      expect(history.location.pathname).toBe('/');
+    });
+
+  test('Tests if the application changes to "/about" when clicking the link',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const about = getByText('About');
+      fireEvent.click(about);
+      expect(history.location.pathname).toBe('/about');
+    });
+
+  test('Tests if the application changes to "/favorites" when clicking the link',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const favorites = getByText('Favorite Pokémons');
+      fireEvent.click(favorites);
+      expect(history.location.pathname).toBe('/favorites');
+    });
+
+  test('Tests if the application changes to "Not found" when clicking an unknown link',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      history.push('not-found');
+      const notFound = getByText(/Page requested not found/);
+      expect(notFound).toBeInTheDocument();
+    });
 });
