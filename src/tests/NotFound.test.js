8948a4241112ff4c1b079dd0859807ce31b49d39
Taís Carvalho <taispcarvalho@gmail.com>
2021-07-08 10:49:29 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testing Not Found Page', () => {
+  it('testing if the heading is h2', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const checkText = getByRole('heading', { level: 2 });
+    const text = 'Page requested not found 😭';
+    expect(checkText).toBeInTheDocument();
+    expect(checkText).toHaveTextContent(text);
+  });
+
+  it('testing if there is a specific image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText(/not found/i);
+    expect(image.src).toMatch('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
