528bc73dd888126e75d8c11cd834580da70bed0b
igordmf <igordanielmf@gmail.com>
2021-07-08 22:14:25 -0300

    Requisito 3 parte 1

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
+import App from '../App';
+
+describe('FavoritePokemon tests', () => {
+  it('renders message "No favorite pokemon found"', () => {
+    const { getByRole, getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    fireEvent.click(getByRole('link', { name: 'Favorite Pokémons' }));
+    const text = getByText('No favorite pokemon found');
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('', () => {
+    const { getByRole, getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+  });
+});
