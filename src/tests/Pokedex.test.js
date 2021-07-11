7fc8fe7712bd7c8ccde6794ce9768fb8cc7170c6
camiloLelis <lafirmafuerte@gmail.com>
2021-07-11 13:03:02 -0300

    requisito-5-tenta-1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,109 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('testa h2 e texto', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const h2 = getByRole('heading', { level: 2 });
+  expect(h2.innerHTML).toContain('Encountered pokémons');
+  expect(h2).toBeInTheDocument();
+});
+
+test('testa botão se tem o texto', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const btn = getByText(/Próximo pokémon/i);
+  expect(btn).toBeInTheDocument();
+});
+
+test('test click ', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const evento = fireEvent.click(getByText(/All/i));
+  console.log( evento );
+  expect(evento).toBe(true);
+})
+
+
+test('testa botão all', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const btn = getByRole( 'button', { name : /All/i });
+    // expect(btn).not.toBeInTheDocument();
+  expect(btn).toBeInTheDocument();
+});
+
+test ('testa se tem apenas um por vez', () => {
+  const { container } = renderWithRouter(<App />);
+  const pokemonLength = container.querySelectorAll('.pokemon');
+  // console.log( ...pokemonLength );
+  // console.log( pokemonLength[0].innerHTML );
+  expect(pokemonLength.length).toBe(1);
+});
+/* test ('testa se tem 7 botoes', () => {
+  const { getAllByTestId } = renderWithRouter(<App />);
+  const pokemonLength = getAllByTestId('pokemon-name');
+  console.log(pokemonLength.length);
+}); */
+
+test ('testa se tem 7 botoes para filtro', () => {
+  const { getAllByTestId } = renderWithRouter(<App />);
+  const pokemonLength = getAllByTestId('pokemon-type-button');
+  // console.log( pokemonLength.length );
+  expect(pokemonLength.length).toBe(7);
+});
+/////////////////////////////////////////////////////////////////////////////////
+
+test ('testa botões electric' , () => {
+  const { getAllByRole } = renderWithRouter(<App />);
+  const btn = getAllByRole('button', {
+    name: /electric/i,
+  });
+  expect(btn.length).toBe(1);
+});
+
+test ('testa botões fire' , () => {
+  const { getAllByRole } = renderWithRouter(<App />);
+  const btn = getAllByRole('button', {
+    name: /fire/i,
+  });
+  expect(btn.length).toBe(1);
+});
+
+test ('testa botões bug' , () => {
+  const { getAllByRole } = renderWithRouter(<App />);
+  const btn = getAllByRole('button', {
+    name: /bug/i,
+  });
+  expect(btn.length).toBe(1);
+});
+
+test ('testa botões poison' , () => {
+  const { getAllByRole } = renderWithRouter(<App />);
+  const btn = getAllByRole('button', {
+    name: /poison/i,
+  });
+  expect(btn.length).toBe(1);
+});
+
+test ('testa botões psychic ' , () => {
+  const { getAllByRole } = renderWithRouter(<App />);
+  const btn = getAllByRole('button', {
+    name: /psychic/i,
+  });
+  expect(btn.length).toBe(1);
+});
+
+test ('testa botões normal' , () => {
+  const { getAllByRole } = renderWithRouter(<App />);
+  const btn = getAllByRole('button', {
+    name: /normal/i,
+  });
+  expect(btn.length).toBe(1);
+});
+
+test ('testa botões dragon ' , () => {
+  const { getAllByRole } = renderWithRouter(<App />);
+  const btn = getAllByRole('button', {
+    name: /dragon/i,
+  });
+  expect(btn.length).toBe(1);
+});
