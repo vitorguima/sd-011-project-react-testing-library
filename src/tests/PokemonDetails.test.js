cb1e10f17380e37af7ef5624e3ef1964b2338079
camiloLelis <lafirmafuerte@gmail.com>
2021-07-12 00:46:23 -0300

    requisito-7-tenta-1

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+test('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {
+
+});
+test('Teste se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
+
+});
+test('Teste se o usuário pode favoritar um pokémon através da página de detalhes.', () => {
+    
+});
