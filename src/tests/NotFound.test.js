7b10d2840f61757d4940623e4ad1b081b43fc9ec
pmaiamateus <pmaiamateus@gmail.com>
2021-07-12 01:12:04 -0300

    adiciona passo 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Teste o componente `<NotFound.js />`', () => {
+  it('Teste se página contém um heading `h2` com o texto'
+  + '`Page requested not found 😭`', () => {
+    render(<NotFound />);
+    expect(screen.getByText('Page requested not found')).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem do Pikachu', () => {
+    render(<NotFound />);
+    const pikachuImage = screen.getByAltText('Pikachu crying because '
+    + 'the page requested was not found');
+    expect(pikachuImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
