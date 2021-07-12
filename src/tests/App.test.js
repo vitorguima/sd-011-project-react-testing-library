d56daf952d3d30805356a3578cfad7ebf3e27cec
pmaiamateus <pmaiamateus@gmail.com>
2021-07-11 23:15:40 -0300

    adiciona passo 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+describe('testa o componente <App.js />', () => {
+  it('Verifica se redireciona para URL / ao clicar no link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkToHome = getByText(/Home/i);
+    fireEvent.click(linkToHome);
+    const home = history.location.pathname;
+    expect(home).toBe('/');
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('Verifica se redireciona para a URL /about ao clicar no link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkToAbout = getByText(/About/i);
+    fireEvent.click(linkToAbout);
+    const about = history.location.pathname;
+    expect(about).toBe('/about');
+  });
+
+  it('Verifica se redireciona para a URL /favorites ao clicar no link Favorite Pokémons',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const linkToFavoritePokemons = getByText(/Favorite Pokémons/i);
+      fireEvent.click(linkToFavoritePokemons);
+      const favoritePokemons = history.location.pathname;
+      expect(favoritePokemons).toBe('/favorites');
+    });
 });
