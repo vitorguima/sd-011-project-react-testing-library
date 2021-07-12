9cd2c75e3c063577b00f952d348f24b559fee51d
pmaiamateus <pmaiamateus@gmail.com>
2021-07-12 00:39:41 -0300

    adiciona passo 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,74 @@
-test('', () => {});
+import { getAllByText } from '@testing-library/dom';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+const favoritePokemons = [
+  {
+    id: 65,
+    name: 'Alakazam',
+    type: 'Psychic',
+    averageWeight: {
+      value: '48.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Unova Accumula Town',
+        map: 'https://cdn2.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png',
+      },
+    ],
+    summary: 'Closing both its eyes heightens all its other senses.'
+    + 'This enables it to use its abilities to their extremes.',
+  },
+  {
+    id: 148,
+    name: 'Dragonair',
+    type: 'Dragon',
+    averageWeight: {
+      value: '16.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Johto Route 45',
+        map: 'https://cdn2.bulbagarden.net/upload/2/21/Johto_Route_45_Map.png',
+      },
+      {
+        location: 'Johto Dragon\'s Den',
+        map: 'https://cdn2.bulbagarden.net/upload/1/1e/Johto_Dragons_Den_Map.png',
+      },
+    ],
+    summary: 'They say that if it emits an aura from its whole body,'
+    + 'the weather will begin to change instantly.',
+  },
+];
+const noFavorites = [];
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found,'
+    + 'se a pessoa não tiver pokémons favoritos.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ favoritePokemons } />
+    );
+    const favoritePokemonsList = getAllByTestId('pokemon-name');
+    expect(favoritePokemonsList.length).toBe(2);
+  });
+
+  it('Testa se **nenhum** card de pokémon é exibido,'
+    + ' se ele não estiver favoritado', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ noFavorites } />);
+    const noCard = getByText('No favorite pokemon found');
+    expect(noCard).toBeInTheDocument();
+  });
+});
