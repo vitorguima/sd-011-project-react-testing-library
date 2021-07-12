14d8861de1b0b0dff5ca8d397399682cec7fcdd5
Raphael <raphaellfpg@gmail.com>
2021-07-12 01:52:34 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('a', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import About from '../components/About';
+
+it('Verifica se contem uma tag h2 com o texto About Pokedex', () => {
+  const { getByText } = renderWithRouter(<About />);
+
+  const h2 = getByText(/About Pokédex/);
+  expect(h2).toBeInTheDocument();
+});
+
+it('Verifica se contem 2 paragrafos na pagina', () => {
+  const { container } = renderWithRouter(<About />);
+  const paragraph = container.querySelectorAll('p');
+
+  expect(paragraph.length).toBe(2);
+});
+
+it('Verifica se a imagem correta está no arquivo', () => {
+  const { getByRole } = renderWithRouter(<About />);
+
+  const img = getByRole('img');
+  expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
