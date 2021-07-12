7c4c0ea14f2a878f6d14d4bd2c3d3ba98db88da4
igordmf <igordanielmf@gmail.com>
2021-07-11 21:34:21 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, cleanup } from '@testing-library/react';
 import App from '../App';
+
+afterEach(cleanup);
+
+const favorite = 'Favorite Pokémons';
+const details = 'More details';
 
 describe('FavoritePokemon tests', () => {
   it('renders message "No favorite pokemon found"', () => {
@@ -11,17 +16,62 @@ describe('FavoritePokemon tests', () => {
       </MemoryRouter>,
     );
 
-    fireEvent.click(getByRole('link', { name: 'Favorite Pokémons' }));
+    fireEvent.click(getByRole('link', { name: favorite }));
     const text = getByText('No favorite pokemon found');
 
     expect(text).toBeInTheDocument();
   });
 
-  // it('', () => {
-  //   const { getByRole, getByText } = render(
-  //     <MemoryRouter>
-  //       <App />
-  //     </MemoryRouter>,
-  //   );
-  // });
+  it('renders all favorites pokemons cards', () => {
+    const { getByRole, getByText, getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    fireEvent.click(getByRole('link', { name: details }));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByRole('link', { name: favorite }));
+
+    expect(getByText('Pikachu')).toBeInTheDocument();
+
+    fireEvent.click(getByRole('link', { name: 'Home' }));
+    fireEvent.click(getByRole('button', { name: 'Normal' }));
+    fireEvent.click(getByRole('link', { name: details }));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByRole('link', { name: favorite }));
+
+    expect(getByText('Snorlax')).toBeInTheDocument();
+
+    fireEvent.click(getByRole('link', { name: 'Home' }));
+    fireEvent.click(getByRole('button', { name: 'Dragon' }));
+    fireEvent.click(getByRole('link', { name: details }));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByRole('link', { name: favorite }));
+
+    expect(getByText('Dragonair')).toBeInTheDocument();
+
+    let favoriteHeading = getByRole('heading', { level: 2 });
+    expect(favoriteHeading).toBeInTheDocument();
+
+    const favoritePokemons = favoriteHeading.nextElementSibling;
+    const favoritesQuantity = 3;
+    expect(favoritePokemons.children.length).toBe(favoritesQuantity);
+
+    let detailsButtons = getAllByRole('link', { name: details });
+    do {
+      fireEvent.click(detailsButtons[0]);
+      fireEvent.click(getByRole('checkbox'));
+      fireEvent.click(getByRole('link', { name: favorite }));
+      if (detailsButtons.length > 1) {
+        detailsButtons = getAllByRole('link', { name: details });
+      } else {
+        detailsButtons.pop();
+      }
+    } while (detailsButtons.length > 0);
+
+    favoriteHeading = getByRole('heading', { level: 2 });
+    expect(favoriteHeading.nextElementSibling)
+      .toHaveTextContent(/no favorite pokemon found/i);
+  });
 });
