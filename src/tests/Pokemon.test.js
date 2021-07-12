5c1ad63ca63a978e04af713c7e1733d63e5bf0fc
camiloLelis <lafirmafuerte@gmail.com>
2021-07-11 23:42:24 -0300

    requisito-6-tenta-3

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,34 +1,20 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
-import App from '../App';
 import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-test('Teste se é renderizado um card pokemon-name', () => {
-  const { getByTestId } = renderWithRouter(<App />);
-  const test = getByTestId('pokemon-name');
-  // console.log(test.innerHTML);
-  expect(test.innerHTML).toBe('Pikachu');
-});
-
-test('Teste se é renderizado um card pokemontype', () => {
-  const { getByTestId } = renderWithRouter(<App />);
-  const test = getByTestId('pokemon-type');
-  expect(test.innerHTML).toBe('Electric');
-});
-
-test('Teste se é renderizado um card- weight ', () => {
-  const { getByTestId } = renderWithRouter(<App />);
-  const test = getByTestId('pokemon-weight');
-  expect(test.innerHTML).toBe('Average weight: 6.0 kg');
-});
-
-test('testa se aparece a imagem', () => {
-  const { getAllByRole } = renderWithRouter(<App />);
-  const img = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
-  const alt = 'Pikachu sprite';
-  const imagem = getAllByRole('img');
-  expect(imagem[0].src).toContain(img);
-  expect(imagem[0].alt).toContain(alt);
+test('renderizar card', () => {
+  const { getByText, getByRole, getByTestId } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/bug/i));
+  const name = getByText('Caterpie');
+  const peso = getByText('Average weight: 2.9 kg');
+  const type = getByTestId('pokemon-type');
+  const imagem = getByRole('img');
+  expect(name).toBeInTheDocument();
+  expect(peso).toBeInTheDocument();
+  expect(type.innerHTML).toBe('Bug');
+  expect(imagem.src).toContain('https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png');
+  expect(imagem.alt).toContain('Caterpie sprite');
 });
 
 test('test link', () => {
