b4bfd17ef2d6c0a70d0fab6e125a6200b9e93d28
Diego Figueirêdo <8kball@gmail.com>
2021-07-13 11:47:23 -0300

    Ajustes requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,51 +1,55 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Componente <Pokedex />', () => {
   it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-    const { container } = renderWithRouter(<App />);
-    const h2 = container.querySelector('h2');
+    const { getByRole } = renderWithRouter(<App />);
+    const h2 = getByRole('heading', { level: 2 });
 
     expect(h2).toBeInTheDocument();
-    expect(h2).toHaveTextContent('Encountered pokémons');
+    expect(h2).toHaveTextContent(/Encountered pokémons/i);
   });
 
   it('Teste se é exibido o próximo Pokémon ao clicar em botão Próximo pokémon', () => {
     const { getByText } = renderWithRouter(<App />);
-    expect(getByText(/Próximo Pokémon/i)).toBeInTheDocument();
+
+    const nextPokemon = getByText(/Próximo Pokémon/i);
+    expect(nextPokemon).toBeInTheDocument();
   });
 
   it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
     const { getByText } = renderWithRouter(<App />);
-    const filterButton = getByText('All');
+    const filterButton = getByText(/All/i);
 
     expect(filterButton).toBeInTheDocument();
+    fireEvent.click(filterButton);
   });
 
   it('Teste se é criado um botão de filtro para cada tipo de Pokémon', () => {
-    const { getAllByRole, getAllByTestId, getByText } = renderWithRouter(<App />);
+    const { getByRole, getAllByTestId } = renderWithRouter(<App />);
 
     const pokemonTypeButton = getAllByTestId('pokemon-type-button');
     const pokemonTypes = 7;
-    const allButton = getByText('All');
 
-    const fire = getAllByRole('button', { name: 'Fire' });
-    const electric = getAllByRole('button', { name: 'Electric' });
-    const poison = getAllByRole('button', { name: 'Poison' });
-    const psychic = getAllByRole('button', { name: 'Psychic' });
-    const dragon = getAllByRole('button', { name: 'Dragon' });
-    const normal = getAllByRole('button', { name: 'Normal' });
-    const bug = getAllByRole('button', { name: 'Bug' });
+    const allButton = getByRole('button', { name: 'All' });
+    const fire = getByRole('button', { name: 'Fire' });
+    const electric = getByRole('button', { name: 'Electric' });
+    const poison = getByRole('button', { name: 'Poison' });
+    const psychic = getByRole('button', { name: 'Psychic' });
+    const dragon = getByRole('button', { name: 'Dragon' });
+    const normal = getByRole('button', { name: 'Normal' });
+    const bug = getByRole('button', { name: 'Bug' });
 
     expect(pokemonTypeButton).toHaveLength(pokemonTypes);
-    expect(allButton).toBeInTheDocument(1);
-    expect(fire).toHaveLength(1);
-    expect(electric).toHaveLength(1);
-    expect(poison).toHaveLength(1);
-    expect(psychic).toHaveLength(1);
-    expect(dragon).toHaveLength(1);
-    expect(normal).toHaveLength(1);
-    expect(bug).toHaveLength(1);
+    expect(allButton).toBeInTheDocument();
+    expect(fire).toBeInTheDocument();
+    expect(electric).toBeInTheDocument();
+    expect(poison).toBeInTheDocument();
+    expect(psychic).toBeInTheDocument();
+    expect(dragon).toBeInTheDocument();
+    expect(normal).toBeInTheDocument();
+    expect(bug).toBeInTheDocument();
   });
 });
