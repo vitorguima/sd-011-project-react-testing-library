b26373ec3746f03d9729dd0a308e4818c6ab31ee
Júlio Mota <juliomotacampos@gmail.com>
2021-07-13 17:20:28 -0300

    Adiciona requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import About from '../components/About';
+
+describe('Test all App component', () => {
+  test('if renders `About` component with Heading `About Pokédex`', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const heading = getByRole('heading');
+    expect(heading.tagName).toBe('H2');
+    expect(heading).toHaveTextContent(/About Pokédex/i);
+  });
+
+  test('if `About` component contains 2 paragraphs about the application', () => {
+    const { getAllByTestId } = renderWithRouter(<About />);
+
+    const paragraphs = getAllByTestId('paragraph');
+    const { length } = paragraphs;
+
+    expect(paragraphs).toHaveLength(length);
+    paragraphs.forEach((paragraph) => {
+      expect(paragraph.tagName).toBe('P');
+    });
+  });
+
+  test('if `About` component contain a Pokédex image', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const image = getByRole('img');
+    expect(image).toBeInTheDocument();
+    expect(image.tagName).toBe('IMG');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
