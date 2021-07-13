c4bb5ecc8ecb34df3a2f265165a655a68a2378ec
Júlio Mota <juliomotacampos@gmail.com>
2021-07-13 15:31:49 -0300

    Primeiro commit :star: Vamos com tudo! :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
-
+// cheguei aqui...
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
