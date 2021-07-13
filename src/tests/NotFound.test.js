4ea6a4dff53683b468e3fdf7f816d4f7543a6f74
Ana Carolina R Santos <kroline0693@gmail.com>
2021-07-13 14:51:28 -0300

    Requisiequisiti4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,2 +1,16 @@
-describe();
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um h2 com o texto Page requested not found 😭', () => {
+    const { getByText } = render(<NotFound />);
+    const header = getByText(/Page requested not found/i);
+    expect(header).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    const { getByAltText } = render(<NotFound />);
+    const textNotFound = getByAltText(/Pikachu crying because/i);
+    expect(textNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
