8444c40a4101cee6c47d2587decd0238bd28dba7
LukeCruz <enio.cruz@yahoo.com.br>
2021-07-15 11:34:37 -0300

    b

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,32 +1,22 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import About from '../components/About';
-import renderWithRouter from './renderWithRouter';
 
-test('if page contains information about Pokédex', () => {
-  const { getByText } = renderWithRouter(<About />);
-
-  const infoPokedex = getByText(/About Pokédex/i);
-  expect(infoPokedex).toBeInTheDocument();
-});
-
-test('if contains a h2 heading with about Pokedex text', () => {
-  const { container } = renderWithRouter(<About />);
-
-  const headerPokedex = container.querySelectorAll('h2');
-  expect(headerPokedex.length).toBe(1);
-});
-
-test('if contains two paragraphs with text about Pokedex', () => {
-  const { container } = renderWithRouter(<About />);
-  const [paragraph] = container.getElementsByTagName('p');
-
-  expect(paragraph).toBeInTheDocument();
-});
-
-test('if is the correct image URL', () => {
-  const { container } = renderWithRouter(<About />);
-  const [image] = container.getElementsByTagName('img');
-
-  expect(image).toBeInTheDocument();
-  expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+describe('Testing About Component', () => {
+  it('test pokedex info', () => {
+    const { getByRole } = render(<About />);
+    const subtitle = getByRole('heading', { level: 2 });
+    expect(subtitle).toHaveTextContent(/About Pokédex/i);
+  });
+  it('test pokedex text', () => {
+    const { container } = render(<About />);
+    const line = container.querySelectorAll('p');
+    expect(line.length).toBe(2);
+  });
+  it('test pokedex URL', () => {
+    const { getByRole } = render(<About />);
+    const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = getByRole('img');
+    expect(img.src).toContain(URL);
+  });
 });
