af10a7853c4b9d7587aad3e0b70dc8529ce28b03
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-07-15 12:38:35 -0300

    Projeto React Testing Library - Envio Item 5 e 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -57,14 +57,32 @@ test('Testa se tem botões de filtro', () => {
   expect(checkBtn.length > 1).toBe(true);
 });
 
+test('São exibidos apenas os Pokemons do tipo selecionado', () => {
+  const { getAllByTestId, getByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const filterButtons = getAllByTestId('pokemon-type-button');
+  filterButtons.forEach((filter) => {
+    fireEvent.click(filter);
+    const typePokemon = getByTestId('pokemon-type');
+    expect(typePokemon.innerHTML).toBe(filter.innerHTML);
+  });
+});
 test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
-  const checkBtn = getByText(/All/i);
-  expect(checkBtn).toBeInTheDocument();
+  // const checkBtn = getByText(/All/i);
+  // expect(checkBtn).toBeInTheDocument();
+  const resetButton = getByText('All');
+  expect(resetButton).toBeInTheDocument();
+  fireEvent.click(resetButton);
+  const allPokemons = getByText(pokemons[0].name);
+  expect(allPokemons).toBeInTheDocument();
 });
 
 test('Testa se há um botão de filtro para cada tipo de Pokémon', () => {
@@ -83,4 +101,16 @@ test('Testa se há um botão de filtro para cada tipo de Pokémon', () => {
   expect(maptypeButtons.includes('Poison')).toBe(true);
   expect(maptypeButtons.includes('Dragon')).toBe(true);
   expect(maptypeButtons.includes('Normal')).toBe(true);
+});
+
+test('O botão Próximo pokémon deve ser desabilitado quando só tiver um pokémon', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const eletricButton = getByRole('button', { name: /Electric/ });
+  fireEvent.click(eletricButton);
+  const nextButton = getByRole('button', { name: /Próximo pokémon/ });
+  expect(nextButton.disabled).toBe(true);
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import pokemons from '../data';
+
+test('Testa se é renderizado um card com as informações de determinado pokémon', () => {
+  const { getByTestId, getByAltText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const pokeName = getByTestId('pokemon-name');
+  expect(pokeName.innerHTML).toBe(pokemons[0].name);
+  const pokeType = getByTestId('pokemon-type');
+  expect(pokeType.innerHTML).toBe(pokemons[0].type);
+  const pokeWeight = getByTestId('pokemon-weight');
+  expect(pokeWeight.innerHTML).toBe('Average weight: 6.0 kg');
+  const pokeImg = getByAltText(`${pokeName.innerHTML} sprite`);
+  expect(pokeImg).toHaveAttribute('src', pokemons[0].image);
+});
+
+test('Testa se o card contém um link para detalhes', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkDetails = getByText('More details');
+  expect(linkDetails).toBeInTheDocument();
+  expect(linkDetails).toHaveAttribute('href', `/pokemons/${pokemons[0].id}`);
+});
+
+test('Testa se existe um ícone de estrela nos Pokémons favoritados', () => {
+  const { getByText, getByLabelText, getByAltText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const moreDetails = getByText(/More details/);
+  fireEvent.click(moreDetails);
+  const favoritedPoke = getByLabelText('Pokémon favoritado?');
+  fireEvent.click(favoritedPoke);
+  const starIcon = getByAltText(`${pokemons[0].name} is marked as favorite`);
+  expect(starIcon).toBeInTheDocument();
+  expect(starIcon).toHaveAttribute('src', '/star-icon.svg');
+});
