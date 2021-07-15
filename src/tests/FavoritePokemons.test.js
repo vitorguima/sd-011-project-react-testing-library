0956e3ba1e90fb3d5d4472b94cbe6a131c88c926
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-07-14 22:00:39 -0300

    Projeto React Testing Library - Envio Item 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+// Requisito resolvido revendo aulas e acessando o site:
+// https://testing-library.com/docs/queries/about/
+// https://app.betrybe.com/course/live-lectures/sd-cohort-8#aula-153-rtl-testando-react-router
+// https://testing-library.com/docs/queries/about/
+// https://testing-library.com/docs/queries/byalttext/
+// Baseado no requisito 2
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  const { getByText } = render(
+    <FavoritePokemons />,
+  );
+  const checkText = getByText('No favorite pokemon found');
+  expect(checkText).toBeInTheDocument();
+});
+
+test('Teste se nenhum pokémons favoritados', () => {
+  const { container } = render(
+    <FavoritePokemons />,
+  );
+  const checkNone = container.querySelectorAll('.favorite-pokemon');
+  console.log(checkNone.length);
+  expect(checkNone.length).toBe(0);
+});
+
+test('Teste se tem pokémons favoritados', () => {
+  const { container } = render(
+    <FavoritePokemons />,
+  );
+  const checkOne = container.querySelectorAll('.favorite-pokemon');
+  console.log(checkOne.length);
+  expect(checkOne.length).toBe(0);
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+// Requisito resolvido revendo aulas e acessando o site:
+// https://testing-library.com/docs/queries/about/
+// https://app.betrybe.com/course/live-lectures/sd-cohort-8#aula-153-rtl-testando-react-router
+// https://testing-library.com/docs/queries/about/
+// https://testing-library.com/docs/queries/byalttext/
+
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+// import Pokedex from '../components/Pokedex';
+import App from '../App';
+
+test('Testa se a pág contém um heading h2 com o texto Encountered pokémons', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const checkHead = container.querySelector('h2');
+  expect(checkHead.innerHTML).toBe('Encountered pokémons');
+});
+
+test('Teste se é exibido o próximo Pokémon o btn Próximo pokémon e clicado', () => {
+  const { getByText, getByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const checkPokemon = getByText(/Pikachu/i);
+  console.log(checkPokemon.innerHTML);
+  const checkBtn = getByText(/Próximo pokémon/);
+  fireEvent.click(checkBtn);
+  const capPokemon = getByTestId('pokemon-name');
+  console.log(capPokemon.innerHTML);
+  expect(checkPokemon.innerText !== capPokemon.innerHTML).toBe(true);
+});
+
+test('Teste exibido um Pokemon por vez', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const checkOne = container.querySelectorAll('.pokemon');
+  console.log(checkOne.length);
+  expect(checkOne.length).toBe(1);
+});
+
+test('Testa se tem botões de filtro', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const checkBtn = container.querySelectorAll('.filter-button');
+  console.log(checkBtn.length);
+  expect(checkBtn.length > 1).toBe(true);
+});
+
+test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const checkBtn = getByText(/All/i);
+  expect(checkBtn).toBeInTheDocument();
+});
