3997fb7f189791729fead68b08e556ed371d1131
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-07-15 10:27:04 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,10 +1,31 @@
-describe('Teste o componente <About.js />', () => {
-  it('Teste se a página contém as informações sobre a Pokédex', () => {
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Aparece "No favorite pokemon found" se não houver pokémons favoritos', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noFavs = getByText('No favorite pokemon found');
+
+    expect(noFavs).toBeInTheDocument();
   });
-  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history, getAllByText, getByRole, getByText } = renderWithRouter(<App />);
+    history.push('/pokemons/10');
+    const favCheckBox = getByRole('checkbox');
+    fireEvent.click(favCheckBox);
+    history.push('/pokemons/23');
+    fireEvent.click(favCheckBox);
+    history.push('/favorites');
+    const pokemonCards = getAllByText(/average weight/i);
+    const caterpie = getByText('Caterpie');
+    const ekans = getByText('Ekans');
+    expect(pokemonCards.length).toEqual(2);
+    expect(caterpie).toBeInTheDocument();
+    expect(ekans).toBeInTheDocument();
   });
-  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-  });
-  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
   });
 });
