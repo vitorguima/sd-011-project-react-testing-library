831c71db258fe100bfaab075c6978e2d2da84d2b
Jander Liborio <jandernunes14@gmail.com>
2021-07-15 08:54:16 -0400

    criado o renderwithrouter

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Test the <FavoritePokemons.js /> component', () => {
-  test('Testa se é exibindo No favorite pokemon found', () => {
+  test('Test if it is displayed on the screen if you dont have favorite pokemons', () => {
     const { getByText } = renderWithRouter(<FavoritePokemons />);
     const notFoundFavorite = getByText('No favorite pokemon found');
 
     expect(notFoundFavorite).toBeInTheDocument();
   });
+
+  // test('Test if all favorite Pokemon cards are displayed.', () => {
+  //   const { getByText } = renderWithRouter(<FavoritePokemons />);
+  //   const notFoundFavorite = getByText('No favorite pokemon found');
+
+  //   expect(notFoundFavorite).toBeInTheDocument();
+  // });
+
+  // test('Test if no Pokemon card is displayed, if it is not favorited.', () => {
+  //   const { getByText } = renderWithRouter(<FavoritePokemons />);
+  //   const notFoundFavorite = getByText('No favorite pokemon found');
+
+  //   expect(notFoundFavorite).toBeInTheDocument();
+  // });
 });
