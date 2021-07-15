f5512831e1eb7b168662b53eef9845c3d8f5b246
Nikolas <nikolas.silva1990@gmail.com>
2021-07-15 12:40:58 -0300

    Requisito 7 com 90% concluído

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -25,3 +25,23 @@ test('Testa se as informações detalhadas do Pokémon são exibidas na tela.', 
   const summary = getByText(/This intelligent Pokémon roasts hard berries/i);
   expect(summary.innerHTML).toBe(pokemons[0].summary);
 });
+
+test('Testa se existe na página uma seção com os mapas', () => {
+  const { container, getByText, getAllByAltText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const moreDetails = getByText(/More details/);
+  fireEvent.click(moreDetails);
+
+  const locationDetails = getByText(`Game Locations of ${pokemons[0].name}`);
+  expect(locationDetails).toBeInTheDocument();
+
+  const location = container.querySelector('em');
+  expect(location.innerHTML).toBe('Kanto Viridian Forest');
+
+  const imgDetails = getAllByAltText(`${pokemons[0].name} location`);
+  expect(imgDetails[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  expect(imgDetails[0]).toHaveAttribute('alt', `${pokemons[0].name} location`);
+});
