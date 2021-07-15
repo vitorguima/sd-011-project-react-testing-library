eb17aac392245a034761962efc16d97ee5d96594
Jander Liborio <jandernunes14@gmail.com>
2021-07-15 07:11:36 -0400

    feito o primeiro req

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,44 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste se toda a página inicial está sendo exibida corretamente', () => {
+  test('Renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('The first link has the text Home', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const home = getByText('Home');
+    expect(home).toBeInTheDocument();
+  });
+
+  test('The second link has the text About', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const about = getByText('About');
+    expect(about).toBeInTheDocument();
+  });
+
+  test('The third link must have the text Favorite Pokemons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const about = getByText('Favorite Pokémons');
+    expect(about).toBeInTheDocument();
+  });
 });
