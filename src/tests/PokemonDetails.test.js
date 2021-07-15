6f3cf4a15b630d6882432664f76b401fe1065672
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-07-15 12:29:35 -0300

    Requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,10 +1,54 @@
-describe('Teste o componente <About.js />', () => {
-  it('Teste se a página contém as informações sobre a Pokédex', () => {
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <PokemonDetails.js />', () => {
+  it('Teste se as informações do Pokémon selecionado são mostradas na tela', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const detailsBtn = getByText(/More Details/i);
+    fireEvent.click(detailsBtn);
+    const pikachuInfo = getByText('Pikachu Details');
+    const h2Details = getByText('Summary');
+    const pokemonInfo = getByText(/Pokémon roasts hard berries with electricity to/i);
+
+    expect(detailsBtn).not.toBeInTheDocument();
+    expect(pikachuInfo).toBeInTheDocument();
+    expect(h2Details).toBeInTheDocument();
+    expect(pokemonInfo).toBeInTheDocument();
   });
-  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  it('Teste se existe uma seção com os mapas contendo as localizações do pokémon', () => {
+    const { getByText, getAllByAltText } = renderWithRouter(<App />);
+
+    const moreDets = getByText(/more details/i);
+    fireEvent.click(moreDets);
+
+    const h2Pok = getByText(/Game Locations of Pikachu/i);
+    const pikachuLoc = getAllByAltText(/Pikachu location/i);
+
+    expect(h2Pok).toBeInTheDocument();
+
+    pikachuLoc.forEach((imageAltPikachu) => {
+      expect(imageAltPikachu).toBeInTheDocument();
+      expect(imageAltPikachu).toHaveAttribute('src');
+      expect(imageAltPikachu.src.length).toBeGreaterThan(0);
+    });
   });
-  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-  });
-  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  it('Teste se o usuário pode favoritar um pokémon através da página de detalhes', () => {
+    const {
+      getByText,
+      getByLabelText,
+      getByAltText,
+    } = renderWithRouter(<App />);
+
+    const moreDets = getByText(/more details/i);
+    expect(moreDets).toBeInTheDocument();
+    fireEvent.click(moreDets);
+    const favArea = getByLabelText(/favoritado\?/i);
+    fireEvent.click(favArea);
+    const altFavArea = getByAltText(/favorite$/i);
+    expect(altFavArea).toBeInTheDocument();
+    fireEvent.click(favArea);
+    expect(altFavArea).not.toBeInTheDocument();
   });
 });
