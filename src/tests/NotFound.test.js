4387e52729467005048acaea631d427c8d1f49c0
Nalu <aluziapantoja@gmail.com>
2021-07-15 12:24:58 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('testando componente not found', () => {
+  it('testando se há um h2', () => {
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={ ['/aaa'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2).toBeInTheDocument();
+    expect(h2).toHaveTextContent(/Page requested not found/i);
+  });
+
+  it('testa se há a imagem', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter initialEntries={ ['/aaa'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const image = getAllByRole('img')[1];
+    const source = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image.src).toContain(source);
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+describe('testando componente pokédex', () => {
+  it('testando se há um h2', () => {
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={ ['/aaa'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2).toBeInTheDocument();
+    expect(h2).toHaveTextContent(/Encountered pokémons/i);
+  });
+
+  it('testa botão próximo', () => {
+
+  });
+});
