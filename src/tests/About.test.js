6d5a542ddc7ebf7d15564e2ef6f9de853d4cc129
unknown <fabnhm@hotmail.com>
2021-07-15 13:11:45 -0300

    Finazando projeto

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testes do componente <About.js />', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    const { getByText } = render(<About />);
+
+    const textAboutPokedex = getByText(/This application simulates a Pokédex/i);
+
+    expect(textAboutPokedex).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByText } = render(<About />);
+
+    const aboutHeading = getByText(/About Pokédex/i);
+
+    expect(aboutHeading).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getByText } = render(<About />);
+    const paragrapheOneText = 'This application simulates a Pokédex,'
+    + ' a digital encyclopedia containing all Pokémons';
+    const paragrapheTwoText = 'One can filter Pokémons by type,'
+    + ' and see more details for each one of them';
+
+    const paragrapheOne = getByText(paragrapheOneText);
+    const paragrapheTwo = getByText(paragrapheTwoText);
+
+    expect(paragrapheOne).toBeInTheDocument();
+    expect(paragrapheTwo).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    const { getByAltText } = render(<About />);
+
+    const image = getByAltText(/Pokédex/i);
+
+    expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,73 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import * as pokedexService from '../services/pokedexService';
+import App from '../App';
+
+jest.mock('../services/pokedexService');
+
+describe('Testes do componente <FavoritePokemons.js />', () => {
+  it('Testa se é exibido na tela a mensagem No favorite pokemon'
+  + ' found, se a pessoa não tiver pokémons favoritos', () => {
+    pokedexService.readFavoritePokemonIds.mockReturnValue([]);
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkFavorite = getByText(/Favorite Pokémons/i);
+
+    fireEvent.click(linkFavorite);
+
+    const textNoFavotite = getByText(/No favorite pokemon found/i);
+
+    expect(textNoFavotite).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const pokeOneIndex = 25;
+    const pokeTwoIndex = 4;
+    const pokeThreeIndex = 10;
+
+    const pokeIndex = [pokeOneIndex, pokeTwoIndex, pokeThreeIndex];
+    pokedexService.readFavoritePokemonIds.mockReturnValue(pokeIndex);
+    const { getByText, getAllByTestId } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkFavorite = getByText(/Favorite Pokémons/i);
+
+    fireEvent.click(linkFavorite);
+
+    const pokemonNodes = getAllByTestId('pokemon-name');
+    const expectedLength = 3;
+    expect(pokemonNodes.length).toBe(expectedLength);
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+    const pokeOneIndex = 25;
+    const pokeTwoIndex = 4;
+
+    const pokeIndex = [pokeOneIndex, pokeTwoIndex];
+    pokedexService.readFavoritePokemonIds.mockReturnValue(pokeIndex);
+    const { getByText, queryByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkFavorite = getByText(/Favorite Pokémons/i);
+
+    fireEvent.click(linkFavorite);
+
+    const pikachu = getByText(/Pikachu/i);
+    const charmander = getByText(/Charmander/i);
+    const caterpie = queryByText(/Caterpie/i);
+    const ekans = queryByText(/Ekans/i);
+
+    expect(pikachu).toBeInTheDocument();
+    expect(charmander).toBeInTheDocument();
+    expect(caterpie).not.toBeInTheDocument();
+    expect(ekans).not.toBeInTheDocument();
+  });
+});
