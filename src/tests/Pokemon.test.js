3713ed42c56dda12fda85d65d4b1e14a1b11f12d
Nikolas <nikolas.silva1990@gmail.com>
2021-07-15 10:45:32 -0300

    Requisito 6 em andamento

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+import pokemons from '../data';
+
+test('Testa se é renderizado um card com as informações de determinado pokémon', () => {
+  const { getByTestId, getByAltText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const pokeName = getByTestId('pokemon-name');
+  expect(pokeName.innerHTML).toBe(pokemons[0].name);
+  const pokeType = getByTestId('pokemon-type');
+  expect(pokeType.innerHTML).toBe(pokemons[0].type);
+  const pokeWeight = getByTestId('pokemon-weight');
+  expect(pokeWeight.innerHTML).toBe('Average weight: 6.0 kg');
+  const pokeImg = getByAltText(`${pokeName.innerHTML} sprite`);
+  expect(pokeImg).toHaveAttribute('src', pokemons[0].image);
+});
