cb5af43375e823372a173538eb146f264d16c88f
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-06 18:43:24 -0300

    Refazendo Projeto React Testing Library

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -14,33 +14,6 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-  const { getByRole } = renderWithRouter(<App />);
-  const home = getByRole('link', { name: 'Home' });
-  const about = getByRole('link', { name: 'About' });
-  const favorite = getByRole('link', { name: 'Favorite Pokémons' });
-  expect(home).toBeInTheDocument();
-  expect(about).toBeInTheDocument();
-  expect(favorite).toBeInTheDocument();
-});
 
-test('Testa se a navegação entre as páginas está correta', () => {
-  const { getByText, history } = renderWithRouter(<App />);
 
-  const home = getByText('Home');
-  const about = getByText('About');
-  const favorite = getByText('Favorite Pokémons');
-  expect(home).toBeInTheDocument();
-  expect(about).toBeInTheDocument();
-  expect(favorite).toBeInTheDocument();
 
-  fireEvent.click(home);
-  expect(history.location.pathname).toBe('/');
-  fireEvent.click(about);
-  expect(history.location.pathname).toBe('/about');
-  fireEvent.click(favorite);
-  expect(history.location.pathname).toBe('/favorites');
-  history.push('/path/random');
-  const notFound = getByText(/Page requested not found/i);
-  expect(notFound).toBeInTheDocument();
-});
