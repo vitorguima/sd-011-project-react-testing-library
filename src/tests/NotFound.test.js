1f251a9419c769c279cb4b1cf9883262e30ec435
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-07 11:52:13 -0300

    4 - Testa NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,3 +1,18 @@
-// import React from 'react';
-// import NotFound from '../components/NotFound';
-// import renderWithRouter from '../renderWithRouter';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const title = getByRole('heading', { level: 2 });
+    expect(title).toHaveTextContent('Page requested not found');
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image.src).toContain(src);
+  });
+});
